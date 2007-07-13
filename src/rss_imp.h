54f9375b325cc03f8d5da9abbf6c4e25e8ee698f
Christophe Dumez <chris@qbittorrent.org>
2007-07-13 06:55:51 +0000

    - Update to GNU GPL v3

--- src/rss_imp.h
+++ src/rss_imp.h
@@ -1,11 +1,11 @@
 /*
  * Bittorrent Client using Qt4 and libtorrent.
- * Copyright (C) 2006  Christophe Dumez, Arnaud Demaiziere
+ * Copyright (C) 2006-2007  Christophe Dumez
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -13,11 +13,9 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- *
- * Contact : chris@qbittorrent.org arnaud@qbittorrent.org
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>
  */
+
 #ifndef __RSS_IMP_H__
 #define __RSS_IMP_H__
 
