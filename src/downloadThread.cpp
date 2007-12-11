29dc0fede0c7a5086c869863f3e412b7e8883f6e
Christophe Dumez <chris@qbittorrent.org>
2007-12-11 20:23:13 +0000

    - DownloadThread can now follow redirections (fixes Mininova search engine)

--- src/downloadThread.cpp
+++ src/downloadThread.cpp
@@ -112,6 +112,14 @@ void subDownloadThread::run(){
     // We have to define CURLOPT_WRITEFUNCTION or it will crash on windows
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, fwrite);
     curl_easy_setopt(curl, CURLOPT_WRITEDATA, f);
+    // Verbose
+    curl_easy_setopt(curl, CURLOPT_VERBOSE, 1);
+    // No progress info (we don't use it)
+    curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 1);
+    // Redirections
+    curl_easy_setopt(curl, CURLOPT_AUTOREFERER, 1);
+    curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1);
+    curl_easy_setopt(curl, CURLOPT_MAXREDIRS, -1);
     qDebug("Downloading %s", url.toUtf8().data());
     res = curl_easy_perform(curl);
     /* always cleanup */
