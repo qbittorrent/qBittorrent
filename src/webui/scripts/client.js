06dd4959157f4d8d027839bd19c32573eb66dfb1
Christophe Dumez <chris@qbittorrent.org>
2008-07-04 07:48:15 +0000

    - Fixed copyright info

--- src/webui/scripts/client.js
+++ src/webui/scripts/client.js
@@ -19,7 +19,7 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
- * /
+ */
 
 window.addEvent('domready', function(){
 	myTable = new dynamicTable('myTable', {overCls: 'over', selectCls: 'selected', altCls: 'alt'});
