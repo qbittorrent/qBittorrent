3ae55b6a6f3c1d9c34add1d818afc9ef63e2dab6
Christophe Dumez <chris@qbittorrent.org>
2008-12-26 13:49:45 +0000

    - Fixed typo

--- src/rss.h
+++ src/rss.h
@@ -670,7 +670,7 @@ class RssManager : public QObject{
       saveStreamList();
       qDeleteAll(streams);
       delete downloader;
-      qDebug("RSSManger deleted");
+      qDebug("RSSManager deleted");
     }
 
     // load the list of the rss stream
