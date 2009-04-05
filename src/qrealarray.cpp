16798879b97cce788f5f26f103c62abbb909cd41
Christophe Dumez <chris@qbittorrent.org>
2009-04-05 17:01:07 +0000

    - License fix due to problems with OpenSSL

--- src/qrealarray.cpp
+++ src/qrealarray.cpp
@@ -16,6 +16,15 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
+ * In addition, as a special exception, the copyright holders give permission to
+ * link this program with the OpenSSL project's "OpenSSL" library (or with
+ * modified versions of it that use the same license as the "OpenSSL" library),
+ * and distribute the linked executables. You must obey the GNU General Public
+ * License in all respects for all of the code used other than "OpenSSL".  If you
+ * modify file(s), you may extend this exception to your version of the file(s),
+ * but you are not obligated to do so. If you do not wish to do so, delete this
+ * exception statement from your version.
+ *
  * Contact : chris@qbittorrent.org, arnaud@qbittorrent.org
  */
 
