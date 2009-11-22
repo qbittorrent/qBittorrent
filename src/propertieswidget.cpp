508c765991d63d08482b6b2de0064a9017c59a47
Christophe Dumez <chris@qbittorrent.org>
2009-11-22 21:50:05 +0000

    - Fix property buttons style on Mac OS by Stephanos Antaris.

--- src/propertieswidget.cpp
+++ src/propertieswidget.cpp
@@ -51,8 +51,8 @@
 #include "pieceavailabilitybar.h"
 
 #ifdef Q_WS_MAC
-#define DEFAULT_BUTTON_CSS ""
-#define SELECTED_BUTTON_CSS "background-color: rgb(255, 208, 105);}"
+#define DEFAULT_BUTTON_CSS "QPushButton {border: 1px solid rgb(85, 81, 91);border-radius: 3px;padding: 2px; margin-left: 8px; margin-right: 8px;}"
+#define SELECTED_BUTTON_CSS "QPushButton {border: 1px solid rgb(85, 81, 91);border-radius: 3px;padding: 2px;background-color: rgb(255, 208, 105); margin-left: 8px; margin-right: 8px;}"
 #else
 #define DEFAULT_BUTTON_CSS "QPushButton {border: 1px solid rgb(85, 81, 91);border-radius: 3px;padding: 2px; margin-left: 3px; margin-right: 3px;}"
 #define SELECTED_BUTTON_CSS "QPushButton {border: 1px solid rgb(85, 81, 91);border-radius: 3px;padding: 2px;background-color: rgb(255, 208, 105); margin-left: 3px; margin-right: 3px;}"
