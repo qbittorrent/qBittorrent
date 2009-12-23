9d06947125f334265cc33eca4d406c8289c7e377
Christophe Dumez <chris@qbittorrent.org>
2009-12-23 20:11:48 +0000

    - Updated Catalan author name

--- src/about_imp.h
+++ src/about_imp.h
@@ -64,7 +64,7 @@ class about : public QDialog, private Ui::AboutDlg{
       te_translation->append(QString::fromUtf8(
           "<i>- <u>Brazilian:</u> Nick Marinho (nickmarinho@gmail.com)<br>\
           - <u>Bulgarian:</u> Tsvetan & Boiko Bankov (emerge_life@users.sourceforge.net)<br>\
-          - <u>Catalan:</u> Gekko Dam Beer (gekko04@users.sourceforge.net)<br>\
+          - <u>Catalan:</u> Francisco Luque Contreras (frannoe@ya.com)<br>\
           - <u>Chinese (Simplified):</u> Guo Yue (yue.guo0418@gmail.com)<br>\
           - <u>Chinese (Traditional):</u> Yi-Shun Wang (dnextstep@gmail.com)<br>\
           - <u>Czech:</u> Jirka Vilim (web@tets.cz)<br>\
