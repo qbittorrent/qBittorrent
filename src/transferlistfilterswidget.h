56dc8cad71b75158713c5e79cbaa81a53b3d3925
Christophe Dumez <chris@qbittorrent.org>
2010-01-03 21:09:01 +0000

    - Added ASSERT

--- src/transferlistfilterswidget.h
+++ src/transferlistfilterswidget.h
@@ -357,8 +357,8 @@ protected slots:
   }
 
   void torrentAboutToBeDeleted(QModelIndex index) {
-    if(!index.isValid()) return;
     Q_ASSERT(index.isValid());
+    if(!index.isValid()) return;
     QString label = transferList->model()->index(index.row(), TR_LABEL).data(Qt::DisplayRole).toString().trimmed();
     if(!label.isEmpty()) {
       // Update label counter
