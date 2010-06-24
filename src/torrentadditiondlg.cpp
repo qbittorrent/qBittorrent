2104e4e2ce2ce7a971d52b10d4070f3d2a647407
Christophe Dumez <chris@qbittorrent.org>
2010-06-24 18:16:59 +0000

    Fix crash when adding magnet links

--- src/torrentadditiondlg.cpp
+++ src/torrentadditiondlg.cpp
@@ -472,7 +472,7 @@ void torrentAdditionDialog::renameSelectedFile() {
       save_path = save_path.replace("\\", "/");
 #endif
       save_path = misc::expandPath(save_path);
-      if(t->num_files() == 1) {
+      if(!is_magnet && t->num_files() == 1) {
         // Remove file name
         QStringList parts = save_path.split("/", QString::SkipEmptyParts);
         const QString single_file_name = parts.takeLast();
