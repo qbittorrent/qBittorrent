e8f229816ef01a0d7fba14ca71bc5ac115130662
Christophe Dumez <chris@qbittorrent.org>
2010-10-18 18:21:00 +0000

    BUGFIX: Fix issues when marking a file as 'not downloaded' causes the torrent to complete

--- src/qtorrenthandle.cpp
+++ src/qtorrenthandle.cpp
@@ -580,9 +580,12 @@ void QTorrentHandle::prioritize_files(const std::vector<int> &v) {
   Q_ASSERT(h.is_valid());
   if(v.size() != (unsigned int)h.get_torrent_info().num_files())
     return;
+  bool was_seed = is_seed();
   h.prioritize_files(v);
-  // Save seed status
-  TorrentPersistentData::saveSeedStatus(*this);
+  if(was_seed && !is_seed()) {
+    // Reset seed status
+    TorrentPersistentData::saveSeedStatus(*this);
+  }
 }
 
 void QTorrentHandle::set_ratio(float ratio) const {
