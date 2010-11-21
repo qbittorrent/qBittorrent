2b45e1baa34fed78f2aa00c21c8a00ecbe125651
Christophe Dumez <chris@qbittorrent.org>
2010-11-21 17:38:48 +0000

    Fix HTTP redirect issue that would cause the torrent addition to show up for automated RSS downloads (Closes #677565)

--- src/downloadthread.cpp
+++ src/downloadthread.cpp
@@ -51,7 +51,7 @@ downloadThread::downloadThread(QObject* parent) : QObject(parent) {
 }
 
 void downloadThread::processDlFinished(QNetworkReply* reply) {
-  QString url = reply->url().toEncoded().data();
+  QString url = reply->url().toString();
   qDebug("Download finished: %s", qPrintable(url));
   if(reply->error() != QNetworkReply::NoError) {
     // Failure
