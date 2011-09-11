51b3a5164d2113619e02f1931ddad1725f92f52d
Christophe Dumez <chris@qbittorrent.org>
2011-09-11 21:26:30 +0300

    Search: Fix torrent download with Python 3

--- src/searchengine/nova3/helpers.py
+++ src/searchengine/nova3/helpers.py
@@ -77,7 +77,7 @@ def retrieve_url(url):
 def download_file(url, referer=None):
     """ Download file at url and write it to a file, return the path to the file and the url """
     file, path = tempfile.mkstemp()
-    file = os.fdopen(file, "w")
+    file = os.fdopen(file, "wb")
     # Download url
     req = urllib.request.Request(url)
     if referer is not None:
