86f2447f8a1eb13981794d5d74cbc48f3f399b07
Christophe Dumez <chris@qbittorrent.org>
2012-05-02 17:34:44 +0300

    Fix possible compilation error on Windows

--- src/qtlibtorrent/qbtsession.cpp
+++ src/qtlibtorrent/qbtsession.cpp
@@ -1565,7 +1565,7 @@ void QBtSession::loadSessionState() {
   // bdecode
   lazy_entry e;
 #if LIBTORRENT_VERSION_MINOR > 15
-  error_code ec;
+  libtorrent::error_code ec;
   lazy_bdecode(&in[0], &in[0] + in.size(), e, ec);
   if(!ec) {
 #else
@@ -1952,7 +1952,7 @@ void QBtSession::setListeningPort(int port) {
   Preferences pref;
   std::pair<int,int> ports(port, port);
 #if LIBTORRENT_VERSION_MINOR > 15
-  error_code ec;
+  libtorrent::error_code ec;
 #endif
   const QString iface_name = pref.getNetworkInterface();
   if(iface_name.isEmpty()) {
