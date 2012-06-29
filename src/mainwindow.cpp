b3aec8e6f97b6a6860cd590edccc4df128c5e631
Christophe Dumez <christophe.dumez@intel.com>
2012-06-29 19:39:53 +0300

    Bring window to front after restoring from systray

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -705,6 +705,8 @@ void MainWindow::toggleVisibility(QSystemTrayIcon::ActivationReason e) {
       setWindowState(windowState() & ~Qt::WindowMinimized | Qt::WindowActive);
       // Then show it
       show();
+      raise();
+      activateWindow();
     }else{
       hide();
     }
