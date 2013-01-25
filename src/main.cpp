a2656670f7a6cad5d1bed420299c06036534209e
Nick Tiskov <daymansmail@gmail.com>
2013-01-25 22:52:12 +0400

    Fixes: 1) Building fails w/o  "CONFIG += strace_win" 2) mingw build
    Commenting Mingw release flags section for now. Strace is not reliable with mingw (only calls to other libraries are resolved, internal calls show up as addresses).

--- src/main.cpp
+++ src/main.cpp
@@ -152,9 +152,11 @@ void sigsegvHandler(int) {
   std::cerr << "qBittorrent version: " << VERSION << std::endl;
   print_stacktrace();
 #else
+#ifdef STACKTRACE_WIN
   StraceDlg dlg;
   dlg.setStacktraceString(straceWin::getBacktrace());
   dlg.exec();
+#endif
 #endif
   raise(SIGSEGV);
 }
@@ -167,9 +169,11 @@ void sigabrtHandler(int) {
   std::cerr << "qBittorrent version: " << VERSION << std::endl;
   print_stacktrace();
 #else
+#ifdef STACKTRACE_WIN
   StraceDlg dlg;
   dlg.setStacktraceString(straceWin::getBacktrace());
   dlg.exec();
+#endif
 #endif
   raise(SIGABRT);
 }
