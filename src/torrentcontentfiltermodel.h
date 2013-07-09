09abc9d8b73094582a8d8868a309563853c12c18
sledgehammer999 <hammered999@gmail.com>
2013-07-09 19:15:53 +0300

    Use naturalSort in TorrentContentFilterModel.

--- src/torrentcontentfiltermodel.h
+++ src/torrentcontentfiltermodel.h
@@ -33,6 +33,7 @@
 
 #include <QSortFilterProxyModel>
 #include "torrentcontentmodelitem.h"
+#include "proplistdelegate.h"
 
 class TorrentContentModel;
 
@@ -53,6 +54,7 @@ signals:
 
 protected:
   virtual bool filterAcceptsRow(int source_row, const QModelIndex& source_parent) const;
+  virtual bool lessThan(const QModelIndex &left, const QModelIndex &right) const;
 
 public slots:
   void selectAll();
