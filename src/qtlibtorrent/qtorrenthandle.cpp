7c1f712181d0a115f7321c14f98b10fc021b560a
Konstantin Goncharik <botankras@gmail.com>
2014-01-04 01:19:30 +0800

    In qtorrenthandle.cpp exception made caught by reference

--- src/qtlibtorrent/qtorrenthandle.cpp
+++ src/qtlibtorrent/qtorrenthandle.cpp
@@ -303,7 +303,7 @@ QStringList QTorrentHandle::url_seeds() const {
       qDebug("URL Seed: %s", it->c_str());
       res << misc::toQString(*it);
     }
-  } catch(std::exception e) {
+  } catch(std::exception &e) {
     std::cout << "ERROR: Failed to convert the URL seed" << std::endl;
   }
   return res;
