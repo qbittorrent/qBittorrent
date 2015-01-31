38455af7917c6ccd6ea9a32a7096693d2ddeeb53
sledgehammer999 <hammered999@gmail.com>
2015-02-01 01:27:51 +0200

    Follow project coding style. Issue #2192.

--- src/addnewtorrentdialog.h
+++ src/addnewtorrentdialog.h
@@ -40,69 +40,69 @@
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
-class AddNewTorrentDialog;
+    class AddNewTorrentDialog;
 }
 QT_END_NAMESPACE
 
 class TorrentContentFilterModel;
 class PropListDelegate;
 
-class AddNewTorrentDialog : public QDialog
+class AddNewTorrentDialog: public QDialog
 {
-  Q_OBJECT
-  
-public:
-  ~AddNewTorrentDialog();
+    Q_OBJECT
 
-  static void showTorrent(const QString& torrent_path, const QString& from_url, QWidget *parent = 0);
-  static void showMagnet(const QString& torrent_link, QWidget *parent = 0);
+public:
+    ~AddNewTorrentDialog();
+
+    static void showTorrent(const QString& torrent_path, const QString& from_url, QWidget *parent = 0);
+    static void showMagnet(const QString& torrent_link, QWidget *parent = 0);
 
 protected:
-  void showEvent(QShowEvent *event);
+    void showEvent(QShowEvent *event);
 
 private slots:
-  void showAdvancedSettings(bool show);
-  void displayContentTreeMenu(const QPoint&);
-  void updateDiskSpaceLabel();
-  void onSavePathChanged(int);
-  void relayout();
-  void renameSelectedFile();
-  void setdialogPosition();
-  void updateMetadata(const QTorrentHandle& h);
-  void browseButton_clicked();
+    void showAdvancedSettings(bool show);
+    void displayContentTreeMenu(const QPoint&);
+    void updateDiskSpaceLabel();
+    void onSavePathChanged(int);
+    void relayout();
+    void renameSelectedFile();
+    void setdialogPosition();
+    void updateMetadata(const QTorrentHandle& h);
+    void browseButton_clicked();
 
 protected slots:
-  virtual void accept();
-  virtual void reject();
+    virtual void accept();
+    virtual void reject();
 
 private:
-  explicit AddNewTorrentDialog(QWidget *parent = 0);
-  bool loadTorrent(const QString& torrent_path, const QString& from_url);
-  bool loadMagnet(const QString& magnet_uri);
-  void loadSavePathHistory();
-  void saveSavePathHistory() const;
-  int indexOfSavePath(const QString& save_path);
-  void updateFileNameInSavePaths(const QString& new_filename);
-  void loadState();
-  void saveState();
-  void setMetadataProgressIndicator(bool visibleIndicator, const QString &labelText = QString());
-  void setupTreeview();
+    explicit AddNewTorrentDialog(QWidget *parent = 0);
+    bool loadTorrent(const QString& torrent_path, const QString& from_url);
+    bool loadMagnet(const QString& magnet_uri);
+    void loadSavePathHistory();
+    void saveSavePathHistory() const;
+    int indexOfSavePath(const QString& save_path);
+    void updateFileNameInSavePaths(const QString& new_filename);
+    void loadState();
+    void saveState();
+    void setMetadataProgressIndicator(bool visibleIndicator, const QString &labelText = QString());
+    void setupTreeview();
 
 private:
-  Ui::AddNewTorrentDialog *ui;
-  TorrentContentFilterModel *m_contentModel;
-  PropListDelegate *m_contentDelegate;
-  bool m_isMagnet;
-  bool m_hasMetadata;
-  QString m_filePath;
-  QString m_url;
-  QString m_hash;
-  boost::intrusive_ptr<libtorrent::torrent_info> m_torrentInfo;
-  QStringList m_filesPath;
-  bool m_hasRenamedFile;
-  QShortcut *editHotkey;
-  QByteArray m_headerState;
-  int m_oldIndex;
+    Ui::AddNewTorrentDialog *ui;
+    TorrentContentFilterModel *m_contentModel;
+    PropListDelegate *m_contentDelegate;
+    bool m_isMagnet;
+    bool m_hasMetadata;
+    QString m_filePath;
+    QString m_url;
+    QString m_hash;
+    boost::intrusive_ptr<libtorrent::torrent_info> m_torrentInfo;
+    QStringList m_filesPath;
+    bool m_hasRenamedFile;
+    QShortcut *editHotkey;
+    QByteArray m_headerState;
+    int m_oldIndex;
 };
 
 #endif // ADDNEWTORRENTDIALOG_H
