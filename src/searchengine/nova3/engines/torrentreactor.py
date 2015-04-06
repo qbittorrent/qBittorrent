d6d0f422f58193acdef698c11f8e644753454efa
DoumanAsh <custparasite@gmx.se>
2015-04-06 08:36:41 +0300

    [search engine] engines update

--- src/searchengine/nova3/engines/torrentreactor.py
+++ src/searchengine/nova3/engines/torrentreactor.py
@@ -1,4 +1,4 @@
-#VERSION: 1.33
+#VERSION: 1.35
 #AUTHORS: Gekko Dam Beer (gekko04@users.sourceforge.net)
 #CONTRIBUTORS: Christophe Dumez (chris@qbittorrent.org)
 #              Bruno Barbieri (brunorex@gmail.com)
@@ -28,91 +28,94 @@
 # POSSIBILITY OF SUCH DAMAGE.
 
 from novaprinter import prettyPrinter
-from helpers import retrieve_url, download_file
-from urllib import error, parse
+from helpers import download_file
+from urllib import parse
 from html.parser import HTMLParser
+from http.client import HTTPConnection as http
 import re
 
 class torrentreactor(object):
-	url = 'http://www.torrentreactor.net'
-	name = 'TorrentReactor.Net'
-	supported_categories = {'all': '', 'movies': '5', 'tv': '8', 'music': '6', 'games': '3', 'anime': '1', 'software': '2'}
+    url = 'http://www.torrentreactor.net'
+    name = 'TorrentReactor.Net'
+    supported_categories = {'all': '', 'movies': '5', 'tv': '8', 'music': '6', 'games': '3', 'anime': '1', 'software': '2'}
 
-	def download_torrent(self, info):
-		print(download_file(info))
+    def download_torrent(self, info):
+        print(download_file(info))
 
-	class SimpleHTMLParser(HTMLParser):
-		def __init__(self, results, url, *args):
-			HTMLParser.__init__(self)
-			self.td_counter = None
-			self.current_item = None
-			self.results = results
-			self.id = None
-			self.url = url
-			self.dispatcher = { 'a' : self.start_a, 'td' : self.start_td }
+    class SimpleHTMLParser(HTMLParser):
+        def __init__(self, results, url, *args):
+            HTMLParser.__init__(self)
+            self.td_counter = None
+            self.current_item = None
+            self.results = results
+            self.id = None
+            self.url = url
+            self.dispatcher = { 'a' : self.start_a, 'td' : self.start_td }
 
-		def handle_starttag(self, tag, attrs):
-			if tag in self.dispatcher:
-				self.dispatcher[tag](attrs)
+        def handle_starttag(self, tag, attrs):
+            if tag in self.dispatcher:
+                self.dispatcher[tag](attrs)
 
-		def start_a(self, attr):
-			params = dict(attr)
-			if re.match("/torrents/\d+.*", params['href']):
-				self.current_item = {}
-				self.current_item['desc_link'] = self.url+params['href'].strip()
-			elif 'torrentreactor.net/download.php' in params['href']:
-				self.td_counter = 0
-				self.current_item['link'] = params['href'].strip()
-				self.current_item['name'] = parse.unquote_plus(params['href'].split('&')[1].split('name=')[1])
+        def start_a(self, attr):
+            params = dict(attr)
+            if re.match("/torrents/\d+.*", params['href']):
+                self.current_item = {}
+                self.current_item['desc_link'] = self.url+params['href'].strip()
+            elif 'torrentreactor.net/download.php' in params['href']:
+                self.td_counter = 0
+                self.current_item['link'] = params['href'].strip()
+                self.current_item['name'] = parse.unquote_plus(params['href'].split('&')[1].split('name=')[1])
 
-		def handle_data(self, data):
-			if self.td_counter == 1:
-				if 'size' not in self.current_item:
-					self.current_item['size'] = ''
-				self.current_item['size']+= data.strip()
-			elif self.td_counter == 2:
-				if 'seeds' not in self.current_item:
-					self.current_item['seeds'] = ''
-				self.current_item['seeds']+= data.strip()
-			elif self.td_counter == 3:
-				if 'leech' not in self.current_item:
-					self.current_item['leech'] = ''
-				self.current_item['leech']+= data.strip()
+        def handle_data(self, data):
+            if self.td_counter == 1:
+                if 'size' not in self.current_item:
+                    self.current_item['size'] = ''
+                self.current_item['size']+= data.strip()
+            elif self.td_counter == 2:
+                if 'seeds' not in self.current_item:
+                    self.current_item['seeds'] = ''
+                self.current_item['seeds']+= data.strip()
+            elif self.td_counter == 3:
+                if 'leech' not in self.current_item:
+                    self.current_item['leech'] = ''
+                self.current_item['leech']+= data.strip()
 
-		def start_td(self,attr):
-			if isinstance(self.td_counter,int):
-				self.td_counter += 1
-				if self.td_counter > 3:
-					self.td_counter = None
-					# add item to results
-					if self.current_item:
-						self.current_item['engine_url'] = self.url
-						if not self.current_item['seeds'].isdigit():
-							self.current_item['seeds'] = 0
-						if not self.current_item['leech'].isdigit():
-							self.current_item['leech'] = 0
-						prettyPrinter(self.current_item)
-						self.has_results = True
-						self.results.append('a')
+        def start_td(self,attr):
+            if isinstance(self.td_counter,int):
+                self.td_counter += 1
+                if self.td_counter > 3:
+                    self.td_counter = None
+                    # add item to results
+                    if self.current_item:
+                        self.current_item['engine_url'] = self.url
+                        if not self.current_item['seeds'].isdigit():
+                            self.current_item['seeds'] = 0
+                        if not self.current_item['leech'].isdigit():
+                            self.current_item['leech'] = 0
+                        prettyPrinter(self.current_item)
+                        self.has_results = True
+                        self.results.append('a')
 
-	def __init__(self):
-		self.results = []
-		self.parser = self.SimpleHTMLParser(self.results, self.url)
+    def search(self, what, cat='all'):
+        i = 0
+        dat = ''
+        connection = http("www.torrentreactor.net")
 
-	def search(self, what, cat='all'):
-		i = 0
-		dat = ''
-		while True and i<11:
-			results = []
-			parser = self.SimpleHTMLParser(results, self.url)
+        while True and i<11:
+            results = []
+            parser = self.SimpleHTMLParser(results, self.url)
+            query = '/torrents-search/%s/%d?sort=seeders.desc&type=all&period=none&categories=%s'%(what, (i*35), self.supported_categories[cat])
+            connection.request("GET", query)
+            response = connection.getresponse()
+            if response.status != 200:
+                break
 
-			try:
-				dat = retrieve_url(self.url+'/torrent-search/%s/%s?sort=seeders.desc&type=all&period=none&categories=%s'%(what, (i*35), self.supported_categories[cat]))
-			except error.HTTPError:
-				break
+            dat = response.read().decode('utf-8')
 
-			parser.feed(dat)
-			parser.close()
-			if len(results) <= 0:
-				break
-			i += 1
+            parser.feed(dat)
+            parser.close()
+            if len(results) <= 0:
+                break
+            i += 1
+
+        connection.close()
