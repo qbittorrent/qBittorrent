2fc14876037d8fca34c23104cf20a600f0c5cc5b
DoumanAsh <custparasite@gmx.se>
2015-04-06 08:36:58 +0300

    [search engine] re-factoring of code

--- src/searchengine/nova3/nova2dl.py
+++ src/searchengine/nova3/nova2dl.py
@@ -25,7 +25,7 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
-#VERSION: 1.10
+#VERSION: 1.20
 
 # Author:
 #  Christophe DUMEZ (chris@qbittorrent.org)
@@ -39,26 +39,26 @@ supported_engines = dict()
 
 engines = glob.glob(os.path.join(os.path.dirname(__file__), 'engines','*.py'))
 for engine in engines:
-	e = engine.split(os.sep)[-1][:-3]
-	if len(e.strip()) == 0: continue
-	if e.startswith('_'): continue
-	try:
-		exec("from engines.%s import %s"%(e,e))
-		exec("engine_url = %s.url"%e)
-		supported_engines[engine_url] = e
-	except:
-		pass
+    e = engine.split(os.sep)[-1][:-3]
+    if len(e.strip()) == 0: continue
+    if e.startswith('_'): continue
+    try:
+        exec("from engines.%s import %s"%(e,e))
+        exec("engine_url = %s.url"%e)
+        supported_engines[engine_url] = e
+    except:
+        pass
 
 if __name__ == '__main__':
-	if len(sys.argv) < 3:
-		raise SystemExit('./nova2dl.py engine_url download_parameter')
-	engine_url = sys.argv[1].strip()
-	download_param = sys.argv[2].strip()
-	if engine_url not in list(supported_engines.keys()):
-		raise SystemExit('./nova2dl.py: this engine_url was not recognized')
-	exec("engine = %s()"%supported_engines[engine_url])
-	if hasattr(engine, 'download_torrent'):
-		engine.download_torrent(download_param)
-	else:
-		print(download_file(download_param))
-	sys.exit(0)
+    if len(sys.argv) < 3:
+        raise SystemExit('./nova2dl.py engine_url download_parameter')
+    engine_url = sys.argv[1].strip()
+    download_param = sys.argv[2].strip()
+    if engine_url not in list(supported_engines.keys()):
+        raise SystemExit('./nova2dl.py: this engine_url was not recognized')
+    exec("engine = %s()"%supported_engines[engine_url])
+    if hasattr(engine, 'download_torrent'):
+        engine.download_torrent(download_param)
+    else:
+        print(download_file(download_param))
+    sys.exit(0)
