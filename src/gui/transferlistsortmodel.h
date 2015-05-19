37673cd86f56344a41ea72a1ca5510c573ad6d49
Vladimir Sinenko <vladimir.sinenko@gmail.com>
2015-05-19 22:10:57 +0600

    Fixed sort order for datetime columns with empty values (closes #2988)
    A small fix belonging to #2531.
    During the sorting empty QDateTime values are shuffled around due to
    unstable sort in QSortFilterProxyModel (see #2526 and #2158), causing
    the transfer list items to constantly change order.
    
    Fixed by using an already existing correct comparison (with a torrent
    hash fallback).

--- src/gui/transferlistsortmodel.h
+++ src/gui/transferlistsortmodel.h
@@ -52,6 +52,7 @@ public:
 private:
     bool lessThan(const QModelIndex &left, const QModelIndex &right) const;
     bool lowerPositionThan(const QModelIndex &left, const QModelIndex &right) const;
+    bool dateLessThan(const int dateColumn, const QModelIndex &left, const QModelIndex &right) const;
     bool filterAcceptsRow(int sourceRow, const QModelIndex &sourceParent) const;
     bool matchFilter(int sourceRow, const QModelIndex &sourceParent) const;
 
