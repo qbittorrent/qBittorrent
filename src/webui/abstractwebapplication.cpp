c5ce99ebecb31ca60e442c4416c04a40b90c3120
ngosang <diegodelasheras@gmail.com>
2015-05-23 19:20:04 +0200

    Web UI: Changes in title bar

--- src/webui/abstractwebapplication.cpp
+++ src/webui/abstractwebapplication.cpp
@@ -194,7 +194,7 @@ bool AbstractWebApplication::readFile(const QString& path, QByteArray &data, QSt
             QString dataStr = QString::fromUtf8(data.constData());
             translateDocument(dataStr);
 
-            if (path.endsWith("about.html"))
+            if (path.endsWith("about.html") || path.endsWith("index.html") || path.endsWith("client.js"))
                 dataStr.replace("${VERSION}", VERSION);
 
             data = dataStr.toUtf8();
