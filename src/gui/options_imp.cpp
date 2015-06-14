915fce01915e4e58b954aa823e52222b9d50eb96
Chocobo1 <Chocobo1@users.noreply.github.com>
2015-06-15 00:57:59 +0800

    Rename menu "Options..." to "Preferences" on *nix and OSX

--- src/gui/options_imp.cpp
+++ src/gui/options_imp.cpp
@@ -64,6 +64,11 @@ options_imp::options_imp(QWidget *parent):
   setupUi(this);
   setAttribute(Qt::WA_DeleteOnClose);
   setModal(true);
+
+#if (defined(Q_OS_UNIX))
+  setWindowTitle(tr("Preferences"));
+#endif
+
   // Icons
   tabSelection->item(TAB_UI)->setIcon(IconProvider::instance()->getIcon("preferences-desktop"));
   tabSelection->item(TAB_BITTORRENT)->setIcon(IconProvider::instance()->getIcon("preferences-system-network"));
