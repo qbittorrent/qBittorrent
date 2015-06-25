436b3a472a3544c97aff0ce35f525b09310d91d8
Chocobo1 <Chocobo1@users.noreply.github.com>
2015-06-25 20:40:09 +0800

    Move translators & thanks to its own file Add HTML header & cleanup

--- src/core/unicodestrings.h
+++ src/core/unicodestrings.h
@@ -26,10 +26,14 @@
  * exception statement from your version.
  */
 
+// This file must be encoded in "UTF-8 with BOM"
 #ifdef _MSC_VER
 #pragma execution_character_set("utf-8")
 #endif
 
+// Because of the poor handling of UTF-8 characters in MSVC (emits warning C4819),
+// we put all problematic UTF-8 chars/strings in this file.
+// See issue #3059 for more details (https://github.com/qbittorrent/qBittorrent/issues/3059).
 const char C_INFINITY[] = "∞";
 const char C_UP[] = "▲";
 const char C_DOWN[] = "▼";
@@ -76,48 +80,3 @@ const char C_LOCALE_VIETNAMESE[] = "tiếng Việt";
 const char C_LOCALE_CHINESE_TRADITIONAL[] = "中文 (繁體)";
 const char C_LOCALE_CHINESE_SIMPLIFIED[] = "中文 (简体)";
 const char C_LOCALE_KOREAN[] = "한글";
-const char C_THANKS[] =
-    "<p>I would first like to thank sourceforge.net for hosting qBittorrent project and for their support.</p>\
-    <p>I am pleased that people from all over the world are contributing to qBittorrent: Ishan Arora (India), Arnaud Demaizière (France) and Stephanos Antaris (Greece). Their help is greatly appreciated</p>\
-    <p>I also want to thank Στέφανος Αντάρης (santaris@csd.auth.gr) and Mirco Chinelli (infinity89@fastwebmail.it) for working on Mac OS X packaging.</p>\
-	<p>I am grateful to Peter Koeleman (peter@qbittorrent.org) and Mohammad Dib (mdib@qbittorrent.org) for working on qBittorrent port to Windows.</p>\
-	<p>Thanks a lot to our graphist Mateusz Toboła (tobejodok@qbittorrent.org) for his great work.</p>";
-const char C_TRANSLATORS[] =
-    "<ul><li><u>Arabic:</u> SDERAWI (abz8868@msn.com), sn51234 (nesseyan@gmail.com) and  Ibrahim Saed ibraheem_alex(Transifex)</li>\
-    <li><u>Armenian:</u> Hrant Ohanyan (hrantohanyan@mail.am)</li>\
-    <li><u>Basque:</u> Xabier Aramendi (azpidatziak@gmail.com)</li>\
-    <li><u>Belarusian:</u> Mihas Varantsou (meequz@gmail.com)</li>\
-    <li><u>Bulgarian:</u> Tsvetan & Boyko Bankoff (emerge_life@users.sourceforge.net)</li>\
-    <li><u>Catalan:</u> Francisco Luque Contreras (frannoe@ya.com)</li>\
-    <li><u>Chinese (Simplified):</u> Guo Yue (yue.guo0418@gmail.com)</li>\
-    <li><u>Chinese (Traditional):</u> Yi-Shun Wang (dnextstep@gmail.com) and 冥王歐西里斯 s8321414(Transifex)</li>\
-    <li><u>Croatian:</u> Oliver Mucafir (oliver.untwist@gmail.com)</li>\
-    <li><u>Czech:</u> Jirka Vilim (web@tets.cz) and Petr Cernobila abr(Transifex)</li>\
-    <li><u>Danish:</u> Mathias Nielsen (comoneo@gmail.com)</li>\
-    <li><u>Dutch:</u> Pieter Heyvaert (pieter_heyvaert@hotmail.com)</li>\
-    <li><u>English(Australia):</u> Robert Readman readmanr(Transifex)</li>\
-    <li><u>English(United Kingdom):</u> Robert Readman readmanr(Transifex)</li>\
-    <li><u>Finnish:</u> Niklas Laxström (nikerabbit@users.sourceforge.net), Pekka Niemi (pekka.niemi@iki.fi) and Jiri Grönroos artnay(Transifex)</li>\
-    <li><u>Galician:</u> Marcos Lans (marcoslansgarza@gmail.com) and antiparvos(Transifex)</li>\
-    <li><u>Georgian:</u> Beqa Arabuli (arabulibeqa@yahoo.com)</li>\
-    <li><u>German:</u> Niels Hoffmann (zentralmaschine@users.sourceforge.net), schnurlos (schnurlos@gmail.com)</li>\
-    <li><u>Greek:</u> Tsvetan Bankov (emerge_life@users.sourceforge.net), Stephanos Antaris (santaris@csd.auth.gr), sledgehammer999(hammered999@gmail.com), Γιάννης Ανθυμίδης Evropi(Transifex) and Panagiotis Tabakis(tabakisp@gmail.com)</li>\
-    <li><u>Hebrew:</u> David Deutsch (d.deffo@gmail.com)</li>\
-    <li><u>Hungarian:</u> Majoros Péter (majoros.peterj@gmail.com)</li>\
-    <li><u>Italian:</u> bovirus (bovirus@live.it) and Matteo Sechi (bu17714@gmail.com)</li>\
-    <li><u>Japanese:</u> Masato Hashimoto (cabezon.hashimoto@gmail.com)</li>\
-    <li><u>Korean:</u> Jin Woo Sin (jin828sin@users.sourceforge.net)</li>\
-    <li><u>Lithuanian:</u> Naglis Jonaitis (njonaitis@gmail.com)</li>\
-    <li><u>Norwegian:</u> Tomaso</li>\
-    <li><u>Polish:</u> Mariusz Fik (fisiu@opensuse.org)</li>\
-    <li><u>Portuguese:</u> Sérgio Marques smarquespt(Transifex)</li>\
-    <li><u>Portuguese(Brazil):</u> Nick Marinho (nickmarinho@gmail.com)</li>\
-    <li><u>Romanian:</u> Obada Denis (obadadenis@users.sourceforge.net), Adrian Gabor Adriannho(Transifex) and Mihai Coman z0id(Transifex)</li>\
-    <li><u>Russian:</u> Nick Khazov (m2k3d0n at users.sourceforge.net), Alexey Morsov (samurai@ricom.ru), Nick Tiskov Dayman(daymansmail (at) gmail (dot) com), Dmitry DmitryKX(Transifex) and kraleksandr kraleksandr(Transifex)</li>\
-    <li><u>Serbian:</u> Anaximandar Milet (anaximandar@operamail.com)</li>\
-    <li><u>Slovak:</u>  helix84</li>\
-    <li><u>Spanish:</u> Alfredo Monclús (alfrix), Francisco Luque Contreras (frannoe@ya.com), José Antonio Moray moray33(Transifex) and Diego de las Heras(Transifex)</li>\
-    <li><u>Swedish:</u> Daniel Nylander (po@danielnylander.se) and Emil Hammarberg Ooglogput(Transifex)</li>\
-    <li><u>Turkish:</u> Hasan YILMAZ (iletisim@hedefturkce.com) and Erdem Bingöl (erdem84@gmail.com)</li>\
-    <li><u>Ukrainian:</u> Oleh Prypin (blaxpirit@gmail.com)</li>\
-    <li><u>Vietnamese:</u> Anh Phan ppanhh(Transifex)</li></ul>";
