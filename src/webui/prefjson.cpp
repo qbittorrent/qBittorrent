d9ef7c1b3282f35feb13c412ea0d46c68701c308
ngosang <diegodelasheras@gmail.com>
2015-07-08 09:06:41 +0200

    [Web UI] Reorder Options dialog code

--- src/webui/prefjson.cpp
+++ src/webui/prefjson.cpp
@@ -50,12 +50,14 @@ QByteArray prefjson::getPreferences()
 {
     const Preferences* const pref = Preferences::instance();
     QVariantMap data;
-    // UI
-    data["locale"] = pref->getLocale();
+
     // Downloads
+    // Hard Disk
     data["save_path"] = Utils::Fs::toNativePath(pref->getSavePath());
     data["temp_path_enabled"] = pref->isTempPathEnabled();
     data["temp_path"] = Utils::Fs::toNativePath(pref->getTempPath());
+    data["preallocate_all"] = pref->preAllocateAllFiles();
+    data["incomplete_files_ext"] = pref->useIncompleteFilesExtension();
     QVariantList l;
     foreach (const QString& s, pref->getScanDirs()) {
         l << Utils::Fs::toNativePath(s);
@@ -70,6 +72,7 @@ QByteArray prefjson::getPreferences()
     data["export_dir"] = Utils::Fs::toNativePath(pref->getTorrentExportDir());
     data["export_dir_fin_enabled"] = pref->isFinishedTorrentExportEnabled();
     data["export_dir_fin"] = Utils::Fs::toNativePath(pref->getFinishedTorrentExportDir());
+    // Email notification upon download completion
     data["mail_notification_enabled"] = pref->isMailNotificationEnabled();
     data["mail_notification_email"] = pref->getMailNotificationEmail();
     data["mail_notification_smtp"] = pref->getMailNotificationSMTP();
@@ -77,48 +80,21 @@ QByteArray prefjson::getPreferences()
     data["mail_notification_auth_enabled"] = pref->getMailNotificationSMTPAuth();
     data["mail_notification_username"] = pref->getMailNotificationSMTPUsername();
     data["mail_notification_password"] = pref->getMailNotificationSMTPPassword();
+    // Run an external program on torrent completion
     data["autorun_enabled"] = pref->isAutoRunEnabled();
     data["autorun_program"] = Utils::Fs::toNativePath(pref->getAutoRunProgram());
-    data["preallocate_all"] = pref->preAllocateAllFiles();
-    data["queueing_enabled"] = pref->isQueueingSystemEnabled();
-    data["max_active_downloads"] = pref->getMaxActiveDownloads();
-    data["max_active_torrents"] = pref->getMaxActiveTorrents();
-    data["max_active_uploads"] = pref->getMaxActiveUploads();
-    data["max_ratio_enabled"] = (pref->getGlobalMaxRatio() >= 0.);
-    data["max_ratio"] = pref->getGlobalMaxRatio();
-    data["max_ratio_act"] = pref->getMaxRatioAction();
-    data["dont_count_slow_torrents"] = pref->ignoreSlowTorrentsForQueueing();
-    data["incomplete_files_ext"] = pref->useIncompleteFilesExtension();
+
     // Connection
+    // Listening Port
     data["listen_port"] = pref->getSessionPort();
     data["upnp"] = pref->isUPnPEnabled();
     data["random_port"] = pref->useRandomPort();
-    data["dl_limit"] = pref->getGlobalDownloadLimit();
-    data["up_limit"] = pref->getGlobalUploadLimit();
+    // Connections Limits
     data["max_connec"] = pref->getMaxConnecs();
     data["max_connec_per_torrent"] = pref->getMaxConnecsPerTorrent();
     data["max_uploads"] = pref->getMaxUploads();
     data["max_uploads_per_torrent"] = pref->getMaxUploadsPerTorrent();
-    data["enable_utp"] = pref->isuTPEnabled();
-    data["limit_utp_rate"] = pref->isuTPRateLimited();
-    data["limit_tcp_overhead"] = pref->includeOverheadInLimits();
-    data["alt_dl_limit"] = pref->getAltGlobalDownloadLimit();
-    data["alt_up_limit"] = pref->getAltGlobalUploadLimit();
-    data["scheduler_enabled"] = pref->isSchedulerEnabled();
-    const QTime start_time = pref->getSchedulerStartTime();
-    data["schedule_from_hour"] = start_time.hour();
-    data["schedule_from_min"] = start_time.minute();
-    const QTime end_time = pref->getSchedulerEndTime();
-    data["schedule_to_hour"] = end_time.hour();
-    data["schedule_to_min"] = end_time.minute();
-    data["scheduler_days"] = pref->getSchedulerDays();
-    // Bittorrent
-    data["dht"] = pref->isDHTEnabled();
-    data["pex"] = pref->isPeXEnabled();
-    data["lsd"] = pref->isLSDEnabled();
-    data["encryption"] = pref->getEncryptionSetting();
-    data["anonymous_mode"] = pref->isAnonymousModeEnabled();
-    // Proxy
+    // Proxy Server
     data["proxy_type"] = pref->getProxyType();
     data["proxy_ip"] = pref->getProxyIp();
     data["proxy_port"] = pref->getProxyPort();
@@ -129,19 +105,61 @@ QByteArray prefjson::getPreferences()
     data["proxy_auth_enabled"] = pref->isProxyAuthEnabled();
     data["proxy_username"] = pref->getProxyUsername();
     data["proxy_password"] = pref->getProxyPassword();
-    // IP Filter
+    // IP Filtering
     data["ip_filter_enabled"] = pref->isFilteringEnabled();
     data["ip_filter_path"] = Utils::Fs::toNativePath(pref->getFilter());
     data["ip_filter_trackers"] = pref->isFilteringTrackerEnabled();
+
+    // Speed
+    // Global Rate Limits
+    data["dl_limit"] = pref->getGlobalDownloadLimit();
+    data["up_limit"] = pref->getGlobalUploadLimit();
+    data["enable_utp"] = pref->isuTPEnabled();
+    data["limit_utp_rate"] = pref->isuTPRateLimited();
+    data["limit_tcp_overhead"] = pref->includeOverheadInLimits();
+    data["alt_dl_limit"] = pref->getAltGlobalDownloadLimit();
+    data["alt_up_limit"] = pref->getAltGlobalUploadLimit();
+    // Scheduling
+    data["scheduler_enabled"] = pref->isSchedulerEnabled();
+    const QTime start_time = pref->getSchedulerStartTime();
+    data["schedule_from_hour"] = start_time.hour();
+    data["schedule_from_min"] = start_time.minute();
+    const QTime end_time = pref->getSchedulerEndTime();
+    data["schedule_to_hour"] = end_time.hour();
+    data["schedule_to_min"] = end_time.minute();
+    data["scheduler_days"] = pref->getSchedulerDays();
+
+    // Bittorrent
+    // Privacy
+    data["dht"] = pref->isDHTEnabled();
+    data["pex"] = pref->isPeXEnabled();
+    data["lsd"] = pref->isLSDEnabled();
+    data["encryption"] = pref->getEncryptionSetting();
+    data["anonymous_mode"] = pref->isAnonymousModeEnabled();
+    // Torrent Queueing
+    data["queueing_enabled"] = pref->isQueueingSystemEnabled();
+    data["max_active_downloads"] = pref->getMaxActiveDownloads();
+    data["max_active_torrents"] = pref->getMaxActiveTorrents();
+    data["max_active_uploads"] = pref->getMaxActiveUploads();
+    data["dont_count_slow_torrents"] = pref->ignoreSlowTorrentsForQueueing();
+    // Share Ratio Limiting
+    data["max_ratio_enabled"] = (pref->getGlobalMaxRatio() >= 0.);
+    data["max_ratio"] = pref->getGlobalMaxRatio();
+    data["max_ratio_act"] = pref->getMaxRatioAction();
+
     // Web UI
+    // Language
+    data["locale"] = pref->getLocale();
+    // HTTP Server
     data["web_ui_port"] = pref->getWebUiPort();
-    data["web_ui_username"] = pref->getWebUiUsername();
-    data["web_ui_password"] = pref->getWebUiPassword();
-    data["bypass_local_auth"] = !pref->isWebUiLocalAuthEnabled();
     data["use_https"] = pref->isWebUiHttpsEnabled();
     data["ssl_key"] = QString::fromLatin1(pref->getWebUiHttpsKey());
     data["ssl_cert"] = QString::fromLatin1(pref->getWebUiHttpsCertificate());
-    // DynDns
+    // Authentication
+    data["web_ui_username"] = pref->getWebUiUsername();
+    data["web_ui_password"] = pref->getWebUiPassword();
+    data["bypass_local_auth"] = !pref->isWebUiLocalAuthEnabled();
+    // Update my dynamic domain name
     data["dyndns_enabled"] = pref->isDynDNSEnabled();
     data["dyndns_service"] = pref->getDynDNSService();
     data["dyndns_username"] = pref->getDynDNSUsername();
@@ -153,31 +171,21 @@ QByteArray prefjson::getPreferences()
 
 void prefjson::setPreferences(const QString& json)
 {
+    Preferences* const pref = Preferences::instance();
     const QVariantMap m = json::fromJson(json).toMap();
 
-    // UI
-    Preferences* const pref = Preferences::instance();
-    if (m.contains("locale")) {
-        QString locale = m["locale"].toString();
-        if (pref->getLocale() != locale) {
-            QTranslator *translator = new QTranslator;
-            if (translator->load(QString::fromUtf8(":/lang/qbittorrent_") + locale)) {
-                qDebug("%s locale recognized, using translation.", qPrintable(locale));
-            }else{
-                qDebug("%s locale unrecognized, using default (en).", qPrintable(locale));
-            }
-            qApp->installTranslator(translator);
-
-            pref->setLocale(locale);
-        }
-    }
     // Downloads
+    // Hard Disk
     if (m.contains("save_path"))
         pref->setSavePath(m["save_path"].toString());
     if (m.contains("temp_path_enabled"))
         pref->setTempPathEnabled(m["temp_path_enabled"].toBool());
     if (m.contains("temp_path"))
         pref->setTempPath(m["temp_path"].toString());
+    if (m.contains("preallocate_all"))
+        pref->preAllocateAllFiles(m["preallocate_all"].toBool());
+    if (m.contains("incomplete_files_ext"))
+        pref->useIncompleteFilesExtension(m["incomplete_files_ext"].toBool());
     if (m.contains("scan_dirs") && m.contains("download_in_scan_dirs")) {
         QVariantList download_at_path_tmp = m["download_in_scan_dirs"].toList();
         QList<bool> download_at_path;
@@ -210,6 +218,7 @@ void prefjson::setPreferences(const QString& json)
         pref->setTorrentExportDir(m["export_dir"].toString());
     if (m.contains("export_dir_fin"))
         pref->setFinishedTorrentExportDir(m["export_dir_fin"].toString());
+    // Email notification upon download completion
     if (m.contains("mail_notification_enabled"))
         pref->setMailNotificationEnabled(m["mail_notification_enabled"].toBool());
     if (m.contains("mail_notification_email"))
@@ -224,41 +233,21 @@ void prefjson::setPreferences(const QString& json)
         pref->setMailNotificationSMTPUsername(m["mail_notification_username"].toString());
     if (m.contains("mail_notification_password"))
         pref->setMailNotificationSMTPPassword(m["mail_notification_password"].toString());
+    // Run an external program on torrent completion
     if (m.contains("autorun_enabled"))
         pref->setAutoRunEnabled(m["autorun_enabled"].toBool());
     if (m.contains("autorun_program"))
         pref->setAutoRunProgram(m["autorun_program"].toString());
-    if (m.contains("preallocate_all"))
-        pref->preAllocateAllFiles(m["preallocate_all"].toBool());
-    if (m.contains("queueing_enabled"))
-        pref->setQueueingSystemEnabled(m["queueing_enabled"].toBool());
-    if (m.contains("max_active_downloads"))
-        pref->setMaxActiveDownloads(m["max_active_downloads"].toInt());
-    if (m.contains("max_active_torrents"))
-        pref->setMaxActiveTorrents(m["max_active_torrents"].toInt());
-    if (m.contains("max_active_uploads"))
-        pref->setMaxActiveUploads(m["max_active_uploads"].toInt());
-    if (m.contains("max_ratio_enabled"))
-        pref->setGlobalMaxRatio(m["max_ratio"].toInt());
-    else
-        pref->setGlobalMaxRatio(-1);
-    if (m.contains("max_ratio_act"))
-        pref->setMaxRatioAction(m["max_ratio_act"].toInt());
-    if (m.contains("dont_count_slow_torrents"))
-        pref->setIgnoreSlowTorrentsForQueueing(m["dont_count_slow_torrents"].toBool());
-    if (m.contains("incomplete_files_ext"))
-        pref->useIncompleteFilesExtension(m["incomplete_files_ext"].toBool());
+
     // Connection
+    // Listening Port
     if (m.contains("listen_port"))
         pref->setSessionPort(m["listen_port"].toInt());
     if (m.contains("upnp"))
         pref->setUPnPEnabled(m["upnp"].toBool());
     if (m.contains("random_port"))
         pref->setRandomPort(m["random_port"].toBool());
-    if (m.contains("dl_limit"))
-        pref->setGlobalDownloadLimit(m["dl_limit"].toInt());
-    if (m.contains("up_limit"))
-        pref->setGlobalUploadLimit(m["up_limit"].toInt());
+    // Connections Limits
     if (m.contains("max_connec"))
         pref->setMaxConnecs(m["max_connec"].toInt());
     if (m.contains("max_connec_per_torrent"))
@@ -267,41 +256,7 @@ void prefjson::setPreferences(const QString& json)
         pref->setMaxUploads(m["max_uploads"].toInt());
     if (m.contains("max_uploads_per_torrent"))
         pref->setMaxUploadsPerTorrent(m["max_uploads_per_torrent"].toInt());
-    if (m.contains("enable_utp"))
-        pref->setuTPEnabled(m["enable_utp"].toBool());
-    if (m.contains("limit_utp_rate"))
-        pref->setuTPRateLimited(m["limit_utp_rate"].toBool());
-    if (m.contains("limit_tcp_overhead"))
-        pref->includeOverheadInLimits(m["limit_tcp_overhead"].toBool());
-    if (m.contains("alt_dl_limit"))
-        pref->setAltGlobalDownloadLimit(m["alt_dl_limit"].toInt());
-    if (m.contains("alt_up_limit"))
-        pref->setAltGlobalUploadLimit(m["alt_up_limit"].toInt());
-    if (m.contains("scheduler_enabled"))
-        pref->setSchedulerEnabled(m["scheduler_enabled"].toBool());
-    if (m.contains("schedule_from_hour") && m.contains("schedule_from_min")) {
-        pref->setSchedulerStartTime(QTime(m["schedule_from_hour"].toInt(),
-                                                                         m["schedule_from_min"].toInt()));
-    }
-    if (m.contains("schedule_to_hour") && m.contains("schedule_to_min")) {
-        pref->setSchedulerEndTime(QTime(m["schedule_to_hour"].toInt(),
-                                                                     m["schedule_to_min"].toInt()));
-    }
-    if (m.contains("scheduler_days"))
-        pref->setSchedulerDays(scheduler_days(m["scheduler_days"].toInt()));
-    // Bittorrent
-    if (m.contains("dht"))
-        pref->setDHTEnabled(m["dht"].toBool());
-    if (m.contains("pex"))
-        pref->setPeXEnabled(m["pex"].toBool());
-    qDebug("Pex support: %d", (int)m["pex"].toBool());
-    if (m.contains("lsd"))
-        pref->setLSDEnabled(m["lsd"].toBool());
-    if (m.contains("encryption"))
-        pref->setEncryptionSetting(m["encryption"].toInt());
-    if (m.contains("anonymous_mode"))
-        pref->enableAnonymousMode(m["anonymous_mode"].toBool());
-    // Proxy
+    // Proxy Server
     if (m.contains("proxy_type"))
         pref->setProxyType(m["proxy_type"].toInt());
     if (m.contains("proxy_ip"))
@@ -320,22 +275,94 @@ void prefjson::setPreferences(const QString& json)
         pref->setProxyUsername(m["proxy_username"].toString());
     if (m.contains("proxy_password"))
         pref->setProxyPassword(m["proxy_password"].toString());
-    // IP Filter
+    // IP Filtering
     if (m.contains("ip_filter_enabled"))
         pref->setFilteringEnabled(m["ip_filter_enabled"].toBool());
     if (m.contains("ip_filter_path"))
         pref->setFilter(m["ip_filter_path"].toString());
     if (m.contains("ip_filter_trackers"))
         pref->setFilteringTrackerEnabled(m["ip_filter_trackers"].toBool());
+
+    // Speed
+    // Global Rate Limits
+    if (m.contains("dl_limit"))
+        pref->setGlobalDownloadLimit(m["dl_limit"].toInt());
+    if (m.contains("up_limit"))
+        pref->setGlobalUploadLimit(m["up_limit"].toInt());
+    if (m.contains("enable_utp"))
+        pref->setuTPEnabled(m["enable_utp"].toBool());
+    if (m.contains("limit_utp_rate"))
+        pref->setuTPRateLimited(m["limit_utp_rate"].toBool());
+    if (m.contains("limit_tcp_overhead"))
+        pref->includeOverheadInLimits(m["limit_tcp_overhead"].toBool());
+    if (m.contains("alt_dl_limit"))
+        pref->setAltGlobalDownloadLimit(m["alt_dl_limit"].toInt());
+    if (m.contains("alt_up_limit"))
+        pref->setAltGlobalUploadLimit(m["alt_up_limit"].toInt());
+    // Scheduling
+    if (m.contains("scheduler_enabled"))
+        pref->setSchedulerEnabled(m["scheduler_enabled"].toBool());
+    if (m.contains("schedule_from_hour") && m.contains("schedule_from_min")) {
+        pref->setSchedulerStartTime(QTime(m["schedule_from_hour"].toInt(),
+                                                                         m["schedule_from_min"].toInt()));
+    }
+    if (m.contains("schedule_to_hour") && m.contains("schedule_to_min")) {
+        pref->setSchedulerEndTime(QTime(m["schedule_to_hour"].toInt(),
+                                                                     m["schedule_to_min"].toInt()));
+    }
+    if (m.contains("scheduler_days"))
+        pref->setSchedulerDays(scheduler_days(m["scheduler_days"].toInt()));
+
+    // Bittorrent
+    // Privacy
+    if (m.contains("dht"))
+        pref->setDHTEnabled(m["dht"].toBool());
+    if (m.contains("pex"))
+        pref->setPeXEnabled(m["pex"].toBool());
+    if (m.contains("lsd"))
+        pref->setLSDEnabled(m["lsd"].toBool());
+    if (m.contains("encryption"))
+        pref->setEncryptionSetting(m["encryption"].toInt());
+    if (m.contains("anonymous_mode"))
+        pref->enableAnonymousMode(m["anonymous_mode"].toBool());
+    // Torrent Queueing
+    if (m.contains("queueing_enabled"))
+        pref->setQueueingSystemEnabled(m["queueing_enabled"].toBool());
+    if (m.contains("max_active_downloads"))
+        pref->setMaxActiveDownloads(m["max_active_downloads"].toInt());
+    if (m.contains("max_active_torrents"))
+        pref->setMaxActiveTorrents(m["max_active_torrents"].toInt());
+    if (m.contains("max_active_uploads"))
+        pref->setMaxActiveUploads(m["max_active_uploads"].toInt());
+    if (m.contains("dont_count_slow_torrents"))
+        pref->setIgnoreSlowTorrentsForQueueing(m["dont_count_slow_torrents"].toBool());
+    // Share Ratio Limiting
+    if (m.contains("max_ratio_enabled"))
+        pref->setGlobalMaxRatio(m["max_ratio"].toInt());
+    else
+        pref->setGlobalMaxRatio(-1);
+    if (m.contains("max_ratio_act"))
+        pref->setMaxRatioAction(m["max_ratio_act"].toInt());
+
     // Web UI
+    // Language
+    if (m.contains("locale")) {
+        QString locale = m["locale"].toString();
+        if (pref->getLocale() != locale) {
+            QTranslator *translator = new QTranslator;
+            if (translator->load(QString::fromUtf8(":/lang/qbittorrent_") + locale)) {
+                qDebug("%s locale recognized, using translation.", qPrintable(locale));
+            }else{
+                qDebug("%s locale unrecognized, using default (en).", qPrintable(locale));
+            }
+            qApp->installTranslator(translator);
+
+            pref->setLocale(locale);
+        }
+    }
+    // HTTP Server
     if (m.contains("web_ui_port"))
         pref->setWebUiPort(m["web_ui_port"].toUInt());
-    if (m.contains("web_ui_username"))
-        pref->setWebUiUsername(m["web_ui_username"].toString());
-    if (m.contains("web_ui_password"))
-        pref->setWebUiPassword(m["web_ui_password"].toString());
-    if (m.contains("bypass_local_auth"))
-        pref->setWebUiLocalAuthEnabled(!m["bypass_local_auth"].toBool());
     if (m.contains("use_https"))
         pref->setWebUiHttpsEnabled(m["use_https"].toBool());
 #ifndef QT_NO_OPENSSL
@@ -350,7 +377,14 @@ void prefjson::setPreferences(const QString& json)
             pref->setWebUiHttpsCertificate(raw_cert);
     }
 #endif
-    // Dyndns
+    // Authentication
+    if (m.contains("web_ui_username"))
+        pref->setWebUiUsername(m["web_ui_username"].toString());
+    if (m.contains("web_ui_password"))
+        pref->setWebUiPassword(m["web_ui_password"].toString());
+    if (m.contains("bypass_local_auth"))
+        pref->setWebUiLocalAuthEnabled(!m["bypass_local_auth"].toBool());
+    // Update my dynamic domain name
     if (m.contains("dyndns_enabled"))
         pref->setDynDNSEnabled(m["dyndns_enabled"].toBool());
     if (m.contains("dyndns_service"))
@@ -361,6 +395,7 @@ void prefjson::setPreferences(const QString& json)
         pref->setDynDNSPassword(m["dyndns_password"].toString());
     if (m.contains("dyndns_domain"))
         pref->setDynDomainName(m["dyndns_domain"].toString());
+
     // Save preferences
     pref->apply();
 }
