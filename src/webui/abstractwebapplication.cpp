8bb2e98b90e3181e7c32d2775c0bbfcaabd2a101
Chocobo1 <Chocobo1@users.noreply.github.com>
2015-07-19 14:45:47 +0800

    Fix localhost address (::ffff:127.0.0.1) is not recognized when connecting to WebUI

--- src/webui/abstractwebapplication.cpp
+++ src/webui/abstractwebapplication.cpp
@@ -316,7 +316,8 @@ void AbstractWebApplication::increaseFailedAttempts()
 bool AbstractWebApplication::isAuthNeeded()
 {
     return (env_.clientAddress != QHostAddress::LocalHost
-            && env_.clientAddress != QHostAddress::LocalHostIPv6)
+            && env_.clientAddress != QHostAddress::LocalHostIPv6
+            && env_.clientAddress != QHostAddress("::ffff:127.0.0.1"))
             || Preferences::instance()->isWebUiLocalAuthEnabled();
 }
 
