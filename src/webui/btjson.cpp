SysAdminDoc/qBittorrent-Vanced

[Web UI] Fix an error in Content tab when the torrent doesn't have metadata

src/webui/btjson.cpp
@@ -481,7 +481,7 @@ QByteArray btjson::getFilesForTorrent(const QString& hash)
     }
 
     if (!torrent->hasMetadata())
-        return QByteArray();
+        return json::toJson(fileList);
 
     const QVector<int> priorities = torrent->filePriorities();
     QVector<qreal> fp = torrent->filesProgress();

qbittorrent/qBittorrent

Conflicts:
	src/webui/btjson.cpp

src/webui/btjson.cpp
@@ -502,7 +502,7 @@ QByteArray btjson::getFilesForTorrent(const QString& hash)
     try {
         QTorrentHandle h = QBtSession::instance()->getTorrentHandle(hash);
         if (!h.has_metadata())
-            return QByteArray();
+            return json::toJson(file_list);
 
         const std::vector<int> priorities = h.file_priorities();
         std::vector<size_type> fp;