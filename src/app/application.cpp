c38987fc03a403f7b4af9a2408e5801618ff7f34
Vladimir Golovnev (Glassez) <glassez@yandex.ru>
2015-11-01 16:57:20 +0300

    Provide torrent paths to external program.

--- src/app/application.cpp
+++ src/app/application.cpp
@@ -142,13 +142,12 @@ void Application::torrentFinished(BitTorrent::TorrentHandle *const torrent)
     // AutoRun program
     if (pref->isAutoRunEnabled()) {
         QString program = pref->getAutoRunProgram();
-        int fileCount = torrent->filesCount();
 
         program.replace("%N", torrent->name());
-        program.replace("%F", (fileCount > 1) ? "" : torrent->fileName(0));
         program.replace("%L", torrent->label());
+        program.replace("%F", Utils::Fs::toNativePath(torrent->contentPath()));
+        program.replace("%R", Utils::Fs::toNativePath(torrent->rootPath()));
         program.replace("%D", Utils::Fs::toNativePath(torrent->savePath()));
-        program.replace("%K", (fileCount > 1) ? "multi" : "single");
         program.replace("%C", QString::number(torrent->filesCount()));
         program.replace("%Z", QString::number(torrent->totalSize()));
         program.replace("%T", torrent->currentTracker());
