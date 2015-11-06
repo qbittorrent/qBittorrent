64daecb2667dcefe9c5e6515b5dafb196ac2e13c
Eugene Shalygin <eugene.shalygin@googlemail.com>
2015-11-06 19:03:18 +0100

    add basic cmake support

---
+++ cmake/Modules/FindQtSingleApplication.cmake
@@ -0,0 +1,76 @@
+# - Try to find the QtSingleApplication includes and library
+# which defines
+#
+# QTSINGLEAPPLICATION_FOUND - system has QtSingleApplication
+# QTSINGLEAPPLICATION_INCLUDE_DIR - where to find header QtSingleApplication
+# QTSINGLEAPPLICATION_LIBRARIES - the libraries to link against to use QtSingleApplication
+# QTSINGLEAPPLICATION_LIBRARY - where to find the QtSingleApplication library (not for general use)
+
+# copyright (c) 2013 TI_Eugene ti.eugene@gmail.com
+#
+# Redistribution and use is allowed according to the terms of the FreeBSD license.
+
+SET(QTSINGLEAPPLICATION_FOUND FALSE)
+
+IF(QT4_FOUND)
+    FIND_PATH(QTSINGLEAPPLICATION_INCLUDE_DIR QtSingleApplication
+                # standard locations
+                /usr/include
+                /usr/include/QtSolutions
+                # qt4 location except mac's frameworks
+                "${QT_INCLUDE_DIR}/QtSolutions"
+                # mac's frameworks
+                ${FRAMEWORK_INCLUDE_DIR}/QtSolutions
+    )
+
+    SET(QTSINGLEAPPLICATION_NAMES ${QTSINGLEAPPLICATION_NAMES} QtSolutions_SingleApplication-2.6 libQtSolutions_SingleApplication-2.6)
+    FIND_LIBRARY(QTSINGLEAPPLICATION_LIBRARY
+        NAMES ${QTSINGLEAPPLICATION_NAMES}
+        PATHS ${QT_LIBRARY_DIR}
+    )
+ELSEIF(Qt5Widgets_FOUND)
+    FOREACH(TOP_INCLUDE_PATH in ${Qt5Widgets_INCLUDE_DIRS} ${FRAMEWORK_INCLUDE_DIR})
+        FIND_PATH(QTSINGLEAPPLICATION_INCLUDE_DIR QtSingleApplication ${TOP_INCLUDE_PATH}/QtSolutions)
+
+        IF(QTSINGLEAPPLICATION_INCLUDE_DIR)
+            BREAK()
+        ENDIF()
+    ENDFOREACH()
+
+    SET(QTSINGLEAPPLICATION_NAMES ${QTSINGLEAPPLICATION_NAMES} QtSolutions_SingleApplication-2.6 libQtSolutions_SingleApplication-2.6)
+    GET_TARGET_PROPERTY(QT5_WIDGETSLIBRARY Qt5::Widgets LOCATION)
+    GET_FILENAME_COMPONENT(QT5_WIDGETSLIBRARYPATH ${QT5_WIDGETSLIBRARY} PATH)
+
+    FIND_LIBRARY(QTSINGLEAPPLICATION_LIBRARY
+        NAMES ${QTSINGLEAPPLICATION_NAMES}
+        PATHS ${QT5_WIDGETSLIBRARYPATH}
+    )
+ENDIF()
+
+IF (QTSINGLEAPPLICATION_LIBRARY AND QTSINGLEAPPLICATION_INCLUDE_DIR)
+
+    SET(QTSINGLEAPPLICATION_LIBRARIES ${QTSINGLEAPPLICATION_LIBRARY})
+    SET(QTSINGLEAPPLICATION_FOUND TRUE)
+
+    IF (CYGWIN)
+        IF(BUILD_SHARED_LIBS)
+        # No need to define QTSINGLEAPPLICATION_USE_DLL here, because it's default for Cygwin.
+        ELSE(BUILD_SHARED_LIBS)
+        SET (QTSINGLEAPPLICATION_DEFINITIONS -DQTSINGLEAPPLICATION_STATIC)
+        ENDIF(BUILD_SHARED_LIBS)
+    ENDIF (CYGWIN)
+
+ENDIF (QTSINGLEAPPLICATION_LIBRARY AND QTSINGLEAPPLICATION_INCLUDE_DIR)
+
+IF (QTSINGLEAPPLICATION_FOUND)
+  IF (NOT QtSingleApplication_FIND_QUIETLY)
+    MESSAGE(STATUS "Found QtSingleApplication: ${QTSINGLEAPPLICATION_LIBRARY}")
+    MESSAGE(STATUS "         includes: ${QTSINGLEAPPLICATION_INCLUDE_DIR}")
+  ENDIF (NOT QtSingleApplication_FIND_QUIETLY)
+ELSE (QTSINGLEAPPLICATION_FOUND)
+  IF (QtSingleApplication_FIND_REQUIRED)
+    MESSAGE(FATAL_ERROR "Could not find QtSingleApplication library")
+  ENDIF (QtSingleApplication_FIND_REQUIRED)
+ENDIF (QTSINGLEAPPLICATION_FOUND)
+
+MARK_AS_ADVANCED(QTSINGLEAPPLICATION_INCLUDE_DIR QTSINGLEAPPLICATION_LIBRARY)
---
+++ cmake/Modules/winconf.cmake
@@ -0,0 +1,86 @@
+# Settings for compiling qBittorrent on Windows
+
+# We want to link with static version of
+# libtorrent
+set(LibtorrentRasterbar_USE_STATIC_LIBS True)
+set(LibtorrentRasterbar_CUSTOM_DEFINITIONS 
+    -DBOOST_ALL_NO_LIB -DBOOST_ASIO_HASH_MAP_BUCKETS=1021
+    -DBOOST_ASIO_SEPARATE_COMPILATION
+    -DBOOST_EXCEPTION_DISABLE
+    -DBOOST_SYSTEM_STATIC_LINK=1
+    -DTORRENT_USE_OPENSSL
+    -DUNICODE
+    -D_UNICODE
+    -DWIN32
+    -D_WIN32
+    -DWIN32_LEAN_AND_MEAN
+    -D_WIN32_WINNT=0x0501
+    -D_WIN32_IE=0x0500
+    -D_CRT_SECURE_NO_DEPRECATE
+    -D_SCL_SECURE_NO_DEPRECATE
+    -D__USE_W32_SOCKETS
+    -D_FILE_OFFSET_BITS=64)
+
+# and boost
+set(Boost_USE_STATIC_LIBS  True)
+# set(Boost_USE_STATIC_RUNTIME True)
+
+# Here we assume that all required libraries are installed into the same prefix
+# with usual unix subdirectories (bin, lib, include)
+# if so, we just need to set CMAKE_SYSTEM_PREFIX_PATH
+# If it is not the case, individual paths need to be specified manually (see below)
+set(COMMON_INSTALL_PREFIX "c:/usr")
+list(APPEND CMAKE_SYSTEM_PREFIX_PATH "${COMMON_INSTALL_PREFIX}")
+
+# If two version of Qt are installed, separate prefixes are needed most likely
+set(QT4_INSTALL_PREFIX "${COMMON_INSTALL_PREFIX}/lib/qt4")
+set(QT5_INSTALL_PREFIX "${COMMON_INSTALL_PREFIX}/lib/qt5")
+
+# it is safe to set Qt dirs even if their files are directly in the prefix
+# Qt4
+if(NOT QT5)
+    LIST(APPEND CMAKE_PROGRAM_PATH  "${QT4_INSTALL_PREFIX}/bin/")
+endif(NOT QT5)
+
+# Qt5
+set(Qt5_DIR "${QT5_INSTALL_PREFIX}/lib/cmake/Qt5")
+
+# And now we can set specific values for the Boost and libtorrent libraries.
+# The following values are generated from the paths listed above just for an example
+# they have to be set to actual locations
+
+# Boost
+# set(BOOST_ROOT "${COMMON_INSTALL_PREFIX}")
+# set(Boost_version_suffix "1_59")
+# if a link like boost-version/boost -> boost was created or the boost directory was renamed in the same way,
+# the following needs adjustment
+# set(BOOST_INCLUDEDIR "${COMMON_INSTALL_PREFIX}/include/boost-${Boost_version_suffix}")
+# set(BOOST_LIBRARYDIR "${COMMON_INSTALL_PREFIX}/lib/")
+
+# libtorrent
+
+# set(PC_LIBTORRENT_RASTERBAR_INCLUDEDIR "${COMMON_INSTALL_PREFIX}")
+# set(PC_LIBTORRENT_RASTERBAR_LIBDIR "${COMMON_INSTALL_PREFIX}/lib")
+
+set(AUTOGEN_TARGETS_FOLDER "generated")
+
+set(CMAKE_INSTALL_BINDIR ".")
+
+# Test 32/64 bits
+if("${CMAKE_SIZEOF_VOID_P}" EQUAL "8")
+    message(STATUS "Target is 64 bits")
+    if (WIN32)
+        set(WINXXBITS Win64)
+    endif(WIN32)
+else("${CMAKE_SIZEOF_VOID_P}" EQUAL "8")
+    message(STATUS "Target is 32 bits")
+    if (WIN32)
+        set(WINXXBITS Win32)
+    endif(WIN32)
+endif("${CMAKE_SIZEOF_VOID_P}" EQUAL "8")
+
+if (MSVC)
+    include(winconf-msvc)
+else (MSVC)
+    include(winconf-mingw)
+endif (MSVC)
