3e6d76245c9d06638db311cf7afbc45b1b45d835
sledgehammer999 <hammered999@gmail.com>
2015-11-22 22:03:17 +0200

    Bump WebUI API_VERSION.

--- src/webui/webapplication.cpp
+++ src/webui/webapplication.cpp
@@ -47,7 +47,7 @@
 #include "websessiondata.h"
 #include "webapplication.h"
 
-static const int API_VERSION = 5;
+static const int API_VERSION = 6;
 static const int API_VERSION_MIN = 2;
 
 const QString WWW_FOLDER = ":/www/public/";
