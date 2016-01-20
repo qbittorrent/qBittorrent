9c45166b7d6ec067aedc47217e615b67b4810af4
jpd <jd@discordant.org>
2016-01-20 00:43:23 +0000

    Remove logging of successful fast resumes.

--- src/base/bittorrent/session.cpp
+++ src/base/bittorrent/session.cpp
@@ -2088,9 +2088,6 @@ void Session::handleAddTorrentAlert(libtorrent::add_torrent_alert *p)
     if (data.resumed) {
         if (fromMagnetUri && !data.addPaused)
             torrent->resume(data.addForced);
-
-        logger->addMessage(tr("'%1' resumed. (fast resume)", "'torrent name' was resumed. (fast resume)")
-                           .arg(torrent->name()));
     }
     else {
         qDebug("This is a NEW torrent (first time)...");
