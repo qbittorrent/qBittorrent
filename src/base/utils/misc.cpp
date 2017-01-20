0517a3eb638cb9643ec84f03e7267fd7d7e1f219
Chocobo1 <Chocobo1@users.noreply.github.com>
2017-01-20 13:43:57 +0800

    Fix rss icon too large in rss settings dialog
    Give a name to the rss icon (in .ui file)
    Add helper function: Utils::Misc::largeIconSize()
    Group functions under the same #ifdef

--- src/base/utils/misc.cpp
+++ src/base/utils/misc.cpp
@@ -611,6 +611,19 @@ void Utils::Misc::openFolderSelect(const QString &absolutePath)
 #endif
 }
 
+QSize Utils::Misc::smallIconSize()
+{
+    // Get DPI scaled icon size (device-dependent), see QT source
+    int s = QApplication::style()->pixelMetric(QStyle::PM_SmallIconSize);
+    return QSize(s, s);
+}
+
+QSize Utils::Misc::largeIconSize()
+{
+    // Get DPI scaled icon size (device-dependent), see QT source
+    int s = QApplication::style()->pixelMetric(QStyle::PM_LargeIconSize);
+    return QSize(s, s);
+}
 #endif // DISABLE_GUI
 
 namespace
@@ -630,16 +643,6 @@ void Utils::Misc::msleep(unsigned long msecs)
 {
     SleeperThread::msleep(msecs);
 }
-
-#ifndef DISABLE_GUI
-QSize Utils::Misc::smallIconSize()
-{
-    // Get DPI scaled icon size (device-dependent), see QT source
-    int s = QApplication::style()->pixelMetric(QStyle::PM_SmallIconSize);
-    return QSize(s, s);
-}
-
-#endif
 
 QString Utils::Misc::osName()
 {
