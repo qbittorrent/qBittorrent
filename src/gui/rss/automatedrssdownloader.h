f96eb587ff8bff255e8692f91560be089714dded
Tim Delaney <timothy.c.delaney@gmail.com>
2017-01-22 15:36:27 +1100

    Bugfix RSS feed list and rules editor. Closes #3782, #6281.
    --HG--
    branch : magao-dev

--- src/gui/rss/automatedrssdownloader.h
+++ src/gui/rss/automatedrssdownloader.h
@@ -32,8 +32,13 @@
 #define AUTOMATEDRSSDOWNLOADER_H
 
 #include <QDialog>
+#include <QHideEvent>
+#include <QPair>
 #include <QRegExpValidator>
+#include <QSet>
 #include <QShortcut>
+#include <QShowEvent>
+#include <QString>
 #include <QWeakPointer>
 
 #include "base/rss/rssdownloadrule.h"
@@ -64,17 +69,21 @@ public:
     ~AutomatedRssDownloader();
     bool isRssDownloaderEnabled() const;
 
+protected:
+    virtual void showEvent(QShowEvent *event) override;
+    virtual void hideEvent(QHideEvent *event) override;
+
 protected slots:
     void loadSettings();
     void saveSettings();
     void loadRulesList();
     void handleRuleCheckStateChange(QListWidgetItem *rule_item);
     void handleFeedCheckStateChange(QListWidgetItem *feed_item);
-    void updateRuleDefinitionBox();
+    void updateRuleDefinitionBox(QListWidgetItem *selected = 0);
     void clearRuleDefinitionBox();
     void saveEditedRule();
     void loadFeedList();
-    void updateFeedList();
+    void updateFeedList(QListWidgetItem *selected = 0);
 
 private slots:
     void displayRulesListMenu(const QPoint &pos);
@@ -95,6 +104,8 @@ private:
     Rss::DownloadRulePtr getCurrentRule() const;
     void initCategoryCombobox();
     void addFeedArticlesToTree(const Rss::FeedPtr &feed, const QStringList &articles);
+    void disconnectRuleFeedSlots();
+    void connectRuleFeedSlots();
 
 private:
     Ui::AutomatedRssDownloader *ui;
@@ -105,6 +116,7 @@ private:
     QRegExp *m_episodeRegex;
     QShortcut *editHotkey;
     QShortcut *deleteHotkey;
+    QSet<QPair<QString, QString >> m_treeListEntries;
 };
 
 #endif // AUTOMATEDRSSDOWNLOADER_H
