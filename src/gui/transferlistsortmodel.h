c8f955e3020689eef258fb55dd4642b0f72e8c0d
sledgehammer999 <hammered999@gmail.com>
2017-02-06 18:28:49 +0200

    Fix previous commit.

--- src/gui/transferlistsortmodel.h
+++ src/gui/transferlistsortmodel.h
@@ -52,7 +52,7 @@ public:
 private:
     bool lessThan(const QModelIndex &left, const QModelIndex &right) const;
     bool lowerPositionThan(const QModelIndex &left, const QModelIndex &right) const;
-    bool dateLessThan(const int dateColumn, const QModelIndex &left, const QModelIndex &right) const;
+    bool dateLessThan(const int dateColumn, const QModelIndex &left, const QModelIndex &right, bool sortInvalidInBottom) const;
     bool filterAcceptsRow(int sourceRow, const QModelIndex &sourceParent) const;
     bool matchFilter(int sourceRow, const QModelIndex &sourceParent) const;
 
