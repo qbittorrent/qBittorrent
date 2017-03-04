3480d3d10c892588c13dc384b88df0ead9621e95
Chocobo1 <Chocobo1@users.noreply.github.com>
2017-03-04 15:03:39 +0800

    Fix incomplete type compile error with Qt4

--- src/base/http/server.cpp
+++ src/base/http/server.cpp
@@ -28,14 +28,18 @@
  * exception statement from your version.
  */
 
+#include "server.h"
+
+#include <QNetworkProxy>
+#include <QStringList>
+
 #ifndef QT_NO_OPENSSL
 #include <QSslSocket>
 #else
 #include <QTcpSocket>
 #endif
-#include <QNetworkProxy>
+
 #include "connection.h"
-#include "server.h"
 
 using namespace Http;
 
