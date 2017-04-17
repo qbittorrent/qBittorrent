75f3dd6d1d610f5a24c5675c675491c980493a6a
Chocobo1 <Chocobo1@users.noreply.github.com>
2017-04-17 16:52:43 +0800

    Revise Utils::Gzip::compress code
    Change signature
    Add ZLIB_CONST define to make  z_stream.next_in const
    Cast to zlib defined type Bytef*
    Set memLevel to 9 in deflateInit2() for maximum performance
    Revise compression loop
    On returning false, free memory correctly by calling deflateEnd()
    Reserve space by the estimation of deflateBound()

--- src/base/utils/gzip.h
+++ src/base/utils/gzip.h
@@ -36,7 +36,7 @@ namespace Utils
 {
     namespace Gzip
     {
-        bool compress(QByteArray src, QByteArray &dest);
+        QByteArray compress(const QByteArray &data, int level = 6, bool *ok = nullptr);
         bool uncompress(QByteArray src, QByteArray &dest);
     }
 }
