2d3914c0778095e8b9bbaa1093a98f013acbcb53
Vladimir Golovnev (Glassez) <glassez@yandex.ru>
2017-05-04 12:39:59 +0300

    Prevent incorrect popullating of Unread RSS list

--- src/gui/rss/feedlistwidget.cpp
+++ src/gui/rss/feedlistwidget.cpp
@@ -138,6 +138,11 @@ void FeedListWidget::handleItemPathChanged(RSS::Item *rssItem)
 void FeedListWidget::handleItemAboutToBeRemoved(RSS::Item *rssItem)
 {
     delete m_rssToTreeItemMapping.take(rssItem);
+
+    // RSS Item is still valid in this slot so if it is the last
+    // item we should prevent Unread list populating
+    if (m_rssToTreeItemMapping.size() == 1)
+        setCurrentItem(nullptr);
 }
 
 QTreeWidgetItem *FeedListWidget::stickyUnreadItem() const
