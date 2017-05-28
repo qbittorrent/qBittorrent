6e9f66eb184699fb5bbcf33aa6276056e61e8f63
Travis C Howard <TravisCHoward@users.noreply.github.com>
2017-05-28 12:46:19 -0400

    Check for localhost auth bypass
    Don't increase the failed authentication attempts counter if "Bypass authentication for localhost" is enabled.
    
    This prevents "Your IP address has been banned after too many failed authentication attempts" if apps accessing the WebUI API try to auth with an incorrect or blank username and password while localhost authentication is bypassed.

--- src/webui/abstractwebapplication.cpp
+++ src/webui/abstractwebapplication.cpp
@@ -322,7 +322,9 @@ void AbstractWebApplication::resetFailedAttempts()
 
 void AbstractWebApplication::increaseFailedAttempts()
 {
-    const int nb_fail = clientFailedAttempts_.value(env_.clientAddress, 0) + 1;
+    if (!isAuthNeeded()) {
+        const int nb_fail = clientFailedAttempts_.value(env_.clientAddress, 0) + 1;
+    }
 
     clientFailedAttempts_[env_.clientAddress] = nb_fail;
     if (nb_fail == MAX_AUTH_FAILED_ATTEMPTS) {
