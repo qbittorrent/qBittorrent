3f00a6e5e306eabafa4e64c4f87f184912e28014
Vladimir Golovnev (qlassez) <glassez@yandex.ru>
2017-08-15 16:17:57 +0300

    Improve utils/fs.* and fix coding style

--- src/base/utils/fs.h
+++ src/base/utils/fs.h
@@ -41,25 +41,25 @@ namespace Utils
 {
     namespace Fs
     {
-        QString toNativePath(const QString& path);
-        QString fromNativePath(const QString& path);
-        QString fileExtension(const QString& filename);
-        QString fileName(const QString& file_path);
-        QString folderName(const QString& file_path);
-        qint64 computePathSize(const QString& path);
-        bool sameFiles(const QString& path1, const QString& path2);
+        QString toNativePath(const QString &path);
+        QString fromNativePath(const QString &path);
+        QString fileExtension(const QString &filename);
+        QString fileName(const QString &filePath);
+        QString folderName(const QString &filePath);
+        qint64 computePathSize(const QString &path);
+        bool sameFiles(const QString &path1, const QString &path2);
         QString toValidFileSystemName(const QString &name, bool allowSeparators = false
                 , const QString &pad = QLatin1String(" "));
-        bool isValidFileSystemName(const QString& name, bool allowSeparators = false);
+        bool isValidFileSystemName(const QString &name, bool allowSeparators = false);
         qint64 freeDiskSpaceOnPath(const QString &path);
-        QString branchPath(const QString& file_path, QString* removed = 0);
-        bool sameFileNames(const QString& first, const QString& second);
-        QString expandPath(const QString& path);
-        QString expandPathAbs(const QString& path);
+        QString branchPath(const QString &filePath, QString *removed = 0);
+        bool sameFileNames(const QString &first, const QString &second);
+        QString expandPath(const QString &path);
+        QString expandPathAbs(const QString &path);
 
-        bool smartRemoveEmptyFolderTree(const QString& path);
-        bool forceRemove(const QString& file_path);
-        void removeDirRecursive(const QString& path);
+        bool smartRemoveEmptyFolderTree(const QString &path);
+        bool forceRemove(const QString &filePath);
+        void removeDirRecursive(const QString &path);
 
         QString tempPath();
     }
