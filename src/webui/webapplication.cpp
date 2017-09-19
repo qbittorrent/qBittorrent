qbittorrent/qBittorrent

commit 6b33db3ae3fe9d37c5f73737b537a64c77d2830e

Create root folder option when adding a torrent in WebUI

src/webui/webapplication.cpp
@@ -394,6 +394,7 @@ void WebApplication::action_command_download()
     QStringList list = urls.split('\n');
     bool skipChecking = request().posts["skip_checking"] == "true";
     bool addPaused = request().posts["paused"] == "true";
+    bool hasRootFolder = request().posts["root_folder"] == "true";
     QString savepath = request().posts["savepath"];
     QString category = request().posts["category"];
     QString cookie = request().posts["cookie"];
@@ -422,6 +423,7 @@ void WebApplication::action_command_download()
     params.skipChecking = skipChecking;
 
     params.addPaused = TriStateBool(addPaused);
+    params.createSubfolder = TriStateBool(hasRootFolder);
     params.savePath = savepath;
     params.category = category;
 
@@ -446,6 +448,7 @@ void WebApplication::action_command_upload()
     CHECK_URI(0);
     bool skipChecking = request().posts["skip_checking"] == "true";
     bool addPaused = request().posts["paused"] == "true";
+    bool hasRootFolder = request().posts["root_folder"] == "true";
     QString savepath = request().posts["savepath"];
     QString category = request().posts["category"];
 
@@ -468,6 +471,7 @@ void WebApplication::action_command_upload()
                 params.skipChecking = skipChecking;
 
                 params.addPaused = TriStateBool(addPaused);
+                params.createSubfolder = TriStateBool(hasRootFolder);
                 params.savePath = savepath;
                 params.category = category;
                 if (!BitTorrent::Session::instance()->addTorrent(torrentInfo, params)) {

src/webui/www/public/download.html
@@ -38,6 +38,10 @@
     <label for="skip_checking" class="leftLabelLarge">QBT_TR(Skip hash check)QBT_TR[CONTEXT=AddNewTorrentDialog]</label>
     <input type="checkbox" name="skip_checking" value="true"/>
 </div>
+<div class="formRow">
+    <label for="root_folder" class="leftLabelLarge">QBT_TR(Create subfolder)QBT_TR[CONTEXT=AddNewTorrentDialog]</label>
+    <input type="checkbox" name="root_folder" value="true" checked="checked"/>
+</div>
 <div id="submitbutton" style="margin-top: 12px; text-align: center;">
     <button type="submit" id="submitButton">QBT_TR(Download)QBT_TR[CONTEXT=downloadFromURL]</button>
 </div>

src/webui/www/public/scripts/mocha-init.js
@@ -57,7 +57,7 @@ initializeWindows = function() {
             paddingVertical: 0,
             paddingHorizontal: 0,
             width: 500,
-            height: 400
+            height: 420
         });
         updateMainData();
     });
@@ -97,7 +97,7 @@ initializeWindows = function() {
             paddingVertical: 0,
             paddingHorizontal: 0,
             width: 500,
-            height: 240
+            height: 260
         });
         updateMainData();
     });

src/webui/www/public/upload.html
@@ -34,6 +34,10 @@
     <label for="skip_checking" class="leftLabelLarge">QBT_TR(Skip hash check)QBT_TR[CONTEXT=AddNewTorrentDialog]</label>
     <input type="checkbox" name="skip_checking" value="true"/>
 </div>
+<div class="formRow">
+    <label for="root_folder" class="leftLabelLarge">QBT_TR(Create subfolder)QBT_TR[CONTEXT=AddNewTorrentDialog]</label>
+    <input type="checkbox" name="root_folder" value="true" checked="checked"/>
+</div>
 <div id="submitbutton" style="margin-top: 30px; text-align: center;">
         <button type="submit" style="font-size: 1em;">QBT_TR(Upload Torrents)QBT_TR[CONTEXT=HttpServer]</button>
 </div>

commit b107c0671d1a6c7ee442a5d99a0d54a21f6bb0f1

WebAPI: fix root_folder default behavior
Bug was introduced in
6b33db3ae3fe9d37c5f73737b537a64c77d2830e

src/webui/webapplication.cpp
@@ -394,7 +394,7 @@ void WebApplication::action_command_download()
     QStringList list = urls.split('\n');
     bool skipChecking = request().posts["skip_checking"] == "true";
     bool addPaused = request().posts["paused"] == "true";
-    bool hasRootFolder = request().posts["root_folder"] == "true";
+    const QString rootFolder = request().posts["root_folder"];
     QString savepath = request().posts["savepath"];
     QString category = request().posts["category"];
     QString cookie = request().posts["cookie"];
@@ -423,9 +423,12 @@ void WebApplication::action_command_download()
     params.skipChecking = skipChecking;
 
     params.addPaused = TriStateBool(addPaused);
-    params.createSubfolder = TriStateBool(hasRootFolder);
     params.savePath = savepath;
     params.category = category;
+    if (rootFolder == "true")
+        params.createSubfolder = TriStateBool::True;
+    else if (rootFolder == "false")
+        params.createSubfolder = TriStateBool::False;
 
     bool partialSuccess = false;
     foreach (QString url, list) {
@@ -448,7 +451,7 @@ void WebApplication::action_command_upload()
     CHECK_URI(0);
     bool skipChecking = request().posts["skip_checking"] == "true";
     bool addPaused = request().posts["paused"] == "true";
-    bool hasRootFolder = request().posts["root_folder"] == "true";
+    const QString rootFolder = request().posts["root_folder"];
     QString savepath = request().posts["savepath"];
     QString category = request().posts["category"];
 
@@ -471,9 +474,13 @@ void WebApplication::action_command_upload()
                 params.skipChecking = skipChecking;
 
                 params.addPaused = TriStateBool(addPaused);
-                params.createSubfolder = TriStateBool(hasRootFolder);
                 params.savePath = savepath;
                 params.category = category;
+                if (rootFolder == "true")
+                    params.createSubfolder = TriStateBool::True;
+                else if (rootFolder == "false")
+                    params.createSubfolder = TriStateBool::False;
+
                 if (!BitTorrent::Session::instance()->addTorrent(torrentInfo, params)) {
                     status(500, "Internal Server Error");
                     print(QObject::tr("Error: Could not add torrent to session."), Http::CONTENT_TYPE_TXT);