58861c35b0deac34c87a5e05eb319206a6d8e30c
Chocobo1 <Chocobo1@users.noreply.github.com>
2018-05-07 19:44:11 +0800

    Fix compile warnings on gcc 8
    The warning was:
    warning: catching polymorphic type ‘class std::exception’ by value
    [-Wcatch-value=]

--- src/base/bittorrent/session.cpp
+++ src/base/bittorrent/session.cpp
@@ -2945,7 +2945,7 @@ void Session::setMaxConnectionsPerTorrent(int max)
             try {
                 handle.set_max_connections(max);
             }
-            catch (std::exception) {}
+            catch (const std::exception &) {}
         }
     }
 }
@@ -2967,7 +2967,7 @@ void Session::setMaxUploadsPerTorrent(int max)
             try {
                 handle.set_max_uploads(max);
             }
-            catch (std::exception) {}
+            catch (const std::exception &) {}
         }
     }
 }
