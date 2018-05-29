e8d378e1672fd5e8aa6afc7a77284d9e96c0b085
Chocobo1 <Chocobo1@users.noreply.github.com>
2018-05-29 13:40:52 +0800

    Improve WebUI security measures
    CSP was erroneously disabled in bad4d94f778a1ceba8a0e16d1836e649d767cca8
    when clickjacking protection is off, now it is back.
    Also added CSP 'frame-ancestors' directive when clickjacking
    protection is enabled.

--- src/webui/webapplication.cpp
+++ src/webui/webapplication.cpp
@@ -533,10 +533,13 @@ Http::Response WebApplication::processRequest(const Http::Request &request, cons
     header(Http::HEADER_X_XSS_PROTECTION, "1; mode=block");
     header(Http::HEADER_X_CONTENT_TYPE_OPTIONS, "nosniff");
 
+    QString csp = QLatin1String("default-src 'self'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; script-src 'self' 'unsafe-inline'; object-src 'none';");
     if (m_isClickjackingProtectionEnabled) {
         header(Http::HEADER_X_FRAME_OPTIONS, "SAMEORIGIN");
-        header(Http::HEADER_CONTENT_SECURITY_POLICY, "default-src 'self'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; script-src 'self' 'unsafe-inline'; object-src 'none';");
+        csp += QLatin1String(" frame-ancestors 'self';");
     }
+
+    header(Http::HEADER_CONTENT_SECURITY_POLICY, csp);
 
     return response();
 }
