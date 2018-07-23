aba80e2b1cd4bb83d246fcb91eabe788fafbfe8b
Thomas Piccirello <thomas@piccirello.com>
2018-07-22 20:33:39 -0400

    Send all rechecks in one request

--- src/webui/www/private/scripts/mocha-init.js
+++ src/webui/www/private/scripts/mocha-init.js
@@ -387,15 +387,13 @@ initializeWindows = function() {
     recheckFN = function() {
         var hashes = torrentsTable.selectedRowsIds();
         if (hashes.length) {
-            hashes.each(function(hash, index) {
-                new Request({
-                    url: 'api/v2/torrents/recheck',
-                    method: 'post',
-                    data: {
-                        hashes: hash
-                    }
-                }).send();
-            });
+            new Request({
+                url: 'api/v2/torrents/recheck',
+                method: 'post',
+                data: {
+                    hashes: hashes.join("|"),
+                }
+            }).send();
             updateMainData();
         }
     };
