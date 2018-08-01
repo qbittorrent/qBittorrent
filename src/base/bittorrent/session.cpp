7716a27d0d88b8f7201edf29565e31fe308df74b
Vladimir Golovnev (Glassez) <glassez@yandex.ru>
2018-08-01 18:25:37 +0300

    Reorder resume data saving conditionals
    Having conditionals which can break saving of torrent resume data
    in order from more likely to less likely is more effective.

--- src/base/bittorrent/session.cpp
+++ src/base/bittorrent/session.cpp
@@ -2350,9 +2350,9 @@ void Session::generateResumeData(bool final)
 {
     foreach (TorrentHandle *const torrent, m_torrents) {
         if (!torrent->isValid()) continue;
-        if (torrent->hasMissingFiles()) continue;
-        if (torrent->isChecking() || torrent->hasError()) continue;
+        if (torrent->isChecking()) continue;
         if (!final && !torrent->needSaveResumeData()) continue;
+        if (torrent->hasMissingFiles() || torrent->hasError()) continue;
 
         saveTorrentResumeData(torrent, final);
     }
