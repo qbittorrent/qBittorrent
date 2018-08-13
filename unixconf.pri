qbittorrent/qBittorrent

commit 5d94db9c7940aee9af769ce314c1dd1be9ddc18c

Move .desktop file

unixconf.pri
@@ -36,7 +36,7 @@ nogui:systemd {
 
 # Menu Icon
 !nogui {
-    menuicon.files = icons/qbittorrent.desktop
+    menuicon.files = dist/unix/qbittorrent.desktop
     menuicon.path = $$DATADIR/applications/
     INSTALLS += menuicon
 

.tx/config
@@ -12,7 +12,7 @@ mode = developer
 
 
 [qbittorrent.qbittorrentdesktop_master]
-source_file = src/icons/qBittorrent.desktop
+source_file = dist/unix/qbittorrent.desktop
 source_lang = en
 type = DESKTOP
 minimum_perc = 23

dist/unix/CMakeLists.txt
@@ -31,7 +31,7 @@ if (Qt5Widgets_FOUND)
             DESTINATION ${CMAKE_INSTALL_DATAROOTDIR}/icons/hicolor
             FILES_MATCHING PATTERN "*.png")
 
-    install(FILES ${qBittorrent_SOURCE_DIR}/src/icons/qbittorrent.desktop
+    install(FILES qbittorrent.desktop
             DESTINATION ${CMAKE_INSTALL_DATAROOTDIR}/applications/
             COMPONENT data)
 

commit acdf9061fe5539c75604d2b7919cc434b2cebb02

Fix regression that broke installing desktop file
In commit 5d94db9c7940aee9af769ce314c1dd1be9ddc18c the desktop file was
moved from src/ to dist/ but the relative path from src/src.pro was
switched to an absolute path from the repository root. This broke
detection of the file from within qmake.

Fix by using the same $DIST_PATH used elsewhere for consistency, which
uses ../dist/.

unixconf.pri
@@ -36,7 +36,7 @@ nogui:systemd {
 
 # Menu Icon
 !nogui {
-    menuicon.files = dist/unix/qbittorrent.desktop
+    menuicon.files = $$DIST_PATH/qbittorrent.desktop
     menuicon.path = $$DATADIR/applications/
     INSTALLS += menuicon
 