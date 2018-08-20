5371e1f28a315e486b4266f196414761857334b8
Thomas Piccirello <thomas@piccirello.com>
2018-08-19 23:26:17 -0400

    Use getStats function for GUI Stats dialog

--- src/gui/statsdialog.cpp
+++ src/gui/statsdialog.cpp
@@ -60,51 +60,30 @@ StatsDialog::~StatsDialog()
 
 void StatsDialog::update()
 {
-    const BitTorrent::SessionStatus &ss = BitTorrent::Session::instance()->status();
-    const BitTorrent::CacheStatus &cs = BitTorrent::Session::instance()->cacheStatus();
+    const QVariantMap stats = BitTorrent::Session::instance()->getStats();
 
     // All-time DL/UL
-    quint64 atd = BitTorrent::Session::instance()->getAlltimeDL();
-    quint64 atu = BitTorrent::Session::instance()->getAlltimeUL();
-    m_ui->labelAlltimeDL->setText(Utils::Misc::friendlyUnit(atd));
-    m_ui->labelAlltimeUL->setText(Utils::Misc::friendlyUnit(atu));
+    m_ui->labelAlltimeDL->setText(Utils::Misc::friendlyUnit(stats["alltime_dl"].toULongLong()));
+    m_ui->labelAlltimeUL->setText(Utils::Misc::friendlyUnit(stats["alltime_ul"].toULongLong()));
     // Total waste (this session)
-    m_ui->labelWaste->setText(Utils::Misc::friendlyUnit(ss.totalWasted));
+    m_ui->labelWaste->setText(Utils::Misc::friendlyUnit(stats["total_wasted_session"].toULongLong()));
     // Global ratio
-    m_ui->labelGlobalRatio->setText(
-                ((atd > 0) && (atu > 0))
-                ? Utils::String::fromDouble(static_cast<qreal>(atu) / atd, 2)
-                : "-");
+    m_ui->labelGlobalRatio->setText(stats["global_ratio"].toString());
+    // Total connected peers
+    m_ui->labelPeers->setText(QString::number(stats["total_peer_connections"].toULongLong()));
     // Cache hits
-    qreal readRatio = cs.readRatio;
-    m_ui->labelCacheHits->setText(QString("%1%").arg(
-        readRatio > 0
-        ? Utils::String::fromDouble(100 * readRatio, 2)
-        : "0"));
+    m_ui->labelCacheHits->setText(stats["read_cache_hits"].toString());
     // Buffers size
-    m_ui->labelTotalBuf->setText(Utils::Misc::friendlyUnit(cs.totalUsedBuffers * 16 * 1024));
+    m_ui->labelTotalBuf->setText(Utils::Misc::friendlyUnit(stats["total_buffers_size"].toULongLong()));
     // Disk overload (100%) equivalent
     // From lt manual: disk_write_queue and disk_read_queue are the number of peers currently waiting on a disk write or disk read
     // to complete before it receives or sends any more data on the socket. It's a metric of how disk bound you are.
 
-    // num_peers is not reliable (adds up peers, which didn't even overcome tcp handshake)
-    quint32 peers = 0;
-    foreach (BitTorrent::TorrentHandle *const torrent, BitTorrent::Session::instance()->torrents())
-        peers += torrent->peersCount();
-
-    m_ui->labelWriteStarve->setText(QString("%1%")
-                                    .arg(((ss.diskWriteQueue > 0) && (peers > 0))
-                                         ? Utils::String::fromDouble((100. * ss.diskWriteQueue) / peers, 2)
-                                         : "0"));
-    m_ui->labelReadStarve->setText(QString("%1%")
-                                   .arg(((ss.diskReadQueue > 0) && (peers > 0))
-                                        ? Utils::String::fromDouble((100. * ss.diskReadQueue) / peers, 2)
-                                        : "0"));
+    m_ui->labelWriteStarve->setText(stats["write_cache_overload"].toString());
+    m_ui->labelReadStarve->setText(stats["read_cache_overload"].toString());
     // Disk queues
-    m_ui->labelQueuedJobs->setText(QString::number(cs.jobQueueLength));
-    m_ui->labelJobsTime->setText(tr("%1 ms", "18 milliseconds").arg(cs.averageJobTime));
-    m_ui->labelQueuedBytes->setText(Utils::Misc::friendlyUnit(cs.queuedBytes));
+    m_ui->labelQueuedJobs->setText(QString::number(stats["queued_io_jobs"].toULongLong()));
+    m_ui->labelJobsTime->setText(tr("%1 ms", "18 milliseconds").arg(stats["average_time_queue"].toULongLong()));
+    m_ui->labelQueuedBytes->setText(Utils::Misc::friendlyUnit(stats["total_queued_size"].toULongLong()));
 
-    // Total connected peers
-    m_ui->labelPeers->setText(QString::number(ss.peersCount));
 }
