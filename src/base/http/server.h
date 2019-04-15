a2a669572caf7e03594aaed9939dc0bc0e31e63e
Chocobo1 <Chocobo1@users.noreply.github.com>
2019-04-15 21:22:11 +0800

    Use QSet for tracking server connections
    We don't need to maintain order between connections so QSet would be more suitable.

--- src/base/http/server.h
+++ src/base/http/server.h
@@ -31,6 +31,7 @@
 #ifndef HTTP_SERVER_H
 #define HTTP_SERVER_H
 
+#include <QSet>
 #include <QSslCertificate>
 #include <QSslKey>
 #include <QTcpServer>
@@ -59,7 +60,7 @@ namespace Http
         void removeConnection(Connection *connection);
 
         IRequestHandler *m_requestHandler;
-        QList<Connection *> m_connections;  // for tracking persistent connections
+        QSet<Connection *> m_connections;  // for tracking persistent connections
 
         bool m_https;
         QList<QSslCertificate> m_certificates;
