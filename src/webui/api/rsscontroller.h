e6495596505de6112c2d6b5c03d7e967809fa4ef
Vladimir Golovnev (Glassez) <glassez@yandex.ru>
2019-08-11 14:52:51 +0300

    Allow to refresh RSS item(s) via WebAPI

--- src/webui/api/rsscontroller.h
+++ src/webui/api/rsscontroller.h
@@ -44,6 +44,7 @@ private slots:
     void removeItemAction();
     void moveItemAction();
     void itemsAction();
+    void refreshItemAction();
     void setRuleAction();
     void renameRuleAction();
     void removeRuleAction();
