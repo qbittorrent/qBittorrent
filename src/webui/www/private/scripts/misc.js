1439bcc8644ebc5e32f8aff282d576d7ce8e40d6
Thomas Piccirello <thomas@piccirello.com>
2019-08-11 23:53:20 -0700

    Move JavaScript code into explicit namespaces
    This cleans up the global namespace by explicitly exporting shared values. All html and JavaScript files have been converted to use explicit exports except for client.js and mocha-init.js

--- src/webui/www/private/scripts/misc.js
+++ src/webui/www/private/scripts/misc.js
@@ -28,166 +28,188 @@
 
 'use strict';
 
-/*
- * JS counterpart of the function in src/misc.cpp
- */
-function friendlyUnit(value, isSpeed) {
-    const units = [
-        "QBT_TR(B)QBT_TR[CONTEXT=misc]",
-        "QBT_TR(KiB)QBT_TR[CONTEXT=misc]",
-        "QBT_TR(MiB)QBT_TR[CONTEXT=misc]",
-        "QBT_TR(GiB)QBT_TR[CONTEXT=misc]",
-        "QBT_TR(TiB)QBT_TR[CONTEXT=misc]",
-        "QBT_TR(PiB)QBT_TR[CONTEXT=misc]",
-        "QBT_TR(EiB)QBT_TR[CONTEXT=misc]"
-    ];
-
-    if ((value === undefined) || (value === null) || (value < 0))
-        return "QBT_TR(Unknown)QBT_TR[CONTEXT=misc]";
-
-    let i = 0;
-    while (value >= 1024.0 && i < 6) {
-        value /= 1024.0;
-        ++i;
-    }
-
-    function friendlyUnitPrecision(sizeUnit) {
-        if (sizeUnit <= 2) return 1; // KiB, MiB
-        else if (sizeUnit === 3) return 2; // GiB
-        else return 3; // TiB, PiB, EiB
-    }
-
-    let ret;
-    if (i === 0)
-        ret = value + " " + units[i];
-    else {
-        const precision = friendlyUnitPrecision(i);
-        const offset = Math.pow(10, precision);
-        // Don't round up
-        ret = (Math.floor(offset * value) / offset).toFixed(precision) + " " + units[i];
-    }
-
-    if (isSpeed)
-        ret += "QBT_TR(/s)QBT_TR[CONTEXT=misc]";
-    return ret;
+if (window.qBittorrent === undefined) {
+    window.qBittorrent = {};
 }
 
-/*
- * JS counterpart of the function in src/misc.cpp
- */
-function friendlyDuration(seconds) {
-    const MAX_ETA = 8640000;
-    if (seconds < 0 || seconds >= MAX_ETA)
-        return "∞";
-    if (seconds === 0)
-        return "0";
-    if (seconds < 60)
-        return "QBT_TR(< 1m)QBT_TR[CONTEXT=misc]";
-    let minutes = seconds / 60;
-    if (minutes < 60)
-        return "QBT_TR(%1m)QBT_TR[CONTEXT=misc]".replace("%1", parseInt(minutes));
-    let hours = minutes / 60;
-    minutes = minutes % 60;
-    if (hours < 24)
-        return "QBT_TR(%1h %2m)QBT_TR[CONTEXT=misc]".replace("%1", parseInt(hours)).replace("%2", parseInt(minutes));
-    const days = hours / 24;
-    hours = hours % 24;
-    if (days < 100)
-        return "QBT_TR(%1d %2h)QBT_TR[CONTEXT=misc]".replace("%1", parseInt(days)).replace("%2", parseInt(hours));
-    return "∞";
-}
-
-function friendlyPercentage(value) {
-    let percentage = (value * 100).round(1);
-    if (isNaN(percentage) || (percentage < 0))
-        percentage = 0;
-    if (percentage > 100)
-        percentage = 100;
-    return percentage.toFixed(1) + "%";
-}
-
-function friendlyFloat(value, precision) {
-    return parseFloat(value).toFixed(precision);
-}
-
-/*
- * From: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toISOString
- */
-if (!Date.prototype.toISOString) {
-    (function() {
-
-        function pad(number) {
-            if (number < 10) {
-                return '0' + number;
-            }
-            return number;
-        }
-
-        Date.prototype.toISOString = function() {
-            return this.getUTCFullYear()
-                + '-' + pad(this.getUTCMonth() + 1)
-                + '-' + pad(this.getUTCDate())
-                + 'T' + pad(this.getUTCHours())
-                + ':' + pad(this.getUTCMinutes())
-                + ':' + pad(this.getUTCSeconds())
-                + '.' + (this.getUTCMilliseconds() / 1000).toFixed(3).slice(2, 5)
-                + 'Z';
+window.qBittorrent.Misc = (function() {
+    const exports = function() {
+        return {
+            friendlyUnit: friendlyUnit,
+            friendlyDuration: friendlyDuration,
+            friendlyPercentage: friendlyPercentage,
+            friendlyFloat: friendlyFloat,
+            parseHtmlLinks: parseHtmlLinks,
+            escapeHtml: escapeHtml,
+            safeTrim: safeTrim,
+            toFixedPointString: toFixedPointString,
+            containsAllTerms: containsAllTerms
         };
+    };
 
-    }());
-}
+    /*
+    * JS counterpart of the function in src/misc.cpp
+    */
+    const friendlyUnit = function(value, isSpeed) {
+        const units = [
+            "QBT_TR(B)QBT_TR[CONTEXT=misc]",
+            "QBT_TR(KiB)QBT_TR[CONTEXT=misc]",
+            "QBT_TR(MiB)QBT_TR[CONTEXT=misc]",
+            "QBT_TR(GiB)QBT_TR[CONTEXT=misc]",
+            "QBT_TR(TiB)QBT_TR[CONTEXT=misc]",
+            "QBT_TR(PiB)QBT_TR[CONTEXT=misc]",
+            "QBT_TR(EiB)QBT_TR[CONTEXT=misc]"
+        ];
 
-/*
- * JS counterpart of the function in src/misc.cpp
- */
-function parseHtmlLinks(text) {
-    const exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
-    return text.replace(exp, "<a target='_blank' href='$1'>$1</a>");
-}
+        if ((value === undefined) || (value === null) || (value < 0))
+            return "QBT_TR(Unknown)QBT_TR[CONTEXT=misc]";
 
-function escapeHtml(str) {
-    const div = document.createElement('div');
-    div.appendChild(document.createTextNode(str));
-    return div.innerHTML;
-}
-
-function safeTrim(value) {
-    try {
-        return value.trim();
-    }
-    catch (e) {
-        if (e instanceof TypeError)
-            return "";
-        throw e;
-    }
-}
-
-function toFixedPointString(number, digits) {
-    // Do not round up number
-    const power = Math.pow(10, digits);
-    return (Math.floor(power * number) / power).toFixed(digits);
-}
-
-/**
- *
- * @param {String} text the text to search
- * @param {Array<String>} terms terms to search for within the text
- * @returns {Boolean} true if all terms match the text, false otherwise
- */
-function containsAllTerms(text, terms) {
-    const textToSearch = text.toLowerCase();
-    return terms.every((function(term) {
-        const isTermRequired = (term[0] === '+');
-        const isTermExcluded = (term[0] === '-');
-        if (isTermRequired || isTermExcluded) {
-            // ignore lonely +/-
-            if (term.length === 1)
-                return true;
-
-            term = term.substring(1);
+        let i = 0;
+        while (value >= 1024.0 && i < 6) {
+            value /= 1024.0;
+            ++i;
         }
 
-        const textContainsTerm = (textToSearch.indexOf(term) !== -1);
-        return isTermExcluded ? !textContainsTerm : textContainsTerm;
-    }));
-}
+        function friendlyUnitPrecision(sizeUnit) {
+            if (sizeUnit <= 2) return 1; // KiB, MiB
+            else if (sizeUnit === 3) return 2; // GiB
+            else return 3; // TiB, PiB, EiB
+        }
+
+        let ret;
+        if (i === 0)
+            ret = value + " " + units[i];
+        else {
+            const precision = friendlyUnitPrecision(i);
+            const offset = Math.pow(10, precision);
+            // Don't round up
+            ret = (Math.floor(offset * value) / offset).toFixed(precision) + " " + units[i];
+        }
+
+        if (isSpeed)
+            ret += "QBT_TR(/s)QBT_TR[CONTEXT=misc]";
+        return ret;
+    }
+
+    /*
+    * JS counterpart of the function in src/misc.cpp
+    */
+    const friendlyDuration = function(seconds) {
+        const MAX_ETA = 8640000;
+        if (seconds < 0 || seconds >= MAX_ETA)
+            return "∞";
+        if (seconds === 0)
+            return "0";
+        if (seconds < 60)
+            return "QBT_TR(< 1m)QBT_TR[CONTEXT=misc]";
+        let minutes = seconds / 60;
+        if (minutes < 60)
+            return "QBT_TR(%1m)QBT_TR[CONTEXT=misc]".replace("%1", parseInt(minutes));
+        let hours = minutes / 60;
+        minutes = minutes % 60;
+        if (hours < 24)
+            return "QBT_TR(%1h %2m)QBT_TR[CONTEXT=misc]".replace("%1", parseInt(hours)).replace("%2", parseInt(minutes));
+        const days = hours / 24;
+        hours = hours % 24;
+        if (days < 100)
+            return "QBT_TR(%1d %2h)QBT_TR[CONTEXT=misc]".replace("%1", parseInt(days)).replace("%2", parseInt(hours));
+        return "∞";
+    }
+
+    const friendlyPercentage = function(value) {
+        let percentage = (value * 100).round(1);
+        if (isNaN(percentage) || (percentage < 0))
+            percentage = 0;
+        if (percentage > 100)
+            percentage = 100;
+        return percentage.toFixed(1) + "%";
+    }
+
+    const friendlyFloat = function(value, precision) {
+        return parseFloat(value).toFixed(precision);
+    }
+
+    /*
+    * From: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toISOString
+    */
+    if (!Date.prototype.toISOString) {
+        (function() {
+
+            function pad(number) {
+                if (number < 10) {
+                    return '0' + number;
+                }
+                return number;
+            }
+
+            Date.prototype.toISOString = function() {
+                return this.getUTCFullYear()
+                    + '-' + pad(this.getUTCMonth() + 1)
+                    + '-' + pad(this.getUTCDate())
+                    + 'T' + pad(this.getUTCHours())
+                    + ':' + pad(this.getUTCMinutes())
+                    + ':' + pad(this.getUTCSeconds())
+                    + '.' + (this.getUTCMilliseconds() / 1000).toFixed(3).slice(2, 5)
+                    + 'Z';
+            };
+
+        }());
+    }
+
+    /*
+    * JS counterpart of the function in src/misc.cpp
+    */
+    const parseHtmlLinks = function(text) {
+        const exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+        return text.replace(exp, "<a target='_blank' href='$1'>$1</a>");
+    }
+
+    const escapeHtml = function(str) {
+        const div = document.createElement('div');
+        div.appendChild(document.createTextNode(str));
+        return div.innerHTML;
+    }
+
+    const safeTrim = function(value) {
+        try {
+            return value.trim();
+        }
+        catch (e) {
+            if (e instanceof TypeError)
+                return "";
+            throw e;
+        }
+    }
+
+    const toFixedPointString = function(number, digits) {
+        // Do not round up number
+        const power = Math.pow(10, digits);
+        return (Math.floor(power * number) / power).toFixed(digits);
+    }
+
+    /**
+     *
+     * @param {String} text the text to search
+     * @param {Array<String>} terms terms to search for within the text
+     * @returns {Boolean} true if all terms match the text, false otherwise
+     */
+    const containsAllTerms = function(text, terms) {
+        const textToSearch = text.toLowerCase();
+        return terms.every((function(term) {
+            const isTermRequired = (term[0] === '+');
+            const isTermExcluded = (term[0] === '-');
+            if (isTermRequired || isTermExcluded) {
+                // ignore lonely +/-
+                if (term.length === 1)
+                    return true;
+
+                term = term.substring(1);
+            }
+
+            const textContainsTerm = (textToSearch.indexOf(term) !== -1);
+            return isTermExcluded ? !textContainsTerm : textContainsTerm;
+        }));
+    }
+
+    return exports();
+})();
