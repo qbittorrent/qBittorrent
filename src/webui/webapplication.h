0f8def9a497a9c8d4a6bf60d9f506a121899e76e
sledgehammer999 <hammered999@gmail.com>
2019-10-01 17:31:55 +0300

    Bump Web API version
    Closes #11304

--- src/webui/webapplication.h
+++ src/webui/webapplication.h
@@ -43,8 +43,8 @@
 #include "base/utils/net.h"
 #include "base/utils/version.h"
 
-constexpr Utils::Version<int, 3, 2> API_VERSION {2, 2, 0};
-constexpr int COMPAT_API_VERSION = 23;
+constexpr Utils::Version<int, 3, 2> API_VERSION {2, 2, 1};
+constexpr int COMPAT_API_VERSION = 24;
 constexpr int COMPAT_API_VERSION_MIN = 23;
 
 class APIController;
