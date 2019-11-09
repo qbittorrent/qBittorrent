e3879f9ab9459e3ec6c0b4444197a5a56e0b18da
Prince Gupta <jagannatharjun11@gmail.com>
2019-11-09 18:13:30 +0530

    CMake: Use libraries from winconf.pri

--- cmake/Modules/winconf-mingw.cmake
+++ cmake/Modules/winconf-mingw.cmake
@@ -6,4 +6,6 @@ list(APPEND LibtorrentRasterbar_CUSTOM_DEFINITIONS
     -D_FILE_OFFSET_BITS=64
     -D__USE_W32_SOCKETS)
 
-link_libraries(advapi32 iphlpapi ole32 powrprof shell32 user32 wsock32 ws2_32)
+# libraries from winconf.pri
+link_libraries(advapi32 iphlpapi ole32 shell32 user32 wsock32 ws2_32)
+
