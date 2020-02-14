8423f43209e716e6bb9138c241a8529a9b7ce8cf
an0n666 <47396879+an0n666@users.noreply.github.com>
2020-02-14 09:39:57 +0000

    Do not list not contacted trackers as error

--- src/base/bittorrent/torrenthandle.cpp
+++ src/base/bittorrent/torrenthandle.cpp
@@ -1654,14 +1654,13 @@ void TorrentHandle::handleTrackerErrorAlert(const lt::tracker_error_alert *p)
 
     // Starting with libtorrent 1.2.x each tracker has multiple local endpoints from which
     // an announce is attempted. Some endpoints might succeed while others might fail.
-    // Emit the signal only if all endpoints have failed. TrackerEntry::isWorking() returns
-    // true if at least one endpoint works.
+    // Emit the signal only if all endpoints have failed.
     const QVector<TrackerEntry> trackerList = trackers();
     const auto iter = std::find_if(trackerList.cbegin(), trackerList.cend(), [&trackerUrl](const TrackerEntry &entry)
     {
         return (entry.url() == trackerUrl);
     });
-    if ((iter != trackerList.cend()) && !iter->isWorking())
+    if ((iter != trackerList.cend()) && (iter->status() == TrackerEntry::NotWorking))
         m_session->handleTorrentTrackerError(this, trackerUrl);
 }
 
