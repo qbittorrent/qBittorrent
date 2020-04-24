5468f101844a00860cd7e503c4334624d9cda7eb
sledgehammer999 <hammered999@gmail.com>
2020-04-25 00:20:15 +0300

    Bump Web API version

--- src/webui/webapplication.h
+++ src/webui/webapplication.h
@@ -43,7 +43,7 @@
 #include "base/utils/net.h"
 #include "base/utils/version.h"
 
-constexpr Utils::Version<int, 3, 2> API_VERSION {2, 5, 0};
+constexpr Utils::Version<int, 3, 2> API_VERSION {2, 5, 1};
 
 class APIController;
 class WebApplication;
