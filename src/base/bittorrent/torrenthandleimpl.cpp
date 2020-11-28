47e9c5ac08b87614140bb1d9863e61423701b1a6
sledgehammer999 <hammered999@gmail.com>
2020-11-29 00:32:53 +0200

    Fix status of torrents without metadata

--- src/base/bittorrent/torrenthandleimpl.cpp
+++ src/base/bittorrent/torrenthandleimpl.cpp
@@ -777,10 +777,10 @@ void TorrentHandleImpl::updateState()
     {
         if (isPaused())
             m_state = TorrentState::PausedDownloading;
-        else if (m_nativeStatus.state == lt::torrent_status::downloading_metadata)
-            m_state = TorrentState::DownloadingMetadata;
         else if (m_session->isQueueingSystemEnabled() && isQueued())
             m_state = TorrentState::QueuedDownloading;
+        else if (m_nativeStatus.state == lt::torrent_status::downloading_metadata) // must come after queue check
+            m_state = TorrentState::DownloadingMetadata;
         else if (isForced())
             m_state = TorrentState::ForcedDownloading;
         else if (m_nativeStatus.download_payload_rate > 0)
