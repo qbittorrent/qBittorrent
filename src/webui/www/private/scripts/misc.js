060804d3b8b2ab398eb3aefe98560a262ebed72c
Chocobo1 <Chocobo1@users.noreply.github.com>
2021-03-23 22:00:36 +0800

    Remove unnecessary escape character

--- src/webui/www/private/scripts/misc.js
+++ src/webui/www/private/scripts/misc.js
@@ -162,7 +162,7 @@ window.qBittorrent.Misc = (function() {
     * JS counterpart of the function in src/misc.cpp
     */
     const parseHtmlLinks = function(text) {
-        const exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+        const exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#/%?=~_|!:,.;]*[-A-Z0-9+&@#/%=~_|])/ig;
         return text.replace(exp, "<a target='_blank' href='$1'>$1</a>");
     }
 
