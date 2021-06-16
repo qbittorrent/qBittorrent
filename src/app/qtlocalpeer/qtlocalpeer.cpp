f3962e683894b7e4560a48c081a40ecf01f6e1e0
Vladimir Golovnev (Glassez) <glassez@yandex.ru>
2021-06-16 18:14:57 +0300

    Remove lockfile only when last app instance is destroyed

--- src/app/qtlocalpeer/qtlocalpeer.cpp
+++ src/app/qtlocalpeer/qtlocalpeer.cpp
@@ -96,7 +96,7 @@ namespace QtLP_Private
 
 const char* QtLocalPeer::ack = "ack";
 
-QtLocalPeer::QtLocalPeer(QObject* parent, const QString &appId)
+QtLocalPeer::QtLocalPeer(QObject *parent, const QString &appId)
     : QObject(parent)
     , id(appId)
 {
@@ -136,8 +136,11 @@ QtLocalPeer::QtLocalPeer(QObject* parent, const QString &appId)
 
 QtLocalPeer::~QtLocalPeer()
 {
-    lockFile.unlock();
-    lockFile.remove();
+    if (!isClient())
+    {
+        lockFile.unlock();
+        lockFile.remove();
+    }
 }
 
 bool QtLocalPeer::isClient()
