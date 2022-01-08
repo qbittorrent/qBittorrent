qbittorrent/qBittorrent

commit 1c0f8b4289b8d298041fe195cdc24f793dce4df5

Redesign "Incomplete folder" feature
Change "Incomplete/temp folder" term with "download folder".
Allow to set "download folder" per torrent (in manual mode) and per category (in automatic mode).

src/webui/api/synccontroller.cpp
@@ -209,6 +209,7 @@ namespace
             case QMetaType::ULongLong:
             case QMetaType::UInt:
             case QMetaType::QDateTime:
+            case QMetaType::Nullptr:
                 if (prevData[key] != value)
                     syncData[key] = value;
                 break;
@@ -426,6 +427,7 @@ SyncController::~SyncController()
 //  - "uploaded_session": Amount of data uploaded since program open
 //  - "amount_left": Amount of data left to download
 //  - "save_path": Torrent save path
+//  - "download_path": Torrent download path
 //  - "completed": Amount of data completed
 //  - "max_ratio": Upload max share ratio
 //  - "max_seeding_time": Upload max seeding time
@@ -496,15 +498,13 @@ void SyncController::maindataAction()
     data["torrents"] = torrents;
 
     QVariantHash categories;
-    const QStringMap categoriesList = session->categories();
-    for (auto it = categoriesList.cbegin(); it != categoriesList.cend(); ++it)
+    const QStringList categoriesList = session->categories();
+    for (const auto &categoryName : categoriesList)
     {
-        const QString &key = it.key();
-        categories[key] = QVariantMap
-        {
-            {"name", key},
-            {"savePath", it.value()}
-        };
+        const BitTorrent::CategoryOptions categoryOptions = session->categoryOptions(categoryName);
+        QJsonObject category = categoryOptions.toJSON();
+        category.insert(QLatin1String("name"), categoryName);
+        categories[categoryName] = category.toVariantMap();
     }
     data["categories"] = categories;
 

src/webui/api/torrentscontroller.cpp
@@ -39,6 +39,7 @@
 #include <QRegularExpression>
 #include <QUrl>
 
+#include "base/bittorrent/categoryoptions.h"
 #include "base/bittorrent/common.h"
 #include "base/bittorrent/downloadpriority.h"
 #include "base/bittorrent/infohash.h"
@@ -103,6 +104,7 @@ const char KEY_PROP_ADDITION_DATE[] = "addition_date";
 const char KEY_PROP_COMPLETION_DATE[] = "completion_date";
 const char KEY_PROP_CREATION_DATE[] = "creation_date";
 const char KEY_PROP_SAVE_PATH[] = "save_path";
+const char KEY_PROP_DOWNLOAD_PATH[] = "download_path";
 const char KEY_PROP_COMMENT[] = "comment";
 
 // File keys
@@ -372,6 +374,7 @@ void TorrentsController::infoAction()
 //   - "completion_date": Torrent completion date
 //   - "creation_date": Torrent creation date
 //   - "save_path": Torrent save path
+//   - "download_path": Torrent download path
 //   - "comment": Torrent comment
 void TorrentsController::propertiesAction()
 {
@@ -430,6 +433,7 @@ void TorrentsController::propertiesAction()
         dataDict[KEY_PROP_CREATION_DATE] = -1;
     }
     dataDict[KEY_PROP_SAVE_PATH] = Utils::Fs::toNativePath(torrent->savePath());
+    dataDict[KEY_PROP_DOWNLOAD_PATH] = Utils::Fs::toNativePath(torrent->downloadPath());
     dataDict[KEY_PROP_COMMENT] = torrent->comment();
 
     setResult(dataDict);
@@ -643,6 +647,8 @@ void TorrentsController::addAction()
     const bool firstLastPiece = parseBool(params()["firstLastPiecePrio"]).value_or(false);
     const std::optional<bool> addPaused = parseBool(params()["paused"]);
     const QString savepath = params()["savepath"].trimmed();
+    const QString downloadPath = params()["downloadPath"].trimmed();
+    const std::optional<bool> useDownloadPath = parseBool(params()["useDownloadPath"]);
     const QString category = params()["category"];
     const QStringList tags = params()["tags"].split(',', Qt::SkipEmptyParts);
     const QString torrentName = params()["rename"].trimmed();
@@ -682,6 +688,8 @@ void TorrentsController::addAction()
     addTorrentParams.addPaused = addPaused;
     addTorrentParams.contentLayout = contentLayout;
     addTorrentParams.savePath = savepath;
+    addTorrentParams.downloadPath = downloadPath;
+    addTorrentParams.useDownloadPath = useDownloadPath;
     addTorrentParams.category = category;
     addTorrentParams.tags.insert(tags.cbegin(), tags.cend());
     addTorrentParams.name = torrentName;
@@ -1095,7 +1103,58 @@ void TorrentsController::setLocationAction()
     {
         LogMsg(tr("WebUI Set location: moving \"%1\", from \"%2\" to \"%3\"")
             .arg(torrent->name(), Utils::Fs::toNativePath(torrent->savePath()), Utils::Fs::toNativePath(newLocation)));
-        torrent->move(Utils::Fs::expandPathAbs(newLocation));
+        torrent->setAutoTMMEnabled(false);
+        torrent->setSavePath(Utils::Fs::expandPathAbs(newLocation));
+    });
+}
+
+void TorrentsController::setSavePathAction()
+{
+    requireParams({"id", "path"});
+
+    const QStringList ids {params()["id"].split('|')};
+    const QString newPath {params()["path"]};
+
+    if (newPath.isEmpty())
+        throw APIError(APIErrorType::BadParams, tr("Save path cannot be empty"));
+
+    // try to create the directory if it does not exist
+    if (!QDir(newPath).mkpath("."))
+        throw APIError(APIErrorType::Conflict, tr("Cannot create target directory"));
+
+    // check permissions
+    if (!QFileInfo(newPath).isWritable())
+        throw APIError(APIErrorType::AccessDenied, tr("Cannot write to directory"));
+
+    applyToTorrents(ids, [&newPath](BitTorrent::Torrent *const torrent)
+    {
+        if (!torrent->isAutoTMMEnabled())
+            torrent->setSavePath(newPath);
+    });
+}
+
+void TorrentsController::setDownloadPathAction()
+{
+    requireParams({"id", "path"});
+
+    const QStringList ids {params()["id"].split('|')};
+    const QString newPath {params()["path"]};
+
+    if (!newPath.isEmpty())
+    {
+        // try to create the directory if it does not exist
+        if (!QDir(newPath).mkpath("."))
+            throw APIError(APIErrorType::Conflict, tr("Cannot create target directory"));
+
+        // check permissions
+        if (!QFileInfo(newPath).isWritable())
+            throw APIError(APIErrorType::AccessDenied, tr("Cannot write to directory"));
+    }
+
+    applyToTorrents(ids, [&newPath](BitTorrent::Torrent *const torrent)
+    {
+        if (!torrent->isAutoTMMEnabled())
+            torrent->setDownloadPath(newPath);
     });
 }
 
@@ -1164,16 +1223,24 @@ void TorrentsController::createCategoryAction()
 {
     requireParams({"category"});
 
-    const QString category {params()["category"]};
-    const QString savePath {params()["savePath"]};
-
+    const QString category = params()["category"];
     if (category.isEmpty())
         throw APIError(APIErrorType::BadParams, tr("Category cannot be empty"));
 
     if (!BitTorrent::Session::isValidCategoryName(category))
         throw APIError(APIErrorType::Conflict, tr("Incorrect category name"));
 
-    if (!BitTorrent::Session::instance()->addCategory(category, savePath))
+    const QString savePath = params()["savePath"];
+    const auto useDownloadPath = parseBool(params()["downloadPathEnabled"]);
+    BitTorrent::CategoryOptions categoryOptions;
+    categoryOptions.savePath = savePath;
+    if (useDownloadPath.has_value())
+    {
+        const QString downloadPath = params()["downloadPath"];
+        categoryOptions.downloadPath = {useDownloadPath.value(), downloadPath};
+    }
+
+    if (!BitTorrent::Session::instance()->addCategory(category, categoryOptions))
         throw APIError(APIErrorType::Conflict, tr("Unable to create category"));
 }
 
@@ -1181,13 +1248,21 @@ void TorrentsController::editCategoryAction()
 {
     requireParams({"category", "savePath"});
 
-    const QString category {params()["category"]};
-    const QString savePath {params()["savePath"]};
-
+    const QString category = params()["category"];
     if (category.isEmpty())
         throw APIError(APIErrorType::BadParams, tr("Category cannot be empty"));
 
-    if (!BitTorrent::Session::instance()->editCategory(category, savePath))
+    const QString savePath = params()["savePath"];
+    const auto useDownloadPath = parseBool(params()["downloadPathEnabled"]);
+    BitTorrent::CategoryOptions categoryOptions;
+    categoryOptions.savePath = savePath;
+    if (useDownloadPath.has_value())
+    {
+        const QString downloadPath = params()["downloadPath"];
+        categoryOptions.downloadPath = {useDownloadPath.value(), downloadPath};
+    }
+
+    if (!BitTorrent::Session::instance()->editCategory(category, categoryOptions))
         throw APIError(APIErrorType::Conflict, tr("Unable to edit category"));
 }
 
@@ -1202,16 +1277,16 @@ void TorrentsController::removeCategoriesAction()
 
 void TorrentsController::categoriesAction()
 {
+    const auto session = BitTorrent::Session::instance();
+
     QJsonObject categories;
-    const QStringMap categoriesMap = BitTorrent::Session::instance()->categories();
-    for (auto it = categoriesMap.cbegin(); it != categoriesMap.cend(); ++it)
+    const QStringList categoriesList = session->categories();
+    for (const auto &categoryName : categoriesList)
     {
-        const auto &key = it.key();
-        categories[key] = QJsonObject
-        {
-            {"name", key},
-            {"savePath", it.value()}
-        };
+        const BitTorrent::CategoryOptions categoryOptions = session->categoryOptions(categoryName);
+        QJsonObject category = categoryOptions.toJSON();
+        category.insert(QLatin1String("name"), categoryName);
+        categories[categoryName] = category;
     }
 
     setResult(categories);

src/webui/webapplication.h
@@ -43,7 +43,7 @@
 #include "base/utils/net.h"
 #include "base/utils/version.h"
 
-inline const Utils::Version<int, 3, 2> API_VERSION {2, 8, 3};
+inline const Utils::Version<int, 3, 2> API_VERSION {2, 8, 4};
 
 class APIController;
 class WebApplication;

src/base/CMakeLists.txt
@@ -8,6 +8,7 @@ add_library(qbt_base STATIC
     bittorrent/bandwidthscheduler.h
     bittorrent/bencoderesumedatastorage.h
     bittorrent/cachestatus.h
+    bittorrent/categoryoptions.h
     bittorrent/common.h
     bittorrent/customstorage.h
     bittorrent/dbresumedatastorage.h
@@ -100,6 +101,7 @@ add_library(qbt_base STATIC
     bittorrent/abstractfilestorage.cpp
     bittorrent/bandwidthscheduler.cpp
     bittorrent/bencoderesumedatastorage.cpp
+    bittorrent/categoryoptions.cpp
     bittorrent/customstorage.cpp
     bittorrent/dbresumedatastorage.cpp
     bittorrent/downloadpriority.cpp

src/base/base.pri
@@ -7,6 +7,7 @@ HEADERS += \
     $$PWD/bittorrent/bandwidthscheduler.h \
     $$PWD/bittorrent/bencoderesumedatastorage.h \
     $$PWD/bittorrent/cachestatus.h \
+    $$PWD/bittorrent/categoryoptions.h \
     $$PWD/bittorrent/common.h \
     $$PWD/bittorrent/customstorage.h \
     $$PWD/bittorrent/downloadpriority.h \
@@ -100,6 +101,7 @@ SOURCES += \
     $$PWD/bittorrent/abstractfilestorage.cpp \
     $$PWD/bittorrent/bandwidthscheduler.cpp \
     $$PWD/bittorrent/bencoderesumedatastorage.cpp \
+    $$PWD/bittorrent/categoryoptions.cpp \
     $$PWD/bittorrent/customstorage.cpp \
     $$PWD/bittorrent/dbresumedatastorage.cpp \
     $$PWD/bittorrent/downloadpriority.cpp \

src/base/bittorrent/addtorrentparams.h
@@ -48,7 +48,8 @@ namespace BitTorrent
         QString category;
         TagSet tags;
         QString savePath;
-        bool disableTempPath = false; // e.g. for imported torrents
+        std::optional<bool> useDownloadPath;
+        QString downloadPath;
         bool sequential = false;
         bool firstLastPiecePriority = false;
         bool addForced = false;

src/base/bittorrent/bencoderesumedatastorage.cpp
@@ -173,11 +173,18 @@ std::optional<BitTorrent::LoadTorrentParams> BitTorrent::BencodeResumeDataStorag
     torrentParams.restored = true;
     torrentParams.category = fromLTString(root.dict_find_string_value("qBt-category"));
     torrentParams.name = fromLTString(root.dict_find_string_value("qBt-name"));
-    torrentParams.savePath = Profile::instance()->fromPortablePath(
-                Utils::Fs::toUniformPath(fromLTString(root.dict_find_string_value("qBt-savePath"))));
     torrentParams.hasSeedStatus = root.dict_find_int_value("qBt-seedStatus");
     torrentParams.firstLastPiecePriority = root.dict_find_int_value("qBt-firstLastPiecePriority");
     torrentParams.seedingTimeLimit = root.dict_find_int_value("qBt-seedingTimeLimit", Torrent::USE_GLOBAL_SEEDING_TIME);
+
+    torrentParams.savePath = Profile::instance()->fromPortablePath(
+                Utils::Fs::toUniformPath(fromLTString(root.dict_find_string_value("qBt-savePath"))));
+    torrentParams.useAutoTMM = torrentParams.savePath.isEmpty();
+    if (!torrentParams.useAutoTMM)
+    {
+        torrentParams.downloadPath = Profile::instance()->fromPortablePath(
+                    Utils::Fs::toUniformPath(fromLTString(root.dict_find_string_value("qBt-downloadPath"))));
+    }
 
     // TODO: The following code is deprecated. Replace with the commented one after several releases in 4.4.x.
     // === BEGIN DEPRECATED CODE === //
@@ -352,7 +359,6 @@ void BitTorrent::BencodeResumeDataStorage::Worker::store(const TorrentID &id, co
         }
     }
 
-    data["qBt-savePath"] = Profile::instance()->toPortablePath(resumeData.savePath).toStdString();
     data["qBt-ratioLimit"] = static_cast<int>(resumeData.ratioLimit * 1000);
     data["qBt-seedingTimeLimit"] = resumeData.seedingTimeLimit;
     data["qBt-category"] = resumeData.category.toStdString();
@@ -361,6 +367,12 @@ void BitTorrent::BencodeResumeDataStorage::Worker::store(const TorrentID &id, co
     data["qBt-seedStatus"] = resumeData.hasSeedStatus;
     data["qBt-contentLayout"] = Utils::String::fromEnum(resumeData.contentLayout).toStdString();
     data["qBt-firstLastPiecePriority"] = resumeData.firstLastPiecePriority;
+
+    if (!resumeData.useAutoTMM)
+    {
+        data["qBt-savePath"] = Profile::instance()->toPortablePath(resumeData.savePath).toStdString();
+        data["qBt-downloadPath"] = Profile::instance()->toPortablePath(resumeData.downloadPath).toStdString();
+    }
 
     const QString resumeFilepath = m_resumeDataDir.absoluteFilePath(QString::fromLatin1("%1.fastresume").arg(id.toString()));
     const nonstd::expected<void, QString> result = Utils::IO::saveToFile(resumeFilepath, data);

src/base/bittorrent/categoryoptions.cpp
@@ -1,0 +1,78 @@
+/*
+ * Bittorrent Client using Qt and libtorrent.
+ * Copyright (C) 2021  Vladimir Golovnev <glassez@yandex.ru>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * In addition, as a special exception, the copyright holders give permission to
+ * link this program with the OpenSSL project's "OpenSSL" library (or with
+ * modified versions of it that use the same license as the "OpenSSL" library),
+ * and distribute the linked executables. You must obey the GNU General Public
+ * License in all respects for all of the code used other than "OpenSSL".  If you
+ * modify file(s), you may extend this exception to your version of the file(s),
+ * but you are not obligated to do so. If you do not wish to do so, delete this
+ * exception statement from your version.
+ */
+
+#include "categoryoptions.h"
+
+#include <QJsonObject>
+#include <QJsonValue>
+
+const QString OPTION_SAVEPATH {QStringLiteral("save_path")};
+const QString OPTION_DOWNLOADPATH {QStringLiteral("download_path")};
+
+BitTorrent::CategoryOptions BitTorrent::CategoryOptions::fromJSON(const QJsonObject &jsonObj)
+{
+    CategoryOptions options;
+    options.savePath = jsonObj.value(OPTION_SAVEPATH).toString();
+
+    const QJsonValue downloadPathValue = jsonObj.value(OPTION_DOWNLOADPATH);
+    if (downloadPathValue.isBool())
+        options.downloadPath = {downloadPathValue.toBool()};
+    else if (downloadPathValue.isString())
+        options.downloadPath = {true, downloadPathValue.toString()};
+
+    return options;
+}
+
+QJsonObject BitTorrent::CategoryOptions::toJSON() const
+{
+    QJsonValue downloadPathValue = QJsonValue::Undefined;
+    if (downloadPath)
+    {
+        if (downloadPath->enabled)
+            downloadPathValue = downloadPath->path;
+        else
+            downloadPathValue = false;
+    }
+
+    return {
+        {OPTION_SAVEPATH, savePath},
+        {OPTION_DOWNLOADPATH, downloadPathValue}
+    };
+}
+
+bool BitTorrent::operator==(const BitTorrent::CategoryOptions::DownloadPathOption &left, const BitTorrent::CategoryOptions::DownloadPathOption &right)
+{
+    return ((left.enabled == right.enabled)
+            && (left.path == right.path));
+}
+
+bool BitTorrent::operator==(const BitTorrent::CategoryOptions &left, const BitTorrent::CategoryOptions &right)
+{
+    return ((left.savePath == right.savePath)
+            && (left.downloadPath == right.downloadPath));
+}

commit 3b77ee1c08fb195d960b9c2478a3e9633ae2b465

WebAPI: fix wrong key used for categories
Regression from 1c0f8b4289b8d298041fe195cdc24f793dce4df5.
Closes #15969.

src/webui/api/synccontroller.cpp
@@ -503,6 +503,8 @@ void SyncController::maindataAction()
     {
         const BitTorrent::CategoryOptions categoryOptions = session->categoryOptions(categoryName);
         QJsonObject category = categoryOptions.toJSON();
+        // adjust it to be compatible with exisitng WebAPI
+        category[QLatin1String("savePath")] = category.take(QLatin1String("save_path"));
         category.insert(QLatin1String("name"), categoryName);
         categories[categoryName] = category.toVariantMap();
     }

src/webui/api/torrentscontroller.cpp
@@ -1285,6 +1285,8 @@ void TorrentsController::categoriesAction()
     {
         const BitTorrent::CategoryOptions categoryOptions = session->categoryOptions(categoryName);
         QJsonObject category = categoryOptions.toJSON();
+        // adjust it to be compatible with exisitng WebAPI
+        category[QLatin1String("savePath")] = category.take(QLatin1String("save_path"));
         category.insert(QLatin1String("name"), categoryName);
         categories[categoryName] = category;
     }

src/webui/webapplication.h
@@ -43,7 +43,7 @@
 #include "base/utils/net.h"
 #include "base/utils/version.h"
 
-inline const Utils::Version<int, 3, 2> API_VERSION {2, 8, 4};
+inline const Utils::Version<int, 3, 2> API_VERSION {2, 8, 5};
 
 class APIController;
 class WebApplication;