1ee61aca3a778d59f80a89659180c5fe5dbc94d8
Eyal Gerber <35224049+Novodes@users.noreply.github.com>
2022-02-08 17:29:33 +0200

    testing

--- src/searchengine/nova/nova2.py
+++ src/searchengine/nova/nova2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+testing
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
 #
