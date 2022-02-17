3755acb63fbc716036f9e9ec695bb9218acc8279
Vladimir Golovnev (Glassez) <glassez@yandex.ru>
2022-02-17 12:04:31 +0300

    Allow to limit max memory working set size

--- src/app/application.h
+++ src/app/application.h
@@ -89,6 +89,11 @@ public:
 
     const QBtCommandLineParameters &commandLineArgs() const;
 
+#ifdef Q_OS_WIN
+    int memoryWorkingSetLimit() const;
+    void setMemoryWorkingSetLimit(int size);
+#endif
+
     // FileLogger properties
     bool isFileLoggerEnabled() const;
     void setFileLoggerEnabled(bool value);
@@ -122,6 +127,9 @@ private slots:
 #endif
 
 private:
+#ifdef Q_OS_WIN
+    void applyMemoryWorkingSetLimit();
+#endif
     void initializeTranslation();
     void processParams(const QStringList &params);
     void runExternalProgram(const BitTorrent::Torrent *torrent) const;
@@ -147,6 +155,9 @@ private:
     QTranslator m_translator;
     QStringList m_paramsQueue;
 
+#ifdef Q_OS_WIN
+    SettingValue<int> m_storeMemoryWorkingSetLimit;
+#endif
     SettingValue<bool> m_storeFileLoggerEnabled;
     SettingValue<bool> m_storeFileLoggerBackup;
     SettingValue<bool> m_storeFileLoggerDeleteOld;
