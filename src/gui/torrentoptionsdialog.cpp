66a7ded55f16902ab9c17e781ac15fb7ea15d877
Aleksandr Cupacenko <apaxuc@gmail.com>
2022-03-05 21:03:59 +0200

    round down conversion for ratio limit spin box

--- src/gui/torrentoptionsdialog.cpp
+++ src/gui/torrentoptionsdialog.cpp
@@ -305,6 +305,7 @@ TorrentOptionsDialog::TorrentOptionsDialog(QWidget *parent, const QVector<BitTor
             ? firstTorrentRatio : session->globalMaxRatio();
     const int maxSeedingTime = (allSameSeedingTime && (firstTorrentSeedingTime >= 0))
             ? firstTorrentSeedingTime : session->globalMaxSeedingMinutes();
+
     m_ui->spinRatioLimit->setValue(maxRatio);
     m_ui->spinTimeLimit->setValue(maxSeedingTime);
 
