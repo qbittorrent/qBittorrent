80e380d3306ebf71ffae14164bb58a718c6d5bdc
Aleksandr Cupacenko <apaxuc@gmail.com>
2022-03-07 15:06:42 +0200

    fix parent name

--- src/gui/doublespinbox.cpp
+++ src/gui/doublespinbox.cpp
@@ -3,6 +3,8 @@
 
 DoubleSpinBox::DoubleSpinBox(QWidget *parent) : QDoubleSpinBox(parent) {}
 
+DoubleSpinBox::~DoubleSpinBox() {}
+
 QString DoubleSpinBox::textFromValue(qreal val) const
 {
     return Utils::String::fromDouble(val, 2);
