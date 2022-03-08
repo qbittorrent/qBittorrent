8efa64d5bea69253949f5239dad1cb1796655bae
Aleksandr Cupacenko <apaxuc@gmail.com>
2022-03-08 15:40:34 +0200

    fix build errors

--- src/base/utils/string.h
+++ src/base/utils/string.h
@@ -68,8 +68,8 @@ namespace Utils::String
 
     QString join(const QList<QStringView> &strings, QStringView separator);
 
-    QString fromDouble(const double n, const int precision);
-    QString fromDouble(const double n, const int precision, const std::function<double (double)>& roundFunc);
+    QString fromDouble(double n, int precision);
+    QString fromDouble(const double n, const int precision, double (*roundFunc)(double));
 
     template <typename T, typename std::enable_if_t<std::is_enum_v<T>, int> = 0>
     QString fromEnum(const T &value)
