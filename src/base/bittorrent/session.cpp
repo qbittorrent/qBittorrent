eb8cd3cfe228072f1a03a76d70226d9034274d59
Vladimir Golovnev (Glassez) <glassez@yandex.ru>
2022-04-18 10:24:56 +0300

    Avoid dereferencing null pointers

--- src/base/bittorrent/session.cpp
+++ src/base/bittorrent/session.cpp
@@ -4150,7 +4150,8 @@ void Session::handleTorrentMetadataReceived(TorrentImpl *const torrent)
     if (!torrentExportDirectory().isEmpty())
     {
 #ifdef QBT_USES_LIBTORRENT2
-        const TorrentInfo torrentInfo {*torrent->nativeHandle().torrent_file_with_hashes()};
+        const std::shared_ptr<lt::torrent_info> completeTorrentInfo = torrent->nativeHandle().torrent_file_with_hashes();
+        const TorrentInfo torrentInfo {*(completeTorrentInfo ? completeTorrentInfo : torrent->nativeHandle().torrent_file())};
 #else
         const TorrentInfo torrentInfo {*torrent->nativeHandle().torrent_file()};
 #endif
@@ -4209,7 +4210,8 @@ void Session::handleTorrentFinished(TorrentImpl *const torrent)
     if (!finishedTorrentExportDirectory().isEmpty())
     {
 #ifdef QBT_USES_LIBTORRENT2
-        const TorrentInfo torrentInfo {*torrent->nativeHandle().torrent_file_with_hashes()};
+        const std::shared_ptr<lt::torrent_info> completeTorrentInfo = torrent->nativeHandle().torrent_file_with_hashes();
+        const TorrentInfo torrentInfo {*(completeTorrentInfo ? completeTorrentInfo : torrent->nativeHandle().torrent_file())};
 #else
         const TorrentInfo torrentInfo {*torrent->nativeHandle().torrent_file()};
 #endif
