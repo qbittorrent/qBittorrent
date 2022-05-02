6b0b817a58b51d462ab6e60f348fd5af03ba46cd
Tom Piccirello <8296030+Piccirello@users.noreply.github.com>
2022-05-02 09:42:27 -0700

    Fix Docker legal notice when using existing config
    If the user is using a pre-existing config, don't update the legal notice.

--- dist/docker/entrypoint.sh
+++ dist/docker/entrypoint.sh
@@ -10,12 +10,12 @@ Session\TempPath=/downloads/temp
 [LegalNotice]
 Accepted=false
 EOF
-fi
 
-if [ "$LEGAL" = "accept" ] ; then
-    sed -i '/^\[LegalNotice\]$/{$!{N;s|\(\[LegalNotice\]\nAccepted=\).*|\1true|}}' /config/qBittorrent/qBittorrent.conf
-else
-    sed -i '/^\[LegalNotice\]$/{$!{N;s|\(\[LegalNotice\]\nAccepted=\).*|\1false|}}' /config/qBittorrent/qBittorrent.conf
+    if [ "$LEGAL" = "accept" ] ; then
+        sed -i '/^\[LegalNotice\]$/{$!{N;s|\(\[LegalNotice\]\nAccepted=\).*|\1true|}}' /config/qBittorrent/qBittorrent.conf
+    else
+        sed -i '/^\[LegalNotice\]$/{$!{N;s|\(\[LegalNotice\]\nAccepted=\).*|\1false|}}' /config/qBittorrent/qBittorrent.conf
+    fi
 fi
 
 HOME="/config" XDG_CONFIG_HOME="/config" XDG_DATA_HOME="/config" qbittorrent-nox --webui-port=$WEBUI_PORT
