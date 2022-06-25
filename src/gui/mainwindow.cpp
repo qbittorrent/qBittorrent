48c29f87898cd9c7bdd769de9a35857353e7c5da
Vladimir Golovnev (Glassez) <glassez@yandex.ru>
2022-06-25 16:39:27 +0300

    Improve D-Bus notifications handling

--- src/gui/mainwindow.cpp
+++ src/gui/mainwindow.cpp
@@ -50,9 +50,8 @@
 #include <QtGlobal>
 #include <QTimer>
 
-#if (defined(Q_OS_UNIX) && !defined(Q_OS_MACOS)) && defined(QT_DBUS_LIB)
-#include <QDBusConnection>
-#include "qtnotify/notifications.h"
+#ifdef QBT_USES_CUSTOMDBUSNOTIFICATIONS
+#include "notifications/dbusnotifier.h"
 #endif
 
 #include "base/bittorrent/session.h"
@@ -147,7 +146,7 @@ MainWindow::MainWindow(IGUIApplication *app, QWidget *parent)
     , m_storeNotificationEnabled(NOTIFICATIONS_SETTINGS_KEY(u"Enabled"_qs))
     , m_storeNotificationTorrentAdded(NOTIFICATIONS_SETTINGS_KEY(u"TorrentAdded"_qs))
     , m_storeExecutionLogTypes(EXECUTIONLOG_SETTINGS_KEY(u"Types"_qs), Log::MsgType::ALL)
-#if (defined(Q_OS_UNIX) && !defined(Q_OS_MACOS)) && defined(QT_DBUS_LIB)
+#ifdef QBT_USES_CUSTOMDBUSNOTIFICATIONS
     , m_storeNotificationTimeOut(NOTIFICATIONS_SETTINGS_KEY(u"Timeout"_qs))
 #endif
 {
@@ -200,6 +199,14 @@ MainWindow::MainWindow(IGUIApplication *app, QWidget *parent)
     {
         m_ui->actionLock->setVisible(true);
     });
+
+#ifdef QBT_USES_CUSTOMDBUSNOTIFICATIONS
+    if (isNotificationsEnabled())
+    {
+        m_notifier = new DBusNotifier(this);
+        connect(m_notifier, &DBusNotifier::messageClicked, this, &MainWindow::balloonClicked);
+    }
+#endif
 
     // Creating Bittorrent session
     updateAltSpeedsBtn(BitTorrent::Session::instance()->isAltGlobalSpeedLimitEnabled());
@@ -527,9 +534,25 @@ bool MainWindow::isNotificationsEnabled() const
     return m_storeNotificationEnabled.get(true);
 }
 
-void MainWindow::setNotificationsEnabled(bool value)
+void MainWindow::setNotificationsEnabled(const bool value)
 {
+    if (m_storeNotificationEnabled == value)
+        return;
+
     m_storeNotificationEnabled = value;
+
+#ifdef QBT_USES_CUSTOMDBUSNOTIFICATIONS
+    if (value)
+    {
+        m_notifier = new DBusNotifier(this);
+        connect(m_notifier, &DBusNotifier::messageClicked, this, &MainWindow::balloonClicked);
+    }
+    else
+    {
+        delete m_notifier;
+        m_notifier = nullptr;
+    }
+#endif
 }
 
 bool MainWindow::isTorrentAddedNotificationsEnabled() const
@@ -1686,27 +1709,8 @@ void MainWindow::showNotificationBalloon(const QString &title, const QString &ms
     if (!isNotificationsEnabled())
         return;
 
-#if (defined(Q_OS_UNIX) && !defined(Q_OS_MACOS)) && defined(QT_DBUS_LIB)
-    OrgFreedesktopNotificationsInterface notifications(u"org.freedesktop.Notifications"_qs
-        , u"/org/freedesktop/Notifications"_qs
-        , QDBusConnection::sessionBus());
-
-    // Testing for 'notifications.isValid()' isn't helpful here.
-    // If the notification daemon is configured to run 'as needed'
-    // the above check can be false if the daemon wasn't started
-    // by another application. In this case DBus will be able to
-    // start the notification daemon and complete our request. Such
-    // a daemon is xfce4-notifyd, DBus autostarts it and after
-    // some inactivity shuts it down. Other DEs, like GNOME, choose
-    // to start their daemons at the session startup and have it sit
-    // idling for the whole session.
-    const QVariantMap hints {{u"desktop-entry"_qs, u"org.qbittorrent.qBittorrent"_qs}};
-    QDBusPendingReply<uint> reply = notifications.Notify(u"qBittorrent"_qs, 0
-        , u"qbittorrent"_qs, title, msg, {}, hints, getNotificationTimeout());
-
-    reply.waitForFinished();
-    if (!reply.isError())
-        return;
+#ifdef QBT_USES_CUSTOMDBUSNOTIFICATIONS
+    m_notifier->showMessage(title, msg, getNotificationTimeout());
 #elif defined(Q_OS_MACOS)
     MacUtils::displayNotification(title, msg);
 #else
@@ -1753,7 +1757,9 @@ void MainWindow::createTrayIcon(const int retries)
         m_systrayIcon->setContextMenu(m_trayIconMenu);
 
         connect(m_systrayIcon, &QSystemTrayIcon::activated, this, &MainWindow::toggleVisibility);
+#ifndef QBT_USES_CUSTOMDBUSNOTIFICATIONS
         connect(m_systrayIcon, &QSystemTrayIcon::messageClicked, this, &MainWindow::balloonClicked);
+#endif
 
         m_systrayIcon->show();
         emit systemTrayIconCreated();
---
+++ src/gui/notifications/dbusnotifier.cpp
@@ -0,0 +1,94 @@
+/*
+ * Bittorrent Client using Qt and libtorrent.
+ * Copyright (C) 2022  Vladimir Golovnev <glassez@yandex.ru>
+ * Copyright (C) 2006  Christophe Dumez <chris@qbittorrent.org>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * In addition, as a special exception, the copyright holders give permission to
+ * link this program with the OpenSSL project's "OpenSSL" library (or with
+ * modified versions of it that use the same license as the "OpenSSL" library),
+ * and distribute the linked executables. You must obey the GNU General Public
+ * License in all respects for all of the code used other than "OpenSSL".  If you
+ * modify file(s), you may extend this exception to your version of the file(s),
+ * but you are not obligated to do so. If you do not wish to do so, delete this
+ * exception statement from your version.
+ */
+
+#include "dbusnotifier.h"
+
+#include <QDBusConnection>
+#include <QDBusPendingCallWatcher>
+#include <QDBusPendingReply>
+
+#include "base/global.h"
+#include "dbusnotificationsinterface.h"
+
+DBusNotifier::DBusNotifier(QObject *parent)
+    : QObject(parent)
+    , m_notificationsInterface {new DBusNotificationsInterface(u"org.freedesktop.Notifications"_qs
+                  , u"/org/freedesktop/Notifications"_qs, QDBusConnection::sessionBus(), this)}
+{
+    // Testing for 'DBusNotificationsInterface::isValid()' isn't helpful here.
+    // If the notification daemon is configured to run 'as needed'
+    // the above check can be false if the daemon wasn't started
+    // by another application. In this case DBus will be able to
+    // start the notification daemon and complete our request. Such
+    // a daemon is xfce4-notifyd, DBus autostarts it and after
+    // some inactivity shuts it down. Other DEs, like GNOME, choose
+    // to start their daemons at the session startup and have it sit
+    // idling for the whole session.
+
+    connect(m_notificationsInterface, &DBusNotificationsInterface::ActionInvoked, this, &DBusNotifier::onActionInvoked);
+    connect(m_notificationsInterface, &DBusNotificationsInterface::NotificationClosed, this, &DBusNotifier::onNotificationClosed);
+}
+
+void DBusNotifier::showMessage(const QString &title, const QString &message, const int timeout)
+{
+    // Assign "default" action to notification to make it clickable
+    const QStringList actions {u"default"_qs, {}};
+    const QVariantMap hints {{u"desktop-entry"_qs, u"org.qbittorrent.qBittorrent"_qs}};
+    const QDBusPendingReply<uint> reply = m_notificationsInterface->notify(u"qBittorrent"_qs, 0
+            , u"qbittorrent"_qs, title, message, actions, hints, timeout);
+    auto *watcher = new QDBusPendingCallWatcher(reply, this);
+    connect(watcher, &QDBusPendingCallWatcher::finished, this, [this](QDBusPendingCallWatcher *self)
+    {
+        const QDBusPendingReply<uint> reply = *self;
+        if (!reply.isError())
+        {
+            const uint messageID = reply.value();
+            m_activeMessages.insert(messageID);
+        }
+
+        self->deleteLater();
+    });
+}
+
+void DBusNotifier::onActionInvoked(const uint messageID, const QString &action)
+{
+    Q_UNUSED(action);
+
+    // Check whether the notification is sent by qBittorrent
+    // to avoid reacting to unrelated notifictions
+    if (m_activeMessages.contains(messageID))
+        emit messageClicked();
+}
+
+void DBusNotifier::onNotificationClosed(const uint messageID, const uint reason)
+{
+    Q_UNUSED(reason);
+
+    m_activeMessages.remove(messageID);
+}
