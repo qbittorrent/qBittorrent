7307097a59ba1949df4eff6185c9811aead0bde9
Jasper <40232406+jaspwr@users.noreply.github.com>
2022-09-12 15:34:12 +1000

    Formatting fix

--- src/gui/properties/trackersadditiondialog.cpp
+++ src/gui/properties/trackersadditiondialog.cpp
@@ -116,7 +116,7 @@ void TrackersAdditionDialog::torrentListDownloadFinished(const Net::DownloadResu
         BitTorrent::TrackerEntry newTracker {line};
         if (!existingTrackers.contains(newTracker))
         {
-            if(nb != 0)
+            if (nb != 0)
                 m_ui->textEditTrackersList->insertPlainText(u"\n"_qs);
             m_ui->textEditTrackersList->insertPlainText(line + u'\n');
             ++nb;
