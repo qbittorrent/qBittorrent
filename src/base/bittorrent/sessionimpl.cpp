41f010dc861483802c092ecb94904b96165c5ba5
Vladimir Golovnev (Glassez) <glassez@yandex.ru>
2022-11-18 15:42:52 +0300

    Don't miss to store metadata of new torrent

--- src/base/bittorrent/sessionimpl.cpp
+++ src/base/bittorrent/sessionimpl.cpp
@@ -5216,6 +5216,8 @@ TorrentImpl *SessionImpl::createTorrent(const lt::torrent_handle &nativeHandle, 
 
     if (isRestored())
     {
+        torrent->saveResumeData(lt::torrent_handle::save_info_dict);
+
         // The following is useless for newly added magnet
         if (torrent->hasMetadata())
         {
