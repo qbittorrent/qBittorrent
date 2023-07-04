213b6e316c99a34de844803a825814bb4d9c1525
Chocobo1 <Chocobo1@users.noreply.github.com>
2023-07-04 19:11:53 +0800

    Don't emit superfluous signal
    `Preferences::apply()` might emit superfluous changed signal even when the settings hasn't
    changed (e.g. not dirty), this commit fixes it.

--- src/base/settingsstorage.cpp
+++ src/base/settingsstorage.cpp
@@ -79,8 +79,10 @@ SettingsStorage *SettingsStorage::instance()
 
 bool SettingsStorage::save()
 {
+    // return `true` only when settings is different AND is saved successfully
+
     const QWriteLocker locker(&m_lock);  // guard for `m_dirty` too
-    if (!m_dirty) return true;
+    if (!m_dirty) return false;
 
     if (!writeNativeSettings())
     {
