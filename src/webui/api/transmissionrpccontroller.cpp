317f3106b7f9d21c47b8c9e3cd34a6f6addbf690
Daniel Kamil Kozar <dkk089@gmail.com>
2024-02-04 18:48:16 +0100

    wip

--- src/webui/api/transmissionrpccontroller.cpp
+++ src/webui/api/transmissionrpccontroller.cpp
@@ -5,9 +5,13 @@
 #include <QJsonObject>
 
 #include "base/bittorrent/session.h"
+#include "base/net/portforwarder.h"
+#include "base/preferences.h"
 #include "base/profile.h"
+#include "base/torrentfileguard.h"
+#include "base/version.h"
+
 #include "apierror.h"
-#include "../webapplication.h"
 
 using namespace Qt::Literals::StringLiterals;
 
@@ -39,7 +43,7 @@ QJsonObject sessionGet(const QJsonObject &args)
 {
     QJsonObject result{};
     const QJsonValue fields = args[u"fields"_s];
-    if (!fields.isNull() && !fields.isArray())
+    if (!fields.isUndefined() && !fields.isArray())
     {
         throw TransmissionAPIError(APIErrorType::BadParams, u"'fields' key in 'arguments' to session-get should be an array"_s);
     }
@@ -64,7 +68,8 @@ QJsonObject sessionGet(const QJsonObject &args)
             result[key] = value_fn();
         }
     };
-    const BitTorrent::Session *session = BitTorrent::Session::instance();
+    const Preferences *const pref = Preferences::instance();
+    const BitTorrent::Session *const session = BitTorrent::Session::instance();
 
     insertIfRequested(u"alt-speed-down"_s, [&] { return session->altGlobalDownloadSpeedLimit(); });
     insertIfRequested(u"alt-speed-enabled"_s, [&] { return session->isAltGlobalSpeedLimitEnabled(); });
@@ -89,6 +94,43 @@ QJsonObject sessionGet(const QJsonObject &args)
         const int val = session->encryption();
         Q_ASSERT(val < static_cast<int>(strings.size()));
         return strings[val];});
+    insertIfRequested(u"idle-seeding-limit-enabled"_s, [&] { return false; }); // TODO
+    insertIfRequested(u"idle-seeding-limit"_s, [&] { return 0; }); // TODO
+    insertIfRequested(u"incomplete-dir-enabled"_s, [&] { return session->isDownloadPathEnabled(); });
+    insertIfRequested(u"incomplete-dir"_s, [&] { return session->downloadPath().toString(); });
+    insertIfRequested(u"lpd-enabled"_s, [&] { return session->isLSDEnabled(); });
+    insertIfRequested(u"peer-limit-global"_s, [&] { return session->maxConnections(); });
+    insertIfRequested(u"peer-limit-per-torrent"_s, [&] { return session->maxConnectionsPerTorrent(); });
+    insertIfRequested(u"peer-port-random-on-start"_s, [&] { return false; }); // functionality deprecated in qbt
+    insertIfRequested(u"peer-port"_s, [&] { return session->port(); });
+    insertIfRequested(u"pex-enabled"_s, [&] { return session->isPeXEnabled(); });
+    insertIfRequested(u"port-forwarding-enabled"_s, [&] { return Net::PortForwarder::instance()->isEnabled(); });
+    insertIfRequested(u"queue-stalled-enabled"_s, [&] { return false; }); // TODO
+    insertIfRequested(u"queue-stalled-minutes"_s, [&] { return 0; }); // TODO
+    insertIfRequested(u"rename-partial-files"_s, [&] { return session->isAppendExtensionEnabled(); });
+    insertIfRequested(u"rpc-version-minimum"_s, [&] { return 18; });
+    insertIfRequested(u"rpc-version-semver"_s, [&] { return u"5.4.0"_s; });
+    insertIfRequested(u"rpc-version"_s, [&] { return 18; });
+    insertIfRequested(u"script-torrent-added-enabled"_s, [&] { return pref->isAutoRunOnTorrentAddedEnabled(); });
+    insertIfRequested(u"script-torrent-added-filename"_s, [&] { return pref->getAutoRunOnTorrentAddedProgram(); });
+    insertIfRequested(u"script-torrent-done-enabled"_s, [&] { return pref->isAutoRunOnTorrentFinishedEnabled(); });
+    insertIfRequested(u"script-torrent-done-filename"_s, [&] { return pref->getAutoRunOnTorrentFinishedProgram(); });
+    insertIfRequested(u"script-torrent-done-seeding-enabled"_s, [&] { return false; }); // not supported in qbt
+    insertIfRequested(u"script-torrent-done-seeding-filename"_s, [&] { return QString{}; }); // not supported in qbt
+    insertIfRequested(u"seed-queue-enabled"_s, [&] { return false; }); // TODO not supported?
+    insertIfRequested(u"seed-queue-size"_s, [&] { return 0; });
+    insertIfRequested(u"seedRatioLimit"_s, [&] { return session->globalMaxRatio(); });
+    insertIfRequested(u"seedRatioLimited"_s, [&] { return session->globalMaxRatio() >= 0.; });
+    insertIfRequested(u"session-id"_s, [&] { return u"foobar"_s; }); // not applicable
+    insertIfRequested(u"speed-limit-down-enabled"_s, [&] { return session->globalDownloadSpeedLimit() > 0; });
+    insertIfRequested(u"speed-limit-down"_s, [&] { return session->globalDownloadSpeedLimit(); });
+    insertIfRequested(u"speed-limit-up-enabled"_s, [&] { return session->globalUploadSpeedLimit() > 0; });
+    insertIfRequested(u"speed-limit-up"_s, [&] { return session->globalUploadSpeedLimit() > 0; });
+    insertIfRequested(u"start-added-torrents"_s, [&] { return !session->isAddTorrentPaused(); });
+    insertIfRequested(u"trash-original-torrent-files"_s, [&] { return TorrentFileGuard::autoDeleteMode() >= TorrentFileGuard::AutoDeleteMode::IfAdded; });
+    insertIfRequested(u"units"_s, [&] { return QJsonObject{}; });
+    insertIfRequested(u"utp-enabled"_s, [&]{return false;}); // not supported in qbt
+    insertIfRequested(u"version"_s, [&]{return QStringLiteral(QBT_VERSION);});
 
     return result;
 }
