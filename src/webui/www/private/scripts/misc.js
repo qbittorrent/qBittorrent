6c7c0ba95fef948448b977ec70f1acd4fa454e96
Chocobo1 <Chocobo1@users.noreply.github.com>
2024-02-19 14:08:28 +0800

    Remove unneeded polyfill
    All browsers already have native support for it.

--- src/webui/www/private/scripts/misc.js
+++ src/webui/www/private/scripts/misc.js
@@ -138,33 +138,6 @@ window.qBittorrent.Misc = (function() {
     };
 
     /*
-     * From: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toISOString
-     */
-    if (!Date.prototype.toISOString) {
-        (function() {
-
-            function pad(number) {
-                if (number < 10) {
-                    return '0' + number;
-                }
-                return number;
-            }
-
-            Date.prototype.toISOString = function() {
-                return this.getUTCFullYear()
-                    + '-' + pad(this.getUTCMonth() + 1)
-                    + '-' + pad(this.getUTCDate())
-                    + 'T' + pad(this.getUTCHours())
-                    + ':' + pad(this.getUTCMinutes())
-                    + ':' + pad(this.getUTCSeconds())
-                    + '.' + (this.getUTCMilliseconds() / 1000).toFixed(3).slice(2, 5)
-                    + 'Z';
-            };
-
-        }());
-    }
-
-    /*
      * JS counterpart of the function in src/misc.cpp
      */
     const parseHtmlLinks = function(text) {
