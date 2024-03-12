3254082ee1cd009319bbbd6a5f596155c32ce3e2
Aliaksei Urbanski <aliaksei.urbanski@gmail.com>
2024-03-12 11:01:59 +0300

    Add the Popularity metric
    I believe it would be nice to have a metric
    that shows how popular a torrent is.
    It's especially helpful for clean up purposes
    if you want to delete the least popular torrents.
    
    Co-authored-by: Chocobo1 <Chocobo1@users.noreply.github.com>

--- src/webui/api/serialize/serialize_torrent.cpp
+++ src/webui/api/serialize/serialize_torrent.cpp
@@ -152,6 +152,7 @@ QVariantMap serialize(const BitTorrent::Torrent &torrent)
         {KEY_TORRENT_MAX_INACTIVE_SEEDING_TIME, torrent.maxInactiveSeedingTime()},
         {KEY_TORRENT_RATIO, adjustRatio(torrent.realRatio())},
         {KEY_TORRENT_RATIO_LIMIT, torrent.ratioLimit()},
+        {KEY_TORRENT_POPULARITY, torrent.popularity()},
         {KEY_TORRENT_SEEDING_TIME_LIMIT, torrent.seedingTimeLimit()},
         {KEY_TORRENT_INACTIVE_SEEDING_TIME_LIMIT, torrent.inactiveSeedingTimeLimit()},
         {KEY_TORRENT_LAST_SEEN_COMPLETE_TIME, Utils::DateTime::toSecsSinceEpoch(torrent.lastSeenComplete())},
