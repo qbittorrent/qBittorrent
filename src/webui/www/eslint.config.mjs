e2d6cd31b2a8781a9242281ef23f09b3f91631f2
Chocobo1 <Chocobo1@users.noreply.github.com>
2024-04-06 15:28:33 +0800

    WebUI: migrate to ESLint v9

---
+++ src/webui/www/eslint.config.mjs
@@ -0,0 +1,41 @@
+import Globals from 'globals';
+import Html from 'eslint-plugin-html';
+import Js from '@eslint/js';
+import Stylistic from '@stylistic/eslint-plugin';
+
+export default [
+    Js.configs.recommended,
+    Stylistic.configs["disable-legacy"],
+    {
+        files: [
+            "**/*.html",
+            "**/*.js",
+            "**/*.mjs"
+        ],
+        languageOptions: {
+            ecmaVersion: 2022,
+            globals: {
+                ...Globals.browser
+            }
+        },
+        plugins: {
+            Html,
+            Stylistic
+        },
+        rules: {
+            "eqeqeq": "error",
+            "no-undef": "off",
+            "no-unused-vars": "off",
+            "Stylistic/no-mixed-operators": [
+                "error",
+                {
+                    "groups": [
+                        ["&", "|", "^", "~", "<<", ">>", ">>>", "==", "!=", "===", "!==", ">", ">=", "<", "<=", "&&", "||", "in", "instanceof"]
+                    ]
+                }
+            ],
+            "Stylistic/nonblock-statement-body-position": ["error", "below"],
+            "Stylistic/semi": "error"
+        }
+    }
+];
