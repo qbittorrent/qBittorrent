05416458db79e050f8500e1d543148ce4cf2ecdb
ManiMatter <124743318+ManiMatter@users.noreply.github.com>
2024-05-02 12:04:03 +0200

    WebAPI: Provide "isPrivate" flag via "torrents/info" endpoint
    PR #20686.

--- src/webui/api/serialize/serialize_torrent.h
+++ src/webui/api/serialize/serialize_torrent.h
@@ -93,5 +93,6 @@ inline const QString KEY_TORRENT_SEEDING_TIME = u"seeding_time"_s;
 inline const QString KEY_TORRENT_AVAILABILITY = u"availability"_s;
 inline const QString KEY_TORRENT_REANNOUNCE = u"reannounce"_s;
 inline const QString KEY_TORRENT_COMMENT = u"comment"_s;
+inline const QString KEY_TORRENT_ISPRIVATE = u"is_private"_s;
 
 QVariantMap serialize(const BitTorrent::Torrent &torrent);
