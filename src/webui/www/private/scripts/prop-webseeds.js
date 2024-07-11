f51bc3a0696875901fa569ac4574858b4835cb60
Thomas Piccirello <thomas@piccirello.com>
2024-07-11 04:32:21 -0700

    Extend standard DynamicTable for WebUI web seeds table
    This makes it easier to add a context menu to rows.

--- src/webui/www/private/scripts/prop-webseeds.js
+++ src/webui/www/private/scripts/prop-webseeds.js
@@ -36,55 +36,7 @@ window.qBittorrent.PropWebseeds ??= (() => {
         };
     };
 
-    const webseedsDynTable = new Class({
-
-        initialize: function() {},
-
-        setup: function(table) {
-            this.table = $(table);
-            this.rows = new Hash();
-        },
-
-        removeRow: function(url) {
-            if (this.rows.has(url)) {
-                this.rows.get(url).destroy();
-                this.rows.erase(url);
-                return true;
-            }
-            return false;
-        },
-
-        removeAllRows: function() {
-            this.rows.each((tr, url) => {
-                this.removeRow(url);
-            });
-        },
-
-        updateRow: function(tr, row) {
-            const tds = tr.getElements("td");
-            for (let i = 0; i < row.length; ++i)
-                tds[i].textContent = row[i];
-            return true;
-        },
-
-        insertRow: function(row) {
-            const url = row[0];
-            if (this.rows.has(url)) {
-                const tableRow = this.rows.get(url);
-                this.updateRow(tableRow, row);
-                return;
-            }
-            // this.removeRow(id);
-            const tr = new Element("tr");
-            this.rows.set(url, tr);
-            for (let i = 0; i < row.length; ++i) {
-                const td = document.createElement("td");
-                td.textContent = row[i];
-                tr.appendChild(td);
-            }
-            tr.injectInside(this.table);
-        },
-    });
+    const torrentWebseedsTable = new window.qBittorrent.DynamicTable.TorrentWebseedsTable();
 
     let current_hash = "";
 
@@ -97,41 +49,41 @@ window.qBittorrent.PropWebseeds ??= (() => {
         }
         const new_hash = torrentsTable.getCurrentTorrentID();
         if (new_hash === "") {
-            wsTable.removeAllRows();
+            torrentWebseedsTable.clear();
             clearTimeout(loadWebSeedsDataTimer);
             loadWebSeedsDataTimer = loadWebSeedsData.delay(10000);
             return;
         }
         if (new_hash !== current_hash) {
-            wsTable.removeAllRows();
+            torrentWebseedsTable.clear();
             current_hash = new_hash;
         }
-        const url = new URI("api/v2/torrents/webseeds?hash=" + current_hash);
         new Request.JSON({
-            url: url,
+            url: new URI("api/v2/torrents/webseeds").setData("hash", current_hash),
             method: "get",
             noCache: true,
-            onFailure: function() {
-                $("error_div").textContent = "QBT_TR(qBittorrent client is not reachable)QBT_TR[CONTEXT=HttpServer]";
+            onComplete: function() {
                 clearTimeout(loadWebSeedsDataTimer);
-                loadWebSeedsDataTimer = loadWebSeedsData.delay(20000);
+                loadWebSeedsDataTimer = loadWebSeedsData.delay(10000);
             },
             onSuccess: function(webseeds) {
-                $("error_div").textContent = "";
+                const selectedWebseeds = torrentWebseedsTable.selectedRowsIds();
+                torrentWebseedsTable.clear();
+
                 if (webseeds) {
                     // Update WebSeeds data
                     webseeds.each((webseed) => {
-                        const row = [];
-                        row.length = 1;
-                        row[0] = webseed.url;
-                        wsTable.insertRow(row);
+                        torrentWebseedsTable.updateRowData({
+                            rowId: webseed.url,
+                            url: webseed.url,
+                        });
                     });
                 }
-                else {
-                    wsTable.removeAllRows();
-                }
-                clearTimeout(loadWebSeedsDataTimer);
-                loadWebSeedsDataTimer = loadWebSeedsData.delay(10000);
+
+                torrentWebseedsTable.updateTable(false);
+
+                if (selectedWebseeds.length > 0)
+                    torrentWebseedsTable.reselectRows(selectedWebseeds);
             }
         }).send();
     };
@@ -142,8 +94,7 @@ window.qBittorrent.PropWebseeds ??= (() => {
         loadWebSeedsData();
     };
 
-    const wsTable = new webseedsDynTable();
-    wsTable.setup($("webseedsTable"));
+    torrentWebseedsTable.setup("torrentWebseedsTableDiv", "torrentWebseedsTableFixedHeaderDiv");
 
     return exports();
 })();
