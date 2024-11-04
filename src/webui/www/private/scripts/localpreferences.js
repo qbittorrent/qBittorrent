6db0de90eef0914da1f8bcf2d7fb06c11ccb57fc
skomerko <168652295+skomerko@users.noreply.github.com>
2024-11-04 21:28:34 +0100

    WebUI: Use modern class syntax to create LocalPreferences class

--- src/webui/www/private/scripts/localpreferences.js
+++ src/webui/www/private/scripts/localpreferences.js
@@ -36,24 +36,24 @@ window.qBittorrent.LocalPreferences ??= (() => {
         };
     };
 
-    const LocalPreferencesClass = new Class({
-        get: (key, defaultValue) => {
+    class LocalPreferencesClass {
+        get(key, defaultValue) {
             const value = localStorage.getItem(key);
             return ((value === null) && (defaultValue !== undefined))
                 ? defaultValue
                 : value;
-        },
+        }
 
-        set: (key, value) => {
+        set(key, value) {
             try {
                 localStorage.setItem(key, value);
             }
             catch (err) {
                 console.error(err);
             }
-        },
+        }
 
-        remove: (key) => {
+        remove(key) {
             try {
                 localStorage.removeItem(key);
             }
@@ -61,7 +61,7 @@ window.qBittorrent.LocalPreferences ??= (() => {
                 console.error(err);
             }
         }
-    });
+    };
 
     return exports();
 })();
