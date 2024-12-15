eb2eea8d345173311908031f8c6fe7ff3e74e95f
Vladimir Golovnev <glassez@yandex.ru>
2024-12-15 14:07:36 +0300

    Avoid race condition when update tracker entries
    PR #21995.

--- src/base/bittorrent/sessionimpl.h
+++ src/base/bittorrent/sessionimpl.h
@@ -42,6 +42,7 @@
 #include <QHash>
 #include <QList>
 #include <QMap>
+#include <QMutex>
 #include <QPointer>
 #include <QSet>
 #include <QThreadPool>
@@ -799,6 +800,7 @@ namespace BitTorrent
         // This field holds amounts of peers reported by trackers in their responses to announces
         // (torrent.tracker_name.tracker_local_endpoint.protocol_version.num_peers)
         QHash<lt::torrent_handle, QHash<std::string, QHash<lt::tcp::endpoint, QMap<int, int>>>> m_updatedTrackerStatuses;
+        QMutex m_updatedTrackerStatusesMutex;
 
         // I/O errored torrents
         QSet<TorrentID> m_recentErroredTorrents;
