2a087166264e26e34769703f18b2fb2a6caab9c9
Luke Memet <lukememet@gmail.com>
2025-02-15 15:57:18 -0500

    revisions

--- src/gui/macosshiftclickhandler.h
+++ src/gui/macosshiftclickhandler.h
@@ -1,6 +1,6 @@
 /*
  * Bittorrent Client using Qt and libtorrent.
- * Copyright (C) 2025  Luke Memet <lukemmtt@users.noreply.github.com>
+ * Copyright (C) 2025  Luke Memet (lukemmtt)
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
