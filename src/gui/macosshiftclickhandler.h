d028f46fab755d3b0699853f9ad5d09372f7faf7
Luke Memet <1598289+lukemmtt@users.noreply.github.com>
2025-02-19 05:52:51 -0500

    Fix shift-click selection on macOS
    PR #22284.
    Closes #16818.

---
+++ src/gui/macosshiftclickhandler.h
@@ -0,0 +1,50 @@
+/*
+ * Bittorrent Client using Qt and libtorrent.
+ * Copyright (C) 2025  Luke Memet (lukemmtt)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * In addition, as a special exception, the copyright holders give permission to
+ * link this program with the OpenSSL project's "OpenSSL" library (or with
+ * modified versions of it that use the same license as the "OpenSSL" library),
+ * and distribute the linked executables. You must obey the GNU General Public
+ * License in all respects for all of the code used other than "OpenSSL".  If you
+ * modify file(s), you may extend this exception to your version of the file(s),
+ * but you are not obligated to do so. If you do not wish to do so, delete this
+ * exception statement from your version.
+ */
+
+#pragma once
+
+#include <QObject>
+#include <QPersistentModelIndex>
+
+class QTreeView;
+
+// Workaround for QTBUG-115838: Shift-click range selection not working properly on macOS
+class MacOSShiftClickHandler final : public QObject
+{
+    Q_OBJECT
+    Q_DISABLE_COPY_MOVE(MacOSShiftClickHandler)
+
+public:
+    explicit MacOSShiftClickHandler(QTreeView *treeView);
+
+private:
+    bool eventFilter(QObject *watched, QEvent *event) override;
+
+    QTreeView *m_treeView = nullptr;
+    QPersistentModelIndex m_lastClickedIndex;
+};
