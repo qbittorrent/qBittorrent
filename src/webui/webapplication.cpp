d058b825ce2ae25dae0951ad5ebc854017299ce9
Hanabishi <13597663+HanabishiRecca@users.noreply.github.com>
2025-03-21 15:28:27 +0000

    WebUI: Fallback alternative UI to the default in case of problems

--- src/webui/webapplication.cpp
+++ src/webui/webapplication.cpp
@@ -75,6 +75,7 @@ const QString DEFAULT_SESSION_COOKIE_NAME = u"SID"_s;
 const QString WWW_FOLDER = u":/www"_s;
 const QString PUBLIC_FOLDER = u"/public"_s;
 const QString PRIVATE_FOLDER = u"/private"_s;
+const QString INDEX_HTML = u"/index.html"_s;
 
 using namespace std::chrono_literals;
 
@@ -155,6 +156,18 @@ namespace
 
         return true;
     }
+
+#ifdef DISABLE_GUI
+    void fallbackAltUI(const QString &path)
+    {
+        if (path != INDEX_HTML)
+            return;
+
+        auto *preferences = Preferences::instance();
+        preferences->setAltWebUIEnabled(false);
+        preferences->apply();
+    }
+#endif
 }
 
 WebApplication::WebApplication(IApplication *app, QObject *parent)
@@ -213,7 +226,7 @@ void WebApplication::sendWebUIFile()
 
     const QString path = (request().path != u"/")
         ? request().path
-        : u"/index.html"_s;
+        : INDEX_HTML;
 
     Path localPath = m_rootFolder
                 / Path(session() ? PRIVATE_FOLDER : PUBLIC_FOLDER)
@@ -227,7 +240,12 @@ void WebApplication::sendWebUIFile()
     if (m_isAltUIUsed)
     {
         if (!Utils::Fs::isRegularFile(localPath))
+        {
+#ifdef DISABLE_GUI
+            fallbackAltUI(path);
+#endif
             throw InternalServerErrorHTTPError(tr("Unacceptable file type, only regular file is allowed."));
+        }
 
         const QString rootFolder = m_rootFolder.data();
 
@@ -235,7 +253,12 @@ void WebApplication::sendWebUIFile()
         while (fileInfo.path() != rootFolder)
         {
             if (fileInfo.isSymLink())
+            {
+#ifdef DISABLE_GUI
+                fallbackAltUI(path);
+#endif
                 throw InternalServerErrorHTTPError(tr("Symlinks inside alternative UI folder are forbidden."));
+            }
 
             fileInfo.setFile(fileInfo.path());
         }
