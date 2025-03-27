9edf6cedf61a7e131cfa3c243f24f81e61abebcd
Chocobo1 <Chocobo1@users.noreply.github.com>
2025-03-27 18:37:12 +0800

    WebAPI: bump version
    Related: https://github.com/qbittorrent/qBittorrent/pull/22460#issuecomment-2748821812

--- src/webui/webapplication.h
+++ src/webui/webapplication.h
@@ -53,7 +53,7 @@
 #include "base/utils/version.h"
 #include "api/isessionmanager.h"
 
-inline const Utils::Version<3, 2> API_VERSION {2, 11, 5};
+inline const Utils::Version<3, 2> API_VERSION {2, 11, 6};
 
 class APIController;
 class AuthController;
