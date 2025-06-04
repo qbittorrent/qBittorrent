58809228ca6eb1c0a843e870d1a5f432638290da
Vladimir Golovnev (Glassez) <glassez@yandex.ru>
2025-06-04 15:48:55 +0300

    Fix command line placeholders description

--- src/gui/optionsdialog.cpp
+++ src/gui/optionsdialog.cpp
@@ -663,7 +663,7 @@ void OptionsDialog::loadDownloadsTabOptions()
 #else
     m_ui->autoRunConsole->hide();
 #endif
-    const auto autoRunStr = u"%1\n    %2\n    %3\n    %4\n    %5\n    %6\n    %7\n    %8\n    %9\n    %10\n    %11\n    %12\n    %13\n%14"_s
+    const auto autoRunStr = u"%1\n    %2\n    %3\n    %4\n    %5\n    %6\n    %7\n    %8\n    %9\n    %10\n    %11\n    %12\n    %13\n    %14\n%15"_s
         .arg(tr("Supported parameters (case sensitive):")
             , tr("%N: Torrent name")
             , tr("%L: Category")
