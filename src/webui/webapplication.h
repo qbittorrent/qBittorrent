7ac160a481e8cd185f6623cde1362dd3011ea028
Thomas Piccirello <thomas@piccirello.com>
2025-06-10 12:48:08 -0700

    Bump WebAPI version

--- src/webui/webapplication.h
+++ src/webui/webapplication.h
@@ -53,7 +53,7 @@
 #include "base/utils/version.h"
 #include "api/isessionmanager.h"
 
-inline const Utils::Version<3, 2> API_VERSION {2, 11, 8};
+inline const Utils::Version<3, 2> API_VERSION {2, 11, 9};
 
 class APIController;
 class AuthController;
