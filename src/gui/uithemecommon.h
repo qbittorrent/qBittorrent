qbittorrent/qBittorrent

commit 6e1b5ec18b31ff38c891f9db44294f555aa4233d

Don't miss to declare some of the color IDs
PR #22330.
Closes #22326.

src/gui/uithemecommon.h
@@ -80,7 +80,33 @@ inline QHash<QString, UIThemeColor> defaultUIThemeColors()
         {u"TransferList.StoppedUploading"_s, {Color::Primer::Light::doneFg, Color::Primer::Dark::doneFg}},
         {u"TransferList.Moving"_s, {Color::Primer::Light::successFg, Color::Primer::Dark::successFg}},
         {u"TransferList.MissingFiles"_s, {Color::Primer::Light::dangerFg, Color::Primer::Dark::dangerFg}},
-        {u"TransferList.Error"_s, {Color::Primer::Light::dangerFg, Color::Primer::Dark::dangerFg}}
+        {u"TransferList.Error"_s, {Color::Primer::Light::dangerFg, Color::Primer::Dark::dangerFg}},
+
+        {u"Palette.Window"_s, {{}, {}}},
+        {u"Palette.WindowText"_s, {{}, {}}},
+        {u"Palette.Base"_s, {{}, {}}},
+        {u"Palette.AlternateBase"_s, {{}, {}}},
+        {u"Palette.Text"_s, {{}, {}}},
+        {u"Palette.ToolTipBase"_s, {{}, {}}},
+        {u"Palette.ToolTipText"_s, {{}, {}}},
+        {u"Palette.BrightText"_s, {{}, {}}},
+        {u"Palette.Highlight"_s, {{}, {}}},
+        {u"Palette.HighlightedText"_s, {{}, {}}},
+        {u"Palette.Button"_s, {{}, {}}},
+        {u"Palette.ButtonText"_s, {{}, {}}},
+        {u"Palette.Link"_s, {{}, {}}},
+        {u"Palette.LinkVisited"_s, {{}, {}}},
+        {u"Palette.Light"_s, {{}, {}}},
+        {u"Palette.Midlight"_s, {{}, {}}},
+        {u"Palette.Mid"_s, {{}, {}}},
+        {u"Palette.Dark"_s, {{}, {}}},
+        {u"Palette.Shadow"_s, {{}, {}}},
+        {u"Palette.WindowTextDisabled"_s, {{}, {}}},
+        {u"Palette.TextDisabled"_s, {{}, {}}},
+        {u"Palette.ToolTipTextDisabled"_s, {{}, {}}},
+        {u"Palette.BrightTextDisabled"_s, {{}, {}}},
+        {u"Palette.HighlightedTextDisabled"_s, {{}, {}}},
+        {u"Palette.ButtonTextDisabled"_s, {{}, {}}}
     };
 }
 

commit dd4a2eb583cbb59e6c6a63a1c77a6de23dc0b86f

Don't expose palette colors in UI theme editor
PR #22923.
Fixes regression introduced by #22330.

src/gui/uithemecommon.h
@@ -80,8 +80,14 @@ inline QHash<QString, UIThemeColor> defaultUIThemeColors()
         {u"TransferList.StoppedUploading"_s, {Color::Primer::Light::doneFg, Color::Primer::Dark::doneFg}},
         {u"TransferList.Moving"_s, {Color::Primer::Light::successFg, Color::Primer::Dark::successFg}},
         {u"TransferList.MissingFiles"_s, {Color::Primer::Light::dangerFg, Color::Primer::Dark::dangerFg}},
-        {u"TransferList.Error"_s, {Color::Primer::Light::dangerFg, Color::Primer::Dark::dangerFg}},
+        {u"TransferList.Error"_s, {Color::Primer::Light::dangerFg, Color::Primer::Dark::dangerFg}}
+    };
+}
 
+// Palette isn't customizable in default theme
+inline QHash<QString, UIThemeColor> defaultPaletteColors()
+{
+    return {
         {u"Palette.Window"_s, {{}, {}}},
         {u"Palette.WindowText"_s, {{}, {}}},
         {u"Palette.Base"_s, {{}, {}}},

src/gui/uithemedialog.cpp
@@ -273,8 +273,6 @@ void UIThemeDialog::loadColors()
     int row = 2;
     for (const QString &id : colorIDs)
     {
-        if (id == u"Log.Normal")
-            qDebug() << "!!!!!!!";
         m_ui->colorsLayout->addWidget(new QLabel(id), row, 0);
 
         const UIThemeColor &defaultColor = defaultColors.value(id);

src/gui/uithemesource.cpp
@@ -105,6 +105,8 @@ DefaultThemeSource::DefaultThemeSource()
     , m_colors {defaultUIThemeColors()}
 {
     loadColors();
+    // Palette isn't customizable in default theme
+    m_colors.insert(defaultPaletteColors());
 }
 
 QByteArray DefaultThemeSource::readStyleSheet()