e3141ce4493664cf0becaefda72fc6d85b0cd012
Bark <32035685+userwiths@users.noreply.github.com>
2025-07-20 11:23:22 +0300

    WebUI: Allow closing dialogs with Escape key
    Tested on the following WebUI Modals:
    1. Delete
    2. Rename files
    3. Settings
    4. About
    5. Statistics
    6. Add torrent
    
    PR #22920.
    Closes #13891.

--- src/webui/www/private/scripts/client.js
+++ src/webui/www/private/scripts/client.js
@@ -1778,6 +1778,24 @@ window.addEventListener("DOMContentLoaded", (event) => {
                 deleteSelectedTorrentsFN(event.shiftKey);
                 break;
 
+            case "Escape": {
+                if (event.target.isContentEditable)
+                    return;
+
+                event.preventDefault();
+                const modalInstances = Object.values(MochaUI.Windows.instances);
+                if (modalInstances.length <= 0)
+                    return;
+
+                // MochaUI.currentModal does not update after a modal is closed
+                const focusedModal = modalInstances.find((modal) => {
+                    return modal.windowEl.hasClass("isFocused");
+                });
+                if (focusedModal !== undefined)
+                    focusedModal.close();
+                break;
+            }
+
             case "f":
             case "F":
                 if (event.ctrlKey || event.metaKey) {
