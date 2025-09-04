943f26b2066999ba65b667797a9df599107a2496
Thomas Piccirello <thomas@piccirello.com>
2025-09-04 14:24:18 -0700

    WebUI: update cookie name when prefs change
    Previously, the cookie name wouldn't be updated until qBittorrent was restarted.

--- src/webui/webapplication.cpp
+++ src/webui/webapplication.cpp
@@ -163,17 +163,6 @@ WebApplication::WebApplication(IApplication *app, QObject *parent)
 
     configure();
     connect(Preferences::instance(), &Preferences::changed, this, &WebApplication::configure);
-
-    m_sessionCookieName = Preferences::instance()->getWebAPISessionCookieName();
-    if (!isValidCookieName(m_sessionCookieName))
-    {
-        if (!m_sessionCookieName.isEmpty())
-        {
-            LogMsg(tr("Unacceptable session cookie name is specified: '%1'. Default one is used.")
-                   .arg(m_sessionCookieName), Log::WARNING);
-        }
-        m_sessionCookieName = DEFAULT_SESSION_COOKIE_NAME;
-    }
 }
 
 WebApplication::~WebApplication()
@@ -461,6 +450,17 @@ void WebApplication::configure()
     m_isAuthSubnetWhitelistEnabled = pref->isWebUIAuthSubnetWhitelistEnabled();
     m_authSubnetWhitelist = pref->getWebUIAuthSubnetWhitelist();
     m_sessionTimeout = pref->getWebUISessionTimeout();
+
+    m_sessionCookieName = pref->getWebAPISessionCookieName();
+    if (!isValidCookieName(m_sessionCookieName))
+    {
+        if (!m_sessionCookieName.isEmpty())
+        {
+            LogMsg(tr("Unacceptable session cookie name is specified: '%1'. Default one is used.")
+                   .arg(m_sessionCookieName), Log::WARNING);
+        }
+        m_sessionCookieName = DEFAULT_SESSION_COOKIE_NAME;
+    }
 
     m_domainList = pref->getServerDomains().split(u';', Qt::SkipEmptyParts);
     std::for_each(m_domainList.begin(), m_domainList.end(), [](QString &entry) { entry = entry.trimmed(); });
