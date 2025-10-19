bdd88dea131a11d15e3cd56e884c3b7a859065f6
cocopaw <223083270+cocopaw@users.noreply.github.com>
2025-10-19 18:10:17 +1100

    Remove Utils::Fs::isValidFileName from TorrentsController::renameFileAction

--- src/webui/api/torrentscontroller.cpp
+++ src/webui/api/torrentscontroller.cpp
@@ -1990,10 +1990,6 @@ void TorrentsController::renameFileAction()
 {
     requireParams({u"hash"_s, u"oldPath"_s, u"newPath"_s});
 
-    const QString newFileName = QFileInfo(params()[u"newPath"_s]).fileName();
-    if (!Utils::Fs::isValidFileName(newFileName))
-        throw APIError(APIErrorType::Conflict, tr("File name has invalid characters"));
-
     const auto id = BitTorrent::TorrentID::fromString(params()[u"hash"_s]);
     BitTorrent::Torrent *const torrent = BitTorrent::Session::instance()->getTorrent(id);
     if (!torrent)
@@ -2017,10 +2013,6 @@ void TorrentsController::renameFileAction()
 void TorrentsController::renameFolderAction()
 {
     requireParams({u"hash"_s, u"oldPath"_s, u"newPath"_s});
-
-    const QString newFolderName = QFileInfo(params()[u"newPath"_s]).fileName();
-    if (!Utils::Fs::isValidFileName(newFolderName))
-        throw APIError(APIErrorType::Conflict, tr("Folder name has invalid characters"));
 
     const auto id = BitTorrent::TorrentID::fromString(params()[u"hash"_s]);
     BitTorrent::Torrent *const torrent = BitTorrent::Session::instance()->getTorrent(id);
