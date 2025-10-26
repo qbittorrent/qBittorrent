qbittorrent/qBittorrent

commit 02892d12501e9ba96cb5a0375da42d447bc5ece2

WebUI: Add new Add Torrent experience
This PR uses the new APIs from #21015 to provide a WebUI Add Torrent experience more closely matching the GUI's.

New functionality:
- View torrent size, date, infohash, files, etc.
- Reprioritize and ignore files before adding
- Specify tags when adding torrent
- Specify save path for incomplete torrent

Closes #20557, closes #10997, closes #12499, closes #14201, closes #15071, closes #15718, closes #16207.
PR #21645.

src/webui/www/private/scripts/dynamicTable.js
@@ -44,6 +44,7 @@ window.qBittorrent.DynamicTable ??= (() => {
             TorrentTrackersTable: TorrentTrackersTable,
             BulkRenameTorrentFilesTable: BulkRenameTorrentFilesTable,
             TorrentFilesTable: TorrentFilesTable,
+            AddTorrentFilesTable: AddTorrentFilesTable,
             LogMessageTable: LogMessageTable,
             LogPeerTable: LogPeerTable,
             RssFeedTable: RssFeedTable,
@@ -65,6 +66,9 @@ window.qBittorrent.DynamicTable ??= (() => {
     };
 
     let DynamicTableHeaderContextMenuClass = null;
+
+    if (typeof LocalPreferences === "undefined")
+        window.LocalPreferences = new window.qBittorrent.LocalPreferences.LocalPreferences();
 
     class DynamicTable {
         setup(dynamicTableDivId, dynamicTableFixedHeaderDivId, contextMenu, useVirtualList = false) {
@@ -2184,12 +2188,13 @@ window.qBittorrent.DynamicTable ??= (() => {
         populateTable(root) {
             this.fileTree.setRoot(root);
             root.children.each((node) => {
-                this.#addNodeToTable(node, 0);
+                this.#addNodeToTable(node, 0, root);
             });
         }
 
-        #addNodeToTable(node, depth) {
+        #addNodeToTable(node, depth, parent) {
             node.depth = depth;
+            node.parent = parent;
 
             if (node.isFolder) {
                 const data = {
@@ -2212,7 +2217,7 @@ window.qBittorrent.DynamicTable ??= (() => {
             }
 
             node.children.each((child) => {
-                this.#addNodeToTable(child, depth + 1);
+                this.#addNodeToTable(child, depth + 1, node);
             });
         }
 
@@ -2719,12 +2724,13 @@ window.qBittorrent.DynamicTable ??= (() => {
         populateTable(root) {
             this.fileTree.setRoot(root);
             root.children.each((node) => {
-                this.#addNodeToTable(node, 0);
+                this.#addNodeToTable(node, 0, root);
             });
         }
 
-        #addNodeToTable(node, depth) {
+        #addNodeToTable(node, depth, parent) {
             node.depth = depth;
+            node.parent = parent;
 
             if (node.isFolder) {
                 if (!this.collapseState.has(node.rowId))
@@ -2735,7 +2741,7 @@ window.qBittorrent.DynamicTable ??= (() => {
                     checked: node.checked,
                     remaining: node.remaining,
                     progress: node.progress,
-                    priority: window.qBittorrent.PropFiles.normalizePriority(node.priority),
+                    priority: window.qBittorrent.TorrentContent.normalizePriority(node.priority),
                     availability: node.availability,
                     fileId: -1,
                     name: node.name
@@ -2752,7 +2758,7 @@ window.qBittorrent.DynamicTable ??= (() => {
             }
 
             node.children.each((child) => {
-                this.#addNodeToTable(child, depth + 1);
+                this.#addNodeToTable(child, depth + 1, node);
             });
         }
 
@@ -2813,9 +2819,9 @@ window.qBittorrent.DynamicTable ??= (() => {
 
                 const downloadCheckbox = td.children[1];
                 if (downloadCheckbox === undefined)
-                    td.append(window.qBittorrent.PropFiles.createDownloadCheckbox(id, row.full_data.fileId, value));
+                    td.append(window.qBittorrent.TorrentContent.createDownloadCheckbox(id, row.full_data.fileId, value));
                 else
-                    window.qBittorrent.PropFiles.updateDownloadCheckbox(downloadCheckbox, id, row.full_data.fileId, value);
+                    window.qBittorrent.TorrentContent.updateDownloadCheckbox(downloadCheckbox, id, row.full_data.fileId, value);
 
             };
             this.columns["checked"].staticWidth = 50;
@@ -2887,16 +2893,18 @@ window.qBittorrent.DynamicTable ??= (() => {
             this.columns["size"].updateTd = displaySize;
 
             // progress
-            this.columns["progress"].updateTd = function(td, row) {
-                const value = Number(this.getRowValue(row));
+            if (this.columns["progress"] !== undefined) {
+                this.columns["progress"].updateTd = function(td, row) {
+                    const value = Number(this.getRowValue(row));
 
-                const progressBar = td.firstElementChild;
-                if (progressBar === null)
-                    td.append(new window.qBittorrent.ProgressBar.ProgressBar(value));
-                else
-                    progressBar.setValue(value);
-            };
-            this.columns["progress"].staticWidth = 100;
+                    const progressBar = td.firstElementChild;
+                    if (progressBar === null)
+                        td.append(new window.qBittorrent.ProgressBar.ProgressBar(value));
+                    else
+                        progressBar.setValue(value);
+                };
+                this.columns["progress"].staticWidth = 100;
+            }
 
             // priority
             this.columns["priority"].updateTd = function(td, row) {
@@ -2905,15 +2913,17 @@ window.qBittorrent.DynamicTable ??= (() => {
 
                 const priorityCombo = td.firstElementChild;
                 if (priorityCombo === null)
-                    td.append(window.qBittorrent.PropFiles.createPriorityCombo(id, row.full_data.fileId, value));
+                    td.append(window.qBittorrent.TorrentContent.createPriorityCombo(id, row.full_data.fileId, value));
                 else
-                    window.qBittorrent.PropFiles.updatePriorityCombo(priorityCombo, id, row.full_data.fileId, value);
+                    window.qBittorrent.TorrentContent.updatePriorityCombo(priorityCombo, id, row.full_data.fileId, value);
             };
             this.columns["priority"].staticWidth = 140;
 
             // remaining, availability
-            this.columns["remaining"].updateTd = displaySize;
-            this.columns["availability"].updateTd = displayPercentage;
+            if (this.columns["remaining"] !== undefined)
+                this.columns["remaining"].updateTd = displaySize;
+            if (this.columns["availability"] !== undefined)
+                this.columns["availability"].updateTd = displayPercentage;
         }
 
         #sortNodesByColumn(root, column) {
@@ -3065,6 +3075,17 @@ window.qBittorrent.DynamicTable ??= (() => {
                         break;
                 }
             });
+        }
+    }
+
+    class AddTorrentFilesTable extends TorrentFilesTable {
+        initColumns() {
+            this.newColumn("checked", "", "", 50, true);
+            this.newColumn("name", "", "QBT_TR(Name)QBT_TR[CONTEXT=TrackerListWidget]", 190, true);
+            this.newColumn("size", "", "QBT_TR(Total Size)QBT_TR[CONTEXT=TrackerListWidget]", 75, true);
+            this.newColumn("priority", "", "QBT_TR(Download Priority)QBT_TR[CONTEXT=TrackerListWidget]", 140, true);
+
+            this.initColumnsFunctions();
         }
     }
 
@@ -3246,7 +3267,8 @@ window.qBittorrent.DynamicTable ??= (() => {
                 if (!tr)
                     return;
 
-                showDownloadPage([this.getRow(tr.rowId).full_data.torrentURL]);
+                const { name, torrentURL } = this._this.rows.get(this.rowId).full_data;
+                qBittorrent.Client.createAddTorrentWindow(name, torrentURL);
             });
         }
         updateRow(tr, fullUpdate) {

src/webui/webapplication.h
@@ -187,8 +187,10 @@ private:
         {{u"torrents"_s, u"editCategory"_s}, Http::METHOD_POST},
         {{u"torrents"_s, u"editTracker"_s}, Http::METHOD_POST},
         {{u"torrents"_s, u"editWebSeed"_s}, Http::METHOD_POST},
+        {{u"torrents"_s, u"fetchMetadata"_s}, Http::METHOD_POST},
         {{u"torrents"_s, u"filePrio"_s}, Http::METHOD_POST},
         {{u"torrents"_s, u"increasePrio"_s}, Http::METHOD_POST},
+        {{u"torrents"_s, u"parseMetadata"_s}, Http::METHOD_POST},
         {{u"torrents"_s, u"reannounce"_s}, Http::METHOD_POST},
         {{u"torrents"_s, u"recheck"_s}, Http::METHOD_POST},
         {{u"torrents"_s, u"removeCategories"_s}, Http::METHOD_POST},

src/webui/www/private/css/Layout.css
@@ -155,7 +155,8 @@ Required by:
     width: 5px;
 }
 
-#desktopNavbar li ul li a {
+#desktopNavbar li ul li a,
+#desktopNavbar li ul li div.anchor {
     color: var(--color-text-default);
     font-weight: normal;
     min-width: 155px;
@@ -163,7 +164,8 @@ Required by:
     position: relative;
 }
 
-#desktopNavbar li ul li a:hover {
+#desktopNavbar li ul li a:hover,
+#desktopNavbar li ul li div.anchor:hover {
     background-color: var(--color-background-hover);
     color: var(--color-text-white);
 }

src/webui/www/private/css/Window.css
@@ -184,6 +184,16 @@ div.mochaToolbarWrapper.bottom {
     width: 16px;
 }
 
+.mochaErrorIcon {
+    background: url("../images/error.svg") no-repeat;
+    background-size: 16px;
+    bottom: 7px;
+    height: 16px;
+    left: 6px;
+    position: absolute;
+    width: 16px;
+}
+
 .mochaIframe {
     width: 100%;
 }

src/webui/www/private/css/style.css
@@ -676,6 +676,17 @@ td.generalLabel {
     width: 1px;
 }
 
+td.fullWidth {
+    box-sizing: border-box;
+    max-width: none;
+    width: 100%;
+    word-break: break-all;
+}
+
+td.noWrap {
+    white-space: nowrap;
+}
+
 #tristate_cb {
     margin-bottom: 0;
     margin-top: 0;
@@ -839,7 +850,8 @@ td.statusBarSeparator {
     color: var(--color-text-green);
 }
 
-#torrentFilesTableDiv .dynamicTable tr.nonAlt:hover {
+#torrentFilesTableDiv .dynamicTable tr.nonAlt:hover,
+#addTorrentFilesTableDiv .dynamicTable tr.nonAlt:hover {
     background-color: var(--color-background-hover);
     color: var(--color-text-white);
 }

src/webui/www/private/download.html
@@ -3,13 +3,11 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>QBT_TR(Add Torrent Links)QBT_TR[CONTEXT=downloadFromURL]</title>
+    <title>QBT_TR(Add Torrent Links)QBT_TR[CONTEXT=DownloadFromURLDialog]</title>
     <link rel="stylesheet" href="css/style.css?v=${CACHEID}" type="text/css">
     <link rel="stylesheet" href="css/Window.css?v=${CACHEID}" type="text/css">
     <script defer src="scripts/localpreferences.js?v=${CACHEID}"></script>
     <script defer src="scripts/color-scheme.js?v=${CACHEID}"></script>
-    <script defer src="scripts/download.js?v=${CACHEID}"></script>
-    <script defer src="scripts/pathAutofill.js?v=${CACHEID}"></script>
     <script>
         "use strict";
 
@@ -23,6 +21,8 @@
                 }
             });
 
+            document.getElementById("urls").focus();
+
             const encodedUrls = new URLSearchParams(window.location.search).get("urls");
             if (encodedUrls !== null) {
                 const urls = encodedUrls.split("|").map(decodeURIComponent);
@@ -30,172 +30,42 @@
                     document.getElementById("urls").value = urls.join("\n");
             }
 
-            let submitted = false;
+            document.getElementById("submitButton").addEventListener("click", (e) => {
+                e.preventDefault();
 
-            document.getElementById("downloadForm").addEventListener("submit", (event) => {
-                document.getElementById("startTorrentHidden").value = document.getElementById("startTorrent").checked ? "false" : "true";
+                const urls = document.getElementById("urls").value.split("\n").map(s => s.trim()).filter(s => (s.length > 0));
+                if (urls.length === 0)
+                    return;
 
-                document.getElementById("dlLimitHidden").value = Number(document.getElementById("dlLimitText").value) * 1024;
-                document.getElementById("upLimitHidden").value = Number(document.getElementById("upLimitText").value) * 1024;
+                for (const url of urls)
+                    window.parent.qBittorrent.Client.createAddTorrentWindow("QBT_TR(Magnet link)QBT_TR[CONTEXT=DownloadFromURLDialog]", url);
 
-                document.getElementById("download_spinner").style.display = "block";
-                submitted = true;
+                window.parent.qBittorrent.Client.closeFrameWindow(window);
             });
 
-            document.getElementById("download_frame").addEventListener("load", (event) => {
-                if (submitted)
-                    window.parent.qBittorrent.Client.closeFrameWindow(window);
+            window.addEventListener("keydown", (event) => {
+                switch (event.key) {
+                    case "Escape":
+                        event.preventDefault();
+                        window.parent.qBittorrent.Client.closeFrameWindow(window);
+                        break;
+                }
             });
-
-            window.qBittorrent.pathAutofill.attachPathAutofill();
         });
     </script>
 </head>
 
 <body>
     <iframe id="download_frame" name="download_frame" class="invisible" title="" src="about:blank"></iframe>
-    <form action="api/v2/torrents/add" enctype="multipart/form-data" method="post" id="downloadForm" style="text-align: center;" target="download_frame" autocorrect="off" autocapitalize="none">
-        <div style="text-align: center;">
-            <br>
-            <h2><label for="urls">QBT_TR(Add torrent links)QBT_TR[CONTEXT=AddNewTorrentDialog]</label></h2>
-            <textarea id="urls" rows="10" name="urls"></textarea>
-            <p><i>QBT_TR(One link per line (HTTP links, Magnet links and info-hashes are supported))QBT_TR[CONTEXT=AddNewTorrentDialog]</i></p>
-            <fieldset class="settings" style="border: 0; text-align: left; margin-top: 6px;">
-                <legend>QBT_TR(Torrent options)QBT_TR[CONTEXT=AddNewTorrentDialog]</legend>
-                <table style="margin: auto;">
-                    <tbody>
-                        <tr>
-                            <td>
-                                <label for="autoTMM">QBT_TR(Torrent Management Mode:)QBT_TR[CONTEXT=AddNewTorrentDialog]</label>
-                            </td>
-                            <td>
-                                <select id="autoTMM" name="autoTMM" onchange="qBittorrent.Download.changeTMM(this)">
-                                    <option selected value="false">QBT_TR(Manual)QBT_TR[CONTEXT=AddNewTorrentDialog]</option>
-                                    <option value="true">QBT_TR(Automatic)QBT_TR[CONTEXT=AddNewTorrentDialog]</option>
-                                </select>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <label for="savepath">QBT_TR(Save files to location:)QBT_TR[CONTEXT=HttpServer]</label>
-                            </td>
-                            <td>
-                                <input type="text" id="savepath" name="savepath" class="pathDirectory" style="width: 16em;">
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <label for="rename">QBT_TR(Rename torrent)QBT_TR[CONTEXT=HttpServer]</label>
-                            </td>
-                            <td>
-                                <input type="text" id="rename" name="rename" style="width: 16em;">
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <label id="categoryLabel" for="categorySelect">QBT_TR(Category:)QBT_TR[CONTEXT=AddNewTorrentDialog]</label>
-                            </td>
-                            <td>
-                                <div class="select-watched-folder-editable">
-                                    <select id="categorySelect" onchange="qBittorrent.Download.changeCategorySelect(this)">
-                                        <option selected value="\other"></option>
-                                    </select>
-                                    <input type="text" name="category" aria-labelledby="categoryLabel">
-                                </div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <label for="startTorrent">QBT_TR(Start torrent)QBT_TR[CONTEXT=AddNewTorrentDialog]</label>
-                            </td>
-                            <td>
-                                <input type="hidden" id="startTorrentHidden" name="stopped">
-                                <input type="checkbox" id="startTorrent">
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <label for="addToTopOfQueue">QBT_TR(Add to top of queue)QBT_TR[CONTEXT=AddNewTorrentDialog]</label>
-                            </td>
-                            <td>
-                                <input type="checkbox" id="addToTopOfQueue" name="addToTopOfQueue" value="true">
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <label for="stopCondition">QBT_TR(Stop condition:)QBT_TR[CONTEXT=AddNewTorrentDialog]</label>
-                            </td>
-                            <td>
-                                <select id="stopCondition" name="stopCondition">
-                                    <option selected value="None">QBT_TR(None)QBT_TR[CONTEXT=AddNewTorrentDialog]</option>
-                                    <option value="MetadataReceived">QBT_TR(Metadata received)QBT_TR[CONTEXT=AddNewTorrentDialog]</option>
-                                    <option value="FilesChecked">QBT_TR(Files checked)QBT_TR[CONTEXT=AddNewTorrentDialog]</option>
-                                </select>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <label for="skip_checking">QBT_TR(Skip hash check)QBT_TR[CONTEXT=AddNewTorrentDialog]</label>
-                            </td>
-                            <td>
-                                <input type="checkbox" id="skip_checking" name="skip_checking" value="true">
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <label for="contentLayout">QBT_TR(Content layout:)QBT_TR[CONTEXT=AddNewTorrentDialog]</label>
-                            </td>
-                            <td>
-                                <select id="contentLayout" name="contentLayout">
-                                    <option selected value="Original">QBT_TR(Original)QBT_TR[CONTEXT=AddNewTorrentDialog]</option>
-                                    <option value="Subfolder">QBT_TR(Create subfolder)QBT_TR[CONTEXT=AddNewTorrentDialog]</option>
-                                    <option value="NoSubfolder">QBT_TR(Don't create subfolder)QBT_TR[CONTEXT=AddNewTorrentDialog]</option>
-                                </select>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <label for="sequentialDownload">QBT_TR(Download in sequential order)QBT_TR[CONTEXT=TransferListWidget]</label>
-                            </td>
-                            <td>
-                                <input type="checkbox" id="sequentialDownload" name="sequentialDownload" value="true">
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <label for="firstLastPiecePrio">QBT_TR(Download first and last pieces first)QBT_TR[CONTEXT=TransferListWidget]</label>
-                            </td>
-                            <td>
-                                <input type="checkbox" id="firstLastPiecePrio" name="firstLastPiecePrio" value="true">
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <label for="dlLimitText">QBT_TR(Limit download rate)QBT_TR[CONTEXT=HttpServer]</label>
-                            </td>
-                            <td>
-                                <input type="hidden" id="dlLimitHidden" name="dlLimit">
-                                <input type="text" id="dlLimitText" style="width: 16em;" placeholder="KiB/s">
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <label for="upLimitText">QBT_TR(Limit upload rate)QBT_TR[CONTEXT=HttpServer]</label>
-                            </td>
-                            <td>
-                                <input type="hidden" id="upLimitHidden" name="upLimit">
-                                <input type="text" id="upLimitText" style="width: 16em;" placeholder="KiB/s">
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-                <div id="submitbutton" style="margin-top: 12px; text-align: center;">
-                    <button type="submit" id="submitButton">QBT_TR(Download)QBT_TR[CONTEXT=downloadFromURL]</button>
-                </div>
-            </fieldset>
+    <div style="text-align: center;">
+        <br>
+        <h2 class="vcenter">QBT_TR(Add torrent links)QBT_TR[CONTEXT=DownloadFromURLDialog]</h2>
+        <textarea id="urls" name="urls" rows="10" required style="width: 80%;" aria-label="QBT_TR(URLs)QBT_TR[CONTEXT=DownloadFromURLDialog]"></textarea>
+        <p><i>QBT_TR(One link per line (HTTP links, Magnet links and info-hashes are supported))QBT_TR[CONTEXT=DownloadFromURLDialog]</i></p>
+        <div id="submitbutton" style="margin-top: 12px; text-align: center;">
+            <button type="submit" id="submitButton">QBT_TR(Download)QBT_TR[CONTEXT=DownloadFromURLDialog]</button>
         </div>
-    </form>
-    <div id="download_spinner" class="mochaSpinner"></div>
+    </div>
 </body>
 
 </html>

src/webui/www/private/index.html
@@ -39,6 +39,7 @@
     <script defer src="scripts/file-tree.js?v=${CACHEID}"></script>
     <script defer src="scripts/dynamicTable.js?locale=${LANG}&v=${CACHEID}"></script>
     <script defer src="scripts/rename-files.js?v=${CACHEID}"></script>
+    <script defer src="scripts/torrent-content.js?v=${CACHEID}"></script>
     <script defer src="scripts/client.js?locale=${LANG}&v=${CACHEID}"></script>
     <script defer src="scripts/contextmenu.js?locale=${LANG}&v=${CACHEID}"></script>
     <script defer src="scripts/pathAutofill.js?v=${CACHEID}"></script>
@@ -59,7 +60,15 @@
                     <li>
                         <a class="returnFalse">QBT_TR(File)QBT_TR[CONTEXT=MainWindow]</a>
                         <ul>
-                            <li><a id="uploadLink"><img class="MyMenuIcon" alt="QBT_TR(Add Torrent File...)QBT_TR[CONTEXT=MainWindow]" src="images/list-add.svg" width="16" height="16">QBT_TR(Add Torrent File...)QBT_TR[CONTEXT=MainWindow]</a></li>
+                            <li>
+                                <div id="uploadLink" class="anchor">
+                                    <label id="uploadLinkLabel" for="fileselectLink" style="cursor: pointer;">
+                                        <img class="MyMenuIcon" alt="QBT_TR(Add Torrent File...)QBT_TR[CONTEXT=MainWindow]" src="images/list-add.svg" width="16" height="16">
+                                        <span>QBT_TR(Add Torrent File...)QBT_TR[CONTEXT=MainWindow]</span>
+                                    </label>
+                                    <input type="file" id="fileselectLink" class="fileselect" accept=".torrent, application/x-bittorrent" name="fileselect[]" multiple style="display: none;">
+                                </div>
+                            </li>
                             <li><a id="downloadLink"><img class="MyMenuIcon" alt="QBT_TR(Add Torrent Link...)QBT_TR[CONTEXT=MainWindow]" src="images/insert-link.svg" width="16" height="16">QBT_TR(Add Torrent Link...)QBT_TR[CONTEXT=MainWindow]</a></li>
                             <li class="divider"><a id="logoutLink"><img class="MyMenuIcon" alt="QBT_TR(Logout)QBT_TR[CONTEXT=HttpServer]" src="images/system-log-out.svg" width="16" height="16">QBT_TR(Logout)QBT_TR[CONTEXT=HttpServer]</a></li>
                             <li><a id="shutdownLink"><img class="MyMenuIcon" alt="QBT_TR(Exit qBittorrent)QBT_TR[CONTEXT=HttpServer]" src="images/application-exit.svg" width="16" height="16">QBT_TR(Exit qBittorrent)QBT_TR[CONTEXT=HttpServer]</a></li>
@@ -113,7 +122,12 @@
             </div>
             <div id="mochaToolbar">
                 &nbsp;&nbsp;
-                <a id="uploadButton"><img class="mochaToolButton" title="QBT_TR(Add Torrent File...)QBT_TR[CONTEXT=MainWindow]" src="images/list-add.svg" alt="QBT_TR(Add Torrent File...)QBT_TR[CONTEXT=MainWindow]" width="24" height="24"></a>
+                <div id="uploadButton" style="display: inline-block;">
+                    <label for="fileselectButton" style="cursor: pointer;">
+                        <img class="mochaToolButton" title="QBT_TR(Add Torrent File...)QBT_TR[CONTEXT=MainWindow]" src="images/list-add.svg" alt="QBT_TR(Add Torrent File...)QBT_TR[CONTEXT=MainWindow]" width="24" height="24">
+                    </label>
+                    <input type="file" id="fileselectButton" class="fileselect" accept=".torrent, application/x-bittorrent" name="fileselect[]" multiple style="display: none;">
+                </div>
                 <a id="downloadButton"><img class="mochaToolButton" title="QBT_TR(Add Torrent Link...)QBT_TR[CONTEXT=MainWindow]" src="images/insert-link.svg" alt="QBT_TR(Add Torrent Link...)QBT_TR[CONTEXT=MainWindow]" width="24" height="24"></a>
                 <a id="deleteButton"><img class="mochaToolButton" title="QBT_TR(Remove)QBT_TR[CONTEXT=TransferListWidget]" src="images/list-remove.svg" alt="QBT_TR(Remove)QBT_TR[CONTEXT=TransferListWidget]" width="24" height="24"></a>
                 <a id="startButton" class="divider"><img class="mochaToolButton" title="QBT_TR(Start)QBT_TR[CONTEXT=TransferListWidget]" src="images/torrent-start.svg" alt="QBT_TR(Start)QBT_TR[CONTEXT=TransferListWidget]" width="24" height="24"></a>

commit e5026e4b19b5c37a33dfdb7877ef7effcc4822be

WebUI: fix opening "Add torrent" window with double click on RSS feed item
Restores the ability to open the "Add torrent" dialogue, when double clicking on an RSS feed item.
Broke in: 02892d12501e9ba96cb5a0375da42d447bc5ece2 as part of #21645.

PR #23413.

src/webui/www/private/scripts/dynamicTable.js
@@ -3245,7 +3245,7 @@ window.qBittorrent.DynamicTable ??= (() => {
                 if (!tr)
                     return;
 
-                const { name, torrentURL } = this._this.rows.get(this.rowId).full_data;
+                const { name, torrentURL } = this.getRow(tr.rowId).full_data;
                 qBittorrent.Client.createAddTorrentWindow(name, torrentURL);
             });
         }