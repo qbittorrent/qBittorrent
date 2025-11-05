6b464b9e61569f553577c2b426f5120c1095cd49
Vladimir Golovnev (Glassez) <glassez@yandex.ru>
2025-11-05 16:50:18 +0300

    Implement TransferListFiltersWidgetItem class

--- src/gui/transferlistfilterswidgetitem.h
+++ src/gui/transferlistfilterswidgetitem.h
@@ -1,7 +1,6 @@
 /*
  * Bittorrent Client using Qt and libtorrent.
- * Copyright (C) 2023-2025  Vladimir Golovnev <glassez@yandex.ru>
- * Copyright (C) 2006  Christophe Dumez <chris@qbittorrent.org>
+ * Copyright (C) 2025  Vladimir Golovnev <glassez@yandex.ru>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -29,40 +28,26 @@
 
 #pragma once
 
-#include <QtContainerFwd>
 #include <QWidget>
 
-#include "base/bittorrent/trackerentry.h"
+class QCheckBox;
+class QString;
 
-class CategoryFilterWidget;
-class StatusFilterWidget;
-class TagFilterWidget;
-class TrackersFilterWidget;
-class TransferListWidget;
-
-namespace BitTorrent
-{
-    class Torrent;
-    struct TrackerEntryStatus;
-}
-
-class TransferListFiltersWidget final : public QWidget
+class TransferListFiltersWidgetItem final : public QWidget
 {
     Q_OBJECT
-    Q_DISABLE_COPY_MOVE(TransferListFiltersWidget)
+    Q_DISABLE_COPY_MOVE(TransferListFiltersWidgetItem)
 
 public:
-    TransferListFiltersWidget(QWidget *parent, TransferListWidget *transferList, bool downloadFavicon);
-    void setDownloadTrackerFavicon(bool value);
+    TransferListFiltersWidgetItem(const QString &caption, QWidget *filterWidget, QWidget *parent = nullptr);
 
-public slots:
-    void addTrackers(const BitTorrent::Torrent *torrent, const QList<BitTorrent::TrackerEntry> &trackers);
-    void removeTrackers(const BitTorrent::Torrent *torrent, const QStringList &trackers);
-    void refreshTrackers(const BitTorrent::Torrent *torrent);
-    void trackerEntryStatusesUpdated(const BitTorrent::Torrent *torrent
-            , const QHash<QString, BitTorrent::TrackerEntryStatus> &updatedTrackers);
+    bool isChecked() const;
+    void setChecked(bool value);
+
+signals:
+    void toggled(bool checked);
 
 private:
-    TransferListWidget *m_transferList = nullptr;
-    TrackersFilterWidget *m_trackersFilterWidget = nullptr;
+    QCheckBox *m_caption = nullptr;
+    QWidget *m_filterWidget = nullptr;
 };
