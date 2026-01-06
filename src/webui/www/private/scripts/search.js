a7744118760ac97e7fd548dbdb5083680784be70
Thomas Piccirello <thomas@piccirello.com>
2026-01-05 16:48:01 -0800

    WebUI: Enable virtual list in search tables

--- src/webui/www/private/scripts/search.js
+++ src/webui/www/private/scripts/search.js
@@ -123,7 +123,7 @@ window.qBittorrent.Search ??= (() => {
             }
         });
         searchResultsTable = new window.qBittorrent.DynamicTable.SearchResultsTable();
-        searchResultsTable.setup("searchResultsTableDiv", "searchResultsTableFixedHeaderDiv", searchResultsTableContextMenu);
+        searchResultsTable.setup("searchResultsTableDiv", "searchResultsTableFixedHeaderDiv", searchResultsTableContextMenu, true);
         getPlugins();
 
         searchResultsTable.dynamicTableDiv.addEventListener("dblclick", (e) => { downloadSearchTorrent(); });
