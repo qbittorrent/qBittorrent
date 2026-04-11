3017dad22218bc6237337075cd9b85988c6e4442
Davikch <davidogluzd@gmail.com>
2026-04-11 23:54:36 +1000

    Add the Average Monthly Upload metric
    The metric shows Total Upload / Active Months. It's very similar to the popularity metric, but it lets the user compare torrents regardless of file sizes, and consenquently, regardless of ratio. This metric shines if one wants to max out the bandwith usage whilst being less concerned about drive space. The metric is disabled by default.

--- src/webui/www/private/scripts/dynamicTable.js
+++ src/webui/www/private/scripts/dynamicTable.js
@@ -1192,6 +1192,7 @@ window.qBittorrent.DynamicTable ??= (() => {
             this.newColumn("eta", "", "QBT_TR(ETA)QBT_TR[CONTEXT=TransferListModel]", 100, true);
             this.newColumn("ratio", "", "QBT_TR(Ratio)QBT_TR[CONTEXT=TransferListModel]", 100, true);
             this.newColumn("popularity", "", "QBT_TR(Popularity)QBT_TR[CONTEXT=TransferListModel]", 100, true);
+            this.newColumn("average_monthly_upload", "", "QBT_TR(Average Monthly Upload)QBT_TR[CONTEXT=TransferListModel]", 100, false);
             this.newColumn("category", "", "QBT_TR(Category)QBT_TR[CONTEXT=TransferListModel]", 100, true);
             this.newColumn("tags", "", "QBT_TR(Tags)QBT_TR[CONTEXT=TransferListModel]", 100, true);
             this.newColumn("creation_date", "", "QBT_TR(Created On)QBT_TR[CONTEXT=TransferListModel]", 100, false);
@@ -1520,6 +1521,9 @@ window.qBittorrent.DynamicTable ??= (() => {
 
             // popularity
             this.columns["popularity"].updateTd = displayRatio;
+
+            // average monthly upload
+            this.columns["average_monthly_upload"].updateTd = this.columns["size"].updateTd;
 
             // creation_date
             this.columns["creation_date"].updateTd = displayDate;
