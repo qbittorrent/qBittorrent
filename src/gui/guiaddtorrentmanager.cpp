0652f5611224d3bd28663b0859a15963c301efe0
Vladimir Golovnev <glassez@yandex.ru>
2026-04-23 15:27:08 +0300

    Fix crash when closing app with AddNewTorrentDialog opened
    Destruct GUIAddTorrentManager instance in proper place.
    Delete AddNewTorrentDialog instances in GUIAddTorrentManager directly.
    
    PR #24107.
    Closes #23461.
    Closes #24104.
    Closes #24105.

--- src/gui/guiaddtorrentmanager.cpp
+++ src/gui/guiaddtorrentmanager.cpp
@@ -1,6 +1,6 @@
 /*
  * Bittorrent Client using Qt and libtorrent.
- * Copyright (C) 2015-2023  Vladimir Golovnev <glassez@yandex.ru>
+ * Copyright (C) 2015-2026  Vladimir Golovnev <glassez@yandex.ru>
  * Copyright (C) 2006  Christophe Dumez <chris@qbittorrent.org>
  *
  * This program is free software; you can redistribute it and/or
@@ -87,6 +87,9 @@ GUIAddTorrentManager::~GUIAddTorrentManager()
     {
         dialog->disconnect(this);
         dialog->reject();
+        // `dialog` should be deleted here in order to avoid accessing 
+        // already deleted core components when deleting later
+        delete dialog;
     }
 }
 
