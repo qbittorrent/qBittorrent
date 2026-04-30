40d2fc218386cb394520eb9840877c571a3a1e1e
Vladimir Golovnev (Glassez) <glassez@yandex.ru>
2026-04-30 13:56:49 +0300

    Last attempt

--- src/gui/deletionconfirmationdialog.h
+++ src/gui/deletionconfirmationdialog.h
@@ -1,6 +1,6 @@
 /*
  * Bittorrent Client using Qt and libtorrent.
- * Copyright (C) 2024  Vladimir Golovnev <glassez@yandex.ru>
+ * Copyright (C) 2024-2026  Vladimir Golovnev <glassez@yandex.ru>
  * Copyright (C) 2006  Christophe Dumez <chris@qbittorrent.org>
  *
  * This program is free software; you can redistribute it and/or
@@ -44,14 +44,10 @@ class DeletionConfirmationDialog final : public QDialog
     Q_DISABLE_COPY_MOVE(DeletionConfirmationDialog)
 
 public:
-    DeletionConfirmationDialog(QWidget *parent, int size, const QString &name, bool defaultDeleteFiles);
+    DeletionConfirmationDialog(QWidget *parent, int torrentsCount, const QString &name, bool defaultDeleteFiles);
     ~DeletionConfirmationDialog() override;
 
     bool isRemoveContentSelected() const;
-
-private slots:
-    void updateRememberButtonState();
-    void on_rememberBtn_clicked();
 
 private:
     Ui::DeletionConfirmationDialog *m_ui = nullptr;
