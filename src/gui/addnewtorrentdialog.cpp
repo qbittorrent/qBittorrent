294f03417233e4352330ce590c8aaab52eaf9ffc
Vladimir Golovnev <glassez@yandex.ru>
2026-07-30 09:13:56 +0300

    Skip processing of already being added torrent source
    PR #24743.
    Closes #24683.

--- src/gui/addnewtorrentdialog.cpp
+++ src/gui/addnewtorrentdialog.cpp
@@ -1,6 +1,6 @@
 /*
  * Bittorrent Client using Qt and libtorrent.
- * Copyright (C) 2022-2025  Vladimir Golovnev <glassez@yandex.ru>
+ * Copyright (C) 2022-2026  Vladimir Golovnev <glassez@yandex.ru>
  * Copyright (C) 2012  Christophe Dumez <chris@qbittorrent.org>
  *
  * This program is free software; you can redistribute it and/or
@@ -854,6 +854,9 @@ void AddNewTorrentDialog::updateMetadata(const BitTorrent::TorrentInfo &metadata
         return;
 
     BitTorrent::TorrentDescriptor &torrentDescr = m_currentContext->torrentDescr;
+    if (torrentDescr.info())
+        return;
+
     Q_ASSERT(metadata.matchesInfoHash(torrentDescr.infoHash()));
     if (!metadata.matchesInfoHash(torrentDescr.infoHash())) [[unlikely]]
         return;
