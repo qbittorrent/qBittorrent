b8889df0484c64e0f3333e077bb8a0bec27cf4de
Sophist <3001893+Sophist-UK@users.noreply.github.com>
2026-08-29 20:24:46 +0100

    Iterate forwards rather than backwards
    I used `closeAllTabs` as a template and that needs to iterate backwards because closing tabs changes the index. But this doesn't change the index and can iterate normally.
    
    Co-authored-by: Chocobo1 <Chocobo1@users.noreply.github.com>

--- src/gui/search/searchwidget.cpp
+++ src/gui/search/searchwidget.cpp
@@ -984,7 +984,7 @@ void SearchWidget::refreshAllTabs()
         return;
     }
 
-    for (int tabIndex = (m_ui->tabWidget->count() - 1); tabIndex >= 0; --tabIndex)
+    for (int tabIndex = 0; tabIndex < m_ui->tabWidget->count(); ++tabIndex)
     {
         if (auto *searchJobWidget = static_cast<SearchJobWidget *>(m_ui->tabWidget->widget(tabIndex));
                 searchJobWidget->status() != SearchJobWidget::Status::Ongoing)
